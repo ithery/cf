1d8e7920a254e88d49182042500ebc96a0ca49dd
heerrr <hery.cresenity@gmail.com>
2014-10-14 13:41:06 +0700

    system

---
+++ system/core/Bootstrap.php
@@ -0,0 +1,60 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * CF process control file, loaded by the front controller.
+ * 
+ * $Id: Bootstrap.php 4409 2009-06-06 00:48:26Z zombor $
+ *
+ * @package    Core
+ * @author     CF Team
+ * @copyright  (c) 2007 CF Team
+ * @license    http://kohanaphp.com/license.html
+ */
+
+define('CF_VERSION',  '2.3.4');
+define('CF_CODENAME', 'buteo regalis');
+
+// Test of CF is running in Windows
+define('CF_IS_WIN', DIRECTORY_SEPARATOR === '\\');
+
+// CF benchmarks are prefixed to prevent collisions
+define('SYSTEM_BENCHMARK', 'system_benchmark');
+
+// Load benchmarking support
+require SYSPATH.'core/CFBenchmark'.EXT;
+
+// Start total_execution
+CFBenchmark::start(SYSTEM_BENCHMARK.'_total_execution');
+
+// Start kohana_loading
+CFBenchmark::start(SYSTEM_BENCHMARK.'_kohana_loading');
+
+// Load core files
+require SYSPATH.'core/utf8'.EXT;
+require SYSPATH.'core/CFEvent'.EXT;
+require SYSPATH.'core/CFData'.EXT;
+require SYSPATH.'core/CFRouter'.EXT;
+require SYSPATH.'core/CF'.EXT;
+
+// Prepare the environment
+CF::setup();
+
+// End kohana_loading
+CFBenchmark::stop(SYSTEM_BENCHMARK.'_kohana_loading');
+
+// Start system_initialization
+CFBenchmark::start(SYSTEM_BENCHMARK.'_system_initialization');
+
+// Prepare the system
+CFEvent::run('system.ready');
+
+// Determine routing
+CFEvent::run('system.routing');
+
+// End system_initialization
+CFBenchmark::stop(SYSTEM_BENCHMARK.'_system_initialization');
+
+// Make the magic happen!
+CFEvent::run('system.execute');
+
+// Clean up and exit
+CFEvent::run('system.shutdown');
