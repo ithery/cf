e9a927992780e9b774d731bff6400e44c752ec0a
Gede <gede@piposystem.com>
2014-12-04 10:50:00 +0700

    new

---
+++ data/app/minitors.php
@@ -0,0 +1,6 @@
+<?php 
+return array(
+	'app_id'=>'101',
+	'code'=>'minitors',
+	'name'=>'Mini TORS',
+);
---
+++ data/domain/minitors.local.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	"app_id"=>'101',
+	"app_code"=>"minitors",
+	"org_id"=>null,
+	"org_code"=>'',
+	"store_id"=>null,
+	"store_code"=>null,
+	"domain"=>"minitors.local",
+);
