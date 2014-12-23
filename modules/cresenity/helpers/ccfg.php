21f390e1abe88bb63d03839962f58148cfe5baff
Andy <andi@piposystem.com>
2014-12-23 14:33:41 +0700

    ccfg

--- modules/cresenity/helpers/ccfg.php
+++ modules/cresenity/helpers/ccfg.php
@@ -43,51 +43,59 @@ class ccfg {
 			
 		   
 		}
-		
+		$ccfg_files = array('app','app_setting');
 		if (!isset(self::$_config[$domain])) {
-            $config_path = DOCROOT . "config" . DIRECTORY_SEPARATOR;
-            $cresenity_config_file = $config_path . "app" . EXT;
-            $config_file = $cresenity_config_file;
-            if ($org_code != "") {
+			self::$_config[$domain] = array();
+			foreach($ccfg_files as $f) {
 				
-                $org_config_path = $config_path . "app" . DIRECTORY_SEPARATOR;
-                if (!is_dir($org_config_path))
-                    mkdir($org_config_path);
-                $org_config_path = $org_config_path . $org_code . DIRECTORY_SEPARATOR;
-                if (!is_dir($org_config_path))
-                    mkdir($org_config_path);
-                $org_config_path = $org_config_path . $domain . DIRECTORY_SEPARATOR;
-                if (!is_dir($org_config_path))
-                    mkdir($org_config_path);
-                $org_config_file = $org_config_path . "app" . EXT;
+				
+				
+				$config_path = DOCROOT . "config" . DIRECTORY_SEPARATOR;
+				$cresenity_config_file = $config_path . $f . EXT;
+				$config_file = $cresenity_config_file;
+				if ($org_code != "") {
+					
+					$org_config_path = $config_path . $f . DIRECTORY_SEPARATOR;
+					if (!is_dir($org_config_path))
+						mkdir($org_config_path);
+					$org_config_path = $org_config_path . $org_code . DIRECTORY_SEPARATOR;
+					if (!is_dir($org_config_path))
+						mkdir($org_config_path);
+					$org_config_path = $org_config_path . $domain . DIRECTORY_SEPARATOR;
+					if (!is_dir($org_config_path))
+						mkdir($org_config_path);
+					$org_config_file = $org_config_path . $f . EXT;
 
-                $config_file = $org_config_file;
-            }
-			
-            if (!file_exists($config_file))
-                $config_file = $cresenity_config_file;
-			
-            $ccfg = require $config_file;
-			if(!is_array($ccfg)) {
-				$ccfg = $config;
-				unset($config);
-			}
-			
-            $app_files = CF::get_files('config','app',$domain);
-			$app_files = array_reverse($app_files);
-			foreach ($app_files as $file) {
-				$app_ccfg = include $file;
-				if(!is_array($app_ccfg)) {
-					trigger_error("Invalid Config Format On ".$file);
+					$config_file = $org_config_file;
 				}
-				$ccfg = array_merge($ccfg,$app_ccfg);
+				
+				if (!file_exists($config_file))
+					$config_file = $cresenity_config_file;
+				$ccfg = array();
+				if(file_exists($config_file)) {
+					$ccfg = require $config_file;
+				}
+				if(!is_array($ccfg)) {
+					$ccfg = $config;
+					unset($config);
+				}
+				
+				$app_files = CF::get_files('config',$f,$domain);
+				
+				$app_files = array_reverse($app_files);
+				foreach ($app_files as $file) {
+					$app_ccfg = include $file;
+					if(!is_array($app_ccfg)) {
+						trigger_error("Invalid Config Format On ".$file);
+					}
+					$ccfg = array_merge($ccfg,$app_ccfg);
+				}
+				
+				
+				self::$_config[$domain] = array_merge(self::$_config[$domain],$ccfg);
+				
 			}
-			
-			
-			self::$_config[$domain] = $ccfg;
-			
-        }
-		
+		}
 		
         if (isset(self::$_config[$domain][$key])) {
             $val = self::$_config[$domain][$key];
