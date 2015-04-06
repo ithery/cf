9ed13c3107807c678b25d84e1e9ef31257200b77
RobinYonathan <robinyonathan@yahoo.com>
2015-04-06 10:29:48 +0700

    add suryagita

---
+++ data/domain/suryagita.local.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	"app_id"=>'301',
+	"app_code"=>"payroll",
+	"org_id"=>'1003',
+	"org_code"=>'suryagita',
+	"store_id"=>null,
+	"store_code"=>null,
+	"domain"=>"suryagita.local",
+);
---
+++ data/org/suryagita.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	"org_id"=>'1003',
+	"org_code"=>"suryagita",
+	"org_name"=>"Suryagita",
+	
+);
+
