c7f8db4e1829da7f1293ee3f3c462942df880de2
septian <septian@pippo-solutindo.com>
2015-05-26 15:50:10 +0700

    add domain dev suryagita

---
+++ data/domain/dev.suryagita.local.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	"app_id"=>'301',
+	"app_code"=>"hrm",
+	"org_id"=>'1004',
+	"org_code"=>'devsuryagita',
+	"store_id"=>null,
+	"store_code"=>null,
+	"domain"=>"dev.suryagita.local",
+);
---
+++ data/org/devsuryagita.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	"org_id"=>'1004',
+	"org_code"=>"devsuryagita",
+	"org_name"=>"Dev Suryagita",
+	
+);
+
