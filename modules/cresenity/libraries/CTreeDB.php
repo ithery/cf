9d6eed0a4401fe4097eb789bba0799423fb8fc90
delvo <delvo@piposystem.com>
2015-06-26 16:53:21 +0700

    update ctreedb

--- modules/cresenity/libraries/CTreeDB.php
+++ modules/cresenity/libraries/CTreeDB.php
@@ -157,7 +157,7 @@ class CTreeDB {
         $db->query($q);
     }
 
-    public function update($id, $data, $parent_id) {
+    public function update($id, $data, $parent_id = NULL) {
         $app = CApp::instance();
         $user = $app->user();
         $db = $this->db;
