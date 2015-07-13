26972ccae7e13e57f10b63386e12005cbd68eda5
lukman <lukman@pippo-solutindo.com>
2015-07-13 15:38:28 +0700

    B2C

---
+++ data/domain/tiketpasti.b2c.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'404',
+	'app_code'=>'torsb2c',
+	'org_id'=>10,
+	'org_code'=>'therabuana',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'tiketpasti.b2c.local',
+);
