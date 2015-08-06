2be33d8459b46857c5600ce95e474d7f9a081a59
lukman <lukman@pippo-solutindo.com>
2015-08-06 10:49:21 +0700

    kliktors

---
+++ data/domain/kliktors.b2b.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'202',
+	'app_code'=>'torsb2b',
+	'org_id'=>1,
+	'org_code'=>'kliktors',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'kliktors.b2b.local',
+);
---
+++ data/org/kliktors.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	"org_id"=>1,
+	"org_code"=>"kliktors",
+	"org_name"=>"KLIKTORS",
+	
+);
+
