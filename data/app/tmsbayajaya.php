73445370a765c4d0977a14f079512c2cbbc9cee2
jefry <jefrey@pippo-solutindo.com>
2015-09-28 14:30:09 +0700

    create

---
+++ data/app/tmsbayajaya.php
@@ -0,0 +1,6 @@
+<?php 
+return array(
+	'app_id'=>'204',
+	'code'=>'tmsbayajaya',
+	'name'=>'TMS-BAYAJAYA',
+);
---
+++ data/domain/tmsbayajaya.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'204',
+	'app_code'=>'tmsbayajaya',
+	'org_id'=>NULL,
+	'org_code'=>NULL,
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'tmsbayajaya.local',
+);
