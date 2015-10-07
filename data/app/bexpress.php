155d98e18ead806615b5f6034e1cfc92b11cd651
raymond <raymond@piposystem.com>
2015-10-07 14:44:58 +0700

    add domain bexpress

---
+++ data/app/bexpress.php
@@ -0,0 +1,6 @@
+<?php 
+return array(
+	'app_id'=>'404',
+	'code'=>'torsb2c',
+	'name'=>'TORS B2C',
+);
---
+++ data/domain/bexpress.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'501',
+	'app_code'=>'bexpress',
+	'org_id'=>NULL,
+	'org_code'=>NULL,
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'bexpress.local',
+);
