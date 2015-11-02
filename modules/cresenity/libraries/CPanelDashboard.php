4d84424072d45641a68e34ac4905b90382443da7
albert <albert@piposystem.com>
2015-11-02 11:55:49 +0700

    paneldashboard

--- modules/cresenity/libraries/CPanelDashboard.php
+++ modules/cresenity/libraries/CPanelDashboard.php
@@ -2,17 +2,20 @@
 
 class CPanelDashboard extends CObject {
 	protected $_engine;
-    protected function __construct($engine_name) {
-		$path = dirname(__FILE__) . DS . "PanelDashboard" . DS . $engine_name . EXT;
+    protected function __construct($engine_name,$app_code=null) {
+		if ($app_code == null) {
+            $app_code = CF::app_code();
+        }
+		$path = DOCROOT . 'application' . DS . $app_code . DS .'default'. DS .'libraries' . DS . "PanelDashboard" . DS . $engine_name . EXT;
 		require_once $path;
-		$this->_engine = $file_name::factory();
+		$this->_engine = $engine_name::factory();
 	}
 	public function factory($engine_name){
         return new CPanelDashboard($engine_name);
 	}
 	
-	protected function test(){
-		$_engine->test();
+	public function render(){
+		return $this->_engine->render();
 	}
 
 }
