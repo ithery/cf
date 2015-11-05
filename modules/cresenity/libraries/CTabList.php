fe348d389fb1432b6ab543f34b9b444c5b0be3cf
raymond <raymond@piposystem.com>
2015-11-05 14:36:23 +0700

    update CTabList

--- modules/cresenity/libraries/CTabList.php
+++ modules/cresenity/libraries/CTabList.php
@@ -1,180 +1,207 @@
 <?php
 
-class CTabList extends CElement {
+    class CTabList extends CElement {
 
-    protected $tabs;
-    protected $scrollspy;
-    protected $tab_position;
-    protected $active_tab;
-    protected $ajax;
-    protected $have_icon;
+        protected $tabs;
+        protected $scrollspy;
+        protected $tab_position;
+        protected $active_tab;
+        protected $ajax;
+        protected $have_icon;
+        protected $widget_class;
 
-    public function __construct($id) {
-        parent::__construct($id);
+        public function __construct($id) {
+            parent::__construct($id);
 
-        $this->tab_position = "left";
-        $this->active_tab = "";
-        $this->ajax = true;
-        $this->have_icon = false;
-        $this->scrollspy = true;
-        $this->tabs = array();
-    }
-
-    public static function factory($id) {
-        return new CTabList($id);
-    }
-
-    public function add_tab($id = "") {
-        $tab = CTab::factory($id);
-        if (strlen($this->active_tab) == 0)
-            $this->active_tab = $tab->id;
-        $this->tabs[] = $tab;
-        return $tab;
-    }
-
-    public function active_tab($tab_id) {
-        $this->set_active_tab($tab_id);
-        return $this;
-    }
-
-    public function set_active_tab($tab_id) {
-        $this->active_tab = $tab_id;
-        return $this;
-    }
-
-    public function set_scrollspy($bool) {
-        $this->scrollspy = $bool;
-        return $this;
-    }
-
-    public function set_ajax($bool) {
-        $this->ajax = $bool;
-        return $this;
-    }
-
-    public function html($indent = 0) {
-
-
-        /*
-          $html = new CStringBuilder();
-          $html->set_indent($indent);
-          $add_class="";
-          if($this->tab_position=="left") {
-          $add_class.=" tabs-left";
-          }
-          $html->appendln('<div class="tabbable '.$add_class.'">');
-          $html->appendln('<ul class="nav nav-tabs ">');
-          foreach($this->tabs as $tab) {
-
-          if($tab->id==$this->active_tab) {
-          $tab->set_active(true);
-          }
-          $html->appendln($tab->header_html($html->get_indent()));
-          }
-          $html->appendln('</ul>');
-          $html->appendln('<div class="tab-content">');
-          foreach($this->tabs as $tab) {
-          $html->appendln($tab->html($html->get_indent()));
-          }
-          $html->appendln('</div>');
-          $html->appendln('</div>');
-          ;
-         */
-        $html = new CStringBuilder();
-        $html->set_indent($indent);
-        $ajax_class = "";
-        if ($this->ajax) {
-            $ajax_class = "ajax";
-            //we create the ajax url if there are no url on tab
-        } else {
-            foreach($this->tabs as $tab) {
-                $tab->set_ajax(false);
-            }
+            $this->tab_position = "left";
+            $this->active_tab = "";
+            $this->ajax = true;
+            $this->have_icon = false;
+            $this->scrollspy = true;
+            $this->tabs = array();
+            $this->widget_class = array();
         }
-        $html->appendln('<div class="row-fluid ' . $ajax_class . '" id="' . $this->id . '">');
-        $html->appendln('	<div class="span12">');
 
-        $html->appendln('		<div class="row-fluid">');
-        $html->appendln('			<div class="span2">');
-        $html->appendln('				<div class="side-nav-container affix-top">');
-        $html->appendln('					<ul id="' . $this->id . '-tab-nav" class="nav nav-tabs nav-stacked">');
-
-        $active_tab = null;
-        foreach ($this->tabs as $tab) {
-            if (strlen($this->active_tab) == 0) {
-                $this->active_tab($tab->id);
-            }
-            if ($tab->id == $this->active_tab) {
-                $tab->set_active(true);
-                $active_tab = $tab;
-            }
-            $tab->set_target('' . $this->id . '-ajax-tab-content');
-
-            $html->appendln($tab->header_html($html->get_indent()));
+        public static function factory($id) {
+            return new CTabList($id);
         }
-        $active_tab_icon = "";
-        $active_tab_label = "";
 
-        if ($active_tab != null) {
-            $active_tab_icon = $active_tab->icon;
-            $active_tab_label = $active_tab->label;
+        public function add_tab($id = "") {
+            $tab = CTab::factory($id);
+            if (strlen($this->active_tab) == 0) $this->active_tab = $tab->id;
+            $this->tabs[] = $tab;
+            return $tab;
+        }
 
-            if (strlen($active_tab->ajax_url) > 0) {
-                $tab_ajax_url = $active_tab->ajax_url;
+        public function active_tab($tab_id) {
+            $this->set_active_tab($tab_id);
+            return $this;
+        }
 
-                /*
-                  $url_base = curl::base();
-                  if(substr($tab_ajax_url,0,strlen($url_base)) == $url_base) {
-                  $tab_ajax_url = substr($tab_ajax_url,strlen($url_base));
-                  }
-                  $tab_ajax_url = curl::base(true,'http').$tab_ajax_url;
-                 */
-                //$active_tab_content = CCurl::factory($tab_ajax_url)->exec()->response();
+        public function set_active_tab($tab_id) {
+            $this->active_tab = $tab_id;
+            return $this;
+        }
+
+        public function set_scrollspy($bool) {
+            $this->scrollspy = $bool;
+            return $this;
+        }
+
+        public function set_ajax($bool) {
+            $this->ajax = $bool;
+            return $this;
+        }
+
+        public function add_widget_class($class) {
+            if (is_array($class)) {
+                $this->widget_class = array_merge($class, $this->widget_class);
             }
+            else {
+                $this->widget_class[] = $class;
+            }
+            return $this;
         }
-        $html->appendln('					</ul>');
-        $html->appendln('				</div>');
-        $html->appendln('			</div>');
-        $html->appendln('			<div class="span10">');
-        $html->appendln('				<div id="' . $this->id . '-tab-widget" class="widget-box nomargin widget-transaction-tab">');
-        $html->appendln('					<div class="widget-title">');
-        if ($this->have_icon) {
-            $html->appendln('						<span class="icon">');
-            $html->appendln('							<i class="icon-' . $active_tab_icon . '"></i>');
-            $html->appendln('						</span>');
-        }
-        $html->appendln('						<h5>' . $active_tab_label . '</h5>');
-        $html->appendln('					</div>');
 
-        $html->appendln('					<div class="widget-content">');
+        public function html($indent = 0) {
 
-        if ($this->ajax) {
-            $html->appendln('						<div id="' . $this->id . '-ajax-tab-content">');
-            $html->appendln('						</div>');
-        } else {
+
+            /*
+              $html = new CStringBuilder();
+              $html->set_indent($indent);
+              $add_class="";
+              if($this->tab_position=="left") {
+              $add_class.=" tabs-left";
+              }
+              $html->appendln('<div class="tabbable '.$add_class.'">');
+              $html->appendln('<ul class="nav nav-tabs ">');
+              foreach($this->tabs as $tab) {
+
+              if($tab->id==$this->active_tab) {
+              $tab->set_active(true);
+              }
+              $html->appendln($tab->header_html($html->get_indent()));
+              }
+              $html->appendln('</ul>');
+              $html->appendln('<div class="tab-content">');
+              foreach($this->tabs as $tab) {
+              $html->appendln($tab->html($html->get_indent()));
+              }
+              $html->appendln('</div>');
+              $html->appendln('</div>');
+              ;
+             */
+            $html = new CStringBuilder();
+            $html->set_indent($indent);
+            $ajax_class = "";
+            if ($this->ajax) {
+                $ajax_class = "ajax";
+                //we create the ajax url if there are no url on tab
+            }
+            else {
+                foreach ($this->tabs as $tab) {
+                    $tab->set_ajax(false);
+                }
+            }
+
+            $classes = $this->classes;
+            $classes = implode(" ", $classes);
+            if (strlen($classes) > 0) {
+                $classes = " " . $classes;
+            }
+            $classes .= ' ' . $ajax_class;
+
+            $widget_classes = $this->widget_class;
+            $widget_classes = implode(" ", $widget_classes);
+            if (strlen($widget_classes) > 0) {
+                $widget_classes = " " . $widget_classes;
+            }
+
+            $html->appendln('<div class="row-fluid ' . $classes . '" id="' . $this->id . '">');
+            $html->appendln('	<div class="span12">');
+
+            $html->appendln('		<div class="row-fluid">');
+            $html->appendln('			<div class="span2">');
+            $html->appendln('				<div class="side-nav-container affix-top">');
+            $html->appendln('					<ul id="' . $this->id . '-tab-nav" class="nav nav-tabs nav-stacked">');
+
+            $active_tab = null;
             foreach ($this->tabs as $tab) {
-                $html->appendln($tab->html($html->get_indent()));
+                if (strlen($this->active_tab) == 0) {
+                    $this->active_tab($tab->id);
+                }
+                if ($tab->id == $this->active_tab) {
+                    $tab->set_active(true);
+                    $active_tab = $tab;
+                }
+                $tab->set_target('' . $this->id . '-ajax-tab-content');
+
+                $html->appendln($tab->header_html($html->get_indent()));
             }
+            $active_tab_icon = "";
+            $active_tab_label = "";
+
+            if ($active_tab != null) {
+                $active_tab_icon = $active_tab->icon;
+                $active_tab_label = $active_tab->label;
+
+                if (strlen($active_tab->ajax_url) > 0) {
+                    $tab_ajax_url = $active_tab->ajax_url;
+
+                    /*
+                      $url_base = curl::base();
+                      if(substr($tab_ajax_url,0,strlen($url_base)) == $url_base) {
+                      $tab_ajax_url = substr($tab_ajax_url,strlen($url_base));
+                      }
+                      $tab_ajax_url = curl::base(true,'http').$tab_ajax_url;
+                     */
+                    //$active_tab_content = CCurl::factory($tab_ajax_url)->exec()->response();
+                }
+            }
+            $html->appendln('					</ul>');
+            $html->appendln('				</div>');
+            $html->appendln('			</div>');
+            $html->appendln('			<div class="span10">');
+            $html->appendln('				<div id="' . $this->id . '-tab-widget" class="widget-box nomargin widget-transaction-tab ' .$widget_classes .'">');
+            $html->appendln('					<div class="widget-title">');
+            if ($this->have_icon) {
+                $html->appendln('						<span class="icon">');
+                $html->appendln('							<i class="icon-' . $active_tab_icon . '"></i>');
+                $html->appendln('						</span>');
+            }
+            $html->appendln('						<h5>' . $active_tab_label . '</h5>');
+            $html->appendln('					</div>');
+
+            $html->appendln('					<div class="widget-content">');
+
+            if ($this->ajax) {
+                $html->appendln('						<div id="' . $this->id . '-ajax-tab-content">');
+                $html->appendln('						</div>');
+            }
+            else {
+                foreach ($this->tabs as $tab) {
+                    $html->appendln($tab->html($html->get_indent()));
+                }
+            }
+
+            $html->appendln('					</div>');
+            $html->appendln('				</div>');
+            $html->appendln('			</div>');
+            $html->appendln('		</div>');
+            $html->appendln('	</div>');
+            $html->appendln('</div>');
+
+            return $html->text();
         }
 
-        $html->appendln('					</div>');
-        $html->appendln('				</div>');
-        $html->appendln('			</div>');
-        $html->appendln('		</div>');
-        $html->appendln('	</div>');
-        $html->appendln('</div>');
+        public function js($indent = 0) {
 
-        return $html->text();
-    }
-
-    public function js($indent = 0) {
-
-        $js = new CStringBuilder();
-        $js->set_indent($indent);
-        foreach ($this->tabs as $tab) {
-            $js->appendln($tab->js($js->get_indent()));
-        }
-        $js->appendln("
+            $js = new CStringBuilder();
+            $js->set_indent($indent);
+            foreach ($this->tabs as $tab) {
+                $js->appendln($tab->js($js->get_indent()));
+            }
+            $js->appendln("
 			
 			jQuery('#" . $this->id . " #" . $this->id . "-tab-nav > li > a.tab-ajax-load').click(function(event) {
 				event.preventDefault();
@@ -221,14 +248,14 @@ class CTabList extends CElement {
 		");
 
 
-        $js->appendln("
+            $js->appendln("
 			jQuery('#" . $this->id . "').find('li.active a.tab-ajax-load').click();
 			
 		");
 
-        return $js->text();
-    }
+            return $js->text();
+        }
 
-}
+    }
 
 ?>
