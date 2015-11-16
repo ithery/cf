9d5033d1080ed81343841f1543f9f9f654f5b094
albert <albert@piposystem.com>
2015-11-16 14:39:41 +0700

    repair org_id

--- data/domain/62hallfamily.local.php
+++ data/domain/62hallfamily.local.php
@@ -2,8 +2,8 @@
 return array(
 	'app_id'=>'800',
 	'app_code'=>'62hallfamily',
-	'org_id'=>'3',
-	'org_code'=>'62hallfamily',
+	'org_id'=>NULL,
+	'org_code'=>NULL,
 	'store_id'=>NULL,
 	'store_code'=>NULL,
 	'domain'=>'62hallfamily.local',
--- data/domain/admin.62hallfamily.local.php
+++ data/domain/admin.62hallfamily.local.php
@@ -2,8 +2,8 @@
 return array(
 	'app_id'=>'890',
 	'app_code'=>'admin62hallfamily',
-	'org_id'=>'89',
-	'org_code'=>'admin62hallfamily',
+	'org_id'=>NULL,
+	'org_code'=>NULL,
 	'store_id'=>NULL,
 	'store_code'=>NULL,
 	'domain'=>'admin.62hallfamily.local',
