4d53eb171259471c04ef2586dffdf9d3d312ef94
hery <hery@ittron.co.id>
2016-01-08 23:42:09 +0700

    Update domain 62pay.local and notary.local

--- data/domain/62pay.local.php
+++ data/domain/62pay.local.php
@@ -2,8 +2,8 @@
 return array(
 	'app_id'=>'201',
 	'app_code'=>'torsapi',
-	'org_id'=>NULL,
-	'org_code'=>NULL,
+	'org_id'=>'62',
+	'org_code'=>'62hall',
 	'store_id'=>NULL,
 	'store_code'=>NULL,
 	'domain'=>'62pay.local',
