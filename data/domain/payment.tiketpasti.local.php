f7dd8b6a68b3b31cecb072c320e15ab87614bf98
hery <hery@ittron.co.id>
2016-03-11 12:13:19 +0700

    Add domain payment.tiketpasti.local

---
+++ data/domain/payment.tiketpasti.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'201',
+	'app_code'=>'torsapi',
+	'org_id'=>'114',
+	'org_code'=>'tiketpasti',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'payment.tiketpasti.local',
+);
