91d8ff72c8f173208c91ee7d248321acd4f65a23
hery <hery@ittron.co.id>
2016-03-21 14:39:31 +0700

    Update Control CFormInputSelectTag

--- modules/cresenity/libraries/CFormInputSelectTag.php
+++ modules/cresenity/libraries/CFormInputSelectTag.php
@@ -55,32 +55,28 @@ class CFormInputSelectTag extends CFormInput {
         $html = new CStringBuilder();
         $html->set_indent($indent);
         $html->append(parent::html($indent));
-        $custom_css = $this->custom_css;
-        $custom_css = crenderer::render_style($custom_css);
+
+        $custom_css = crenderer::render_style($this->custom_css);
         $multiple = ' multiple="multiple"';
         if (strlen($custom_css) > 0) {
             $custom_css = ' style="' . $custom_css . '"';
         }
 
-        $classes = $this->classes;
-        $classes = implode(" ", $classes);
-        if (strlen($classes) > 0)
+        $classes = implode(" ", $this->classes);
+        if (strlen($classes) > 0) {
             $classes = " " . $classes;
+        }
         if ($this->bootstrap == '3') {
             $classes = $classes . " form-control ";
         }
 
-        $html->appendln('<input type="hidden"  class="' . $classes . '" name="' . $this->name . '" id="' . $this->id . '" value="' . $this->value . '" ' . $custom_css . '/>')->br();
+        $html->appendln('<input type="hidden"  class="' . $classes . '" name="' . $this->name . '" id="' . $this->id . '" ' . $custom_css . '/>')->br();
 
         return $html->text();
     }
 
     public function js($indent = 0) {
 
-
-
-        $str_js_init = "";
-        
         $vals = $this->value;
         if(!is_array($vals)) {
             $vals = array($vals);
@@ -118,7 +114,7 @@ class CFormInputSelectTag extends CFormInput {
 
         if(strlen($vals_str)>0) {
              $str .= "
-                        $('#" . $this->id . "').select2('val',['test1','test2']);
+                        $('#" . $this->id . "').select2('val',[".$vals_str."]);
                 ";
         }
         
@@ -127,10 +123,6 @@ class CFormInputSelectTag extends CFormInput {
         $js->set_indent($indent);
         //echo $str;
         $js->append($str)->br();
-
-
-
-
 
         return $js->text();
     }
