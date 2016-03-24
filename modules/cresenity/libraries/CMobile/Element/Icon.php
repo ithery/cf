6ffb71e273e54dc3fec41f596954176bee3da7c6
eko <eko@piposystem.com>
2016-03-24 11:21:25 +0700

    add jscroll

--- modules/cresenity/libraries/CMobile/Element/Icon.php
+++ modules/cresenity/libraries/CMobile/Element/Icon.php
@@ -4,22 +4,22 @@ class CMobile_Element_Icon extends CMobile_Element {
 
     protected $icon;
     protected $type;
+
     public function __construct($id) {
         parent::__construct($id);
         $this->icon = "";
-		$this->tag = "i";
-		$this->type = "left";
+        $this->tag = "i";
+        $this->type = "left";
     }
 
     public static function factory($id = '') {
         return new CMobile_Element_Icon($id);
     }
-	
-	public function set_type($type) {
+
+    public function set_type($type) {
         $this->type = $type;
         return $this;
-		
-	}
+    }
 
     public function set_icon($ic) {
         $this->icon = $ic;
@@ -27,22 +27,17 @@ class CMobile_Element_Icon extends CMobile_Element {
     }
 
     protected function html_attr() {
-			
-		$this->add_class('material-icons');
-		$this->add_class($this->type);
-		
-		return parent::html_attr();
-		
-		
-	}
-		
-	public function html($indent = 0) {
-		$this->add($this->icon);
-		return parent::html($indent);
-		
-	}
 
-    
+        $this->add_class('material-icons');
+        $this->add_class($this->type);
+
+        return parent::html_attr();
+    }
+
+    public function html($indent = 0) {
+        $this->add($this->icon);
+        return parent::html($indent);
+    }
 
 }
 
