a318cbb74bf5ee8f8b52e8d0f020f2950aeaacf0
joko <joko@pippo-solutindo.com>
2016-04-11 14:04:33 +0700

    update

--- data/domain/admin.bioto.local.php
+++ data/domain/admin.bioto.local.php
@@ -2,9 +2,10 @@
 return array(
 	'app_id'=>'900',
 	'app_code'=>'adminbioto',
-	'org_id'=>NULL,
-	'org_code'=>NULL,
+	'org_id'=>900002,
+	'org_code'=>'bioto',
 	'store_id'=>NULL,
 	'store_code'=>NULL,
 	'domain'=>'admin.bioto.local',
+	'shared_app_code'=>array('biotoshare'),
 );
--- data/domain/bioto.local.php
+++ data/domain/bioto.local.php
@@ -2,9 +2,10 @@
 return array(
 	'app_id'=>'901',
 	'app_code'=>'bioto',
-	'org_id'=>NULL,
-	'org_code'=>NULL,
+	'org_id'=>900002,
+	'org_code'=>'bioto',
 	'store_id'=>NULL,
 	'store_code'=>NULL,
 	'domain'=>'bioto.local',
+	'shared_app_code'=>array('biotoshare'),
 );
