114f0e254da54b555d59cd2ee7e8b5cd0a91f9c1
isman <isman@ittron-indonesia.com>
2016-04-27 13:25:41 +0700

    bioto image

--- data/domain/bioto.local.php
+++ data/domain/bioto.local.php
@@ -3,8 +3,9 @@ return array(
 	'app_id'=>'901',
 	'app_code'=>'bioto',
 	'org_id'=>900002,
-	'org_code'=>NULL,
+	'org_code'=>'bioto',
 	'store_id'=>NULL,
 	'store_code'=>NULL,
 	'domain'=>'bioto.local',
+	'shared_app_code'=>array('biotoshare'),
 );
