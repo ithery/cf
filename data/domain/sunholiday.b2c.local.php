0d3af52acfb784a0039f97400a2e3a6137bd9d93
isman <isman@ittron-indonesia.com>
2016-04-28 15:04:58 +0700

    timepicker bug

---
+++ data/domain/sunholiday.b2c.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'404',
+	'app_code'=>'torsb2c',
+	'org_id'=>17,
+	'org_code'=>'sunholiday',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'sunholiday.b2c.local',
+);
