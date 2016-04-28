8b722767252c7ffeb6e1f4ee94f86c970ab2fe0f
isman <isman@ittron-indonesia.com>
2016-04-28 16:39:04 +0700

    cms sun holiday

---
+++ data/domain/cmssunholiday.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'405',
+	'app_code'=>'cmstorsb2c',
+	'org_id'=>16,
+	'org_code'=>'sunholiday',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'cmssunholiday.local',
+);
