4c786e5804d42e78c6b2941ab60c18957295f665
septian <septian@pippo-solutindo.com>
2016-06-03 17:57:27 +0700

    ittronstore

---
+++ data/domain/adminittronstore.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'1000',
+	'app_code'=>'adminittronstore',
+	'org_id'=>NULL,
+	'org_code'=>NULL,
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'admin.ittronstore.local',
+);
