743fc80151745ad405df8a813c02db508bf5175c
eko <eko@piposystem.com>
2016-06-10 17:02:02 +0700

    admin.namecard.local

---
+++ data/app/adminnamecard.php
@@ -0,0 +1,6 @@
+<?php 
+return array(
+	'app_id'=>'1090',
+	'code'=>'adminnamecard',
+	'name'=>'Admin Name Card',
+);
--- data/domain/admin.namecard.local.php
+++ data/domain/admin.namecard.local.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-	"app_id"=>'0',
-	"app_code"=>"admin",
+	"app_id"=>'1090',
+	"app_code"=>"adminnamecard",
 	"org_id"=>null,
 	"org_code"=>null,
 	"store_id"=>null,
