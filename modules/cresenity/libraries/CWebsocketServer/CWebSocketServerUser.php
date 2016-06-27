63d60edad86aba7cf0ed8e559e50ef66d0afdff8
Hery <hery@ittron.co.id>
2016-06-27 09:02:05 +0700

    websocket server

--- modules/cresenity/libraries/CWebsocketServer/CWebSocketServerUser.php
+++ modules/cresenity/libraries/CWebsocketServer/CWebSocketServerUser.php
@@ -1,15 +1,20 @@
 <?php
-class WebSocket {
+
+class CWebSocketServerUser {
+
   public $socket;
   public $id;
   public $headers = array();
   public $handshake = false;
+
   public $handlingPartialPacket = false;
   public $partialBuffer = "";
+
   public $sendingContinuous = false;
   public $partialMessage = "";
   
   public $hasSentClose = false;
+
   function __construct($id, $socket) {
     $this->id = $id;
     $this->socket = $socket;
