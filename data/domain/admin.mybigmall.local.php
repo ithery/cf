549d9fbb4a6bf76e74b2c3e8dfaa2f1a5efbeccd
albert <albert@piposystem.com>
2016-08-22 08:37:44 +0700

    change folder

--- data/domain/admin.mybigmall.local.php
+++ data/domain/admin.mybigmall.local.php
@@ -3,7 +3,7 @@ return array(
 	'app_id'=>'890',
 	'app_code'=>'admin62hallfamily',
 	'org_id'=>'620017',
-	'org_code'=>'mybigmall',
+	'org_code'=>'mybigmall.co',
 	'store_id'=>NULL,
 	'store_code'=>NULL,
 	'domain'=>'admin.mybigmall.local',
