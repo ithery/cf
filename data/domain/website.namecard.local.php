75a5022177c35ba1abc04a74424affd8651c3d6d
eko <eko@piposystem.com>
2016-08-23 16:33:49 +0700

    mobile.mybigmall.local domain

---
+++ data/domain/website.namecard.local.php
@@ -0,0 +1,14 @@
+<?php 
+return array(
+	'app_id'=>'888',
+	'app_code'=>'namecard',
+	'org_id'=>1,
+	'org_code'=>NULL,
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'namecard.local',
+        'shared_app_code'=>array(
+            "adminnamecard"
+        ),
+        'theme' => 'materialize',
+);
