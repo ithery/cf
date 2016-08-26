54ae9e71526312135ee70f3d64b129927631cefc
albert <albert@piposystem.com>
2016-08-26 15:06:20 +0700

    change code

--- data/domain/mybigmall.local.php
+++ data/domain/mybigmall.local.php
@@ -3,7 +3,7 @@ return array(
 	'app_id'=>'800',
 	'app_code'=>'62hallfamily',
 	'org_id'=>'620017',
-	'org_code'=>'mybigmall',
+	'org_code'=>'mybigmall.co',
 	'store_id'=>NULL,
 	'store_code'=>NULL,
 	'domain'=>'mybigmall.local',
