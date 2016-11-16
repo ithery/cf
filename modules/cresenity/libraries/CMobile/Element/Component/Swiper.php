dd2faf8385d8b2e6dd80906cef22942e66d35e82
hery <hery@ittron.co.id>
2016-11-16 16:41:30 +0700

    update swiper for jquery selector

--- modules/cresenity/libraries/CMobile/Element/Component/Swiper.php
+++ modules/cresenity/libraries/CMobile/Element/Component/Swiper.php
@@ -4,42 +4,43 @@ defined('SYSPATH') OR die('No direct access allowed.');
 
 class CMobile_Element_Component_Swiper extends CMobile_Element_AbstractComponent {
 
-	protected $wrapper;
-	protected $manual_init;
+    protected $wrapper;
+    protected $manual_init;
+
     public function __construct($id = "") {
 
         parent::__construct($id);
         $this->tag = "div";
         $this->manual_init = false;
-		$this->add_div($this->id."_pagination")->add_class('swiper-pagination');
-		$this->wrapper = $this->add_div($this->id."_wrapper")->add_class('swiper-wrapper');
+        $this->add_div($this->id . "_pagination")->add_class('swiper-pagination');
+        $this->wrapper = $this->add_div($this->id . "_wrapper")->add_class('swiper-wrapper');
     }
 
     public static function factory($id = "") {
         return new CMobile_Element_Component_Swiper($id);
     }
 
-    public function add_item($id="") {
-		$element = CMobile_Element_Component_Swiper_Item::factory($id);
-		$this->wrapper->add($element);
-		return $element;
-	}
-	public function manual_init($value) {
-		$this->manual_init = $value;
-		return $this;
-	}
+    public function add_item($id = "") {
+        $element = CMobile_Element_Component_Swiper_Item::factory($id);
+        $this->wrapper->add($element);
+        return $element;
+    }
 
+    public function manual_init($value) {
+        $this->manual_init = $value;
+        return $this;
+    }
 
-	public function build() {
-		$this->add_class('swiper-container');
-	}
+    public function build() {
+        $this->add_class('swiper-container');
+    }
 
-
-	public function js($indent=0) {
-		$js = "";
-		if(!$this->manual_init) {
-			$js .= "
-	        var swiper = new Swiper('.swiper-container', {
+    public function js($indent = 0) {
+        $js = "";
+        if (!$this->manual_init) {
+            $js .= "
+			
+	        var swiper_" . $this->id . " = new Swiper('#" . $this->id . "', {
 		        pagination: '.swiper-pagination',
 		        effect:'slide',
 		        paginationHide:false,
@@ -50,8 +51,9 @@ class CMobile_Element_Component_Swiper extends CMobile_Element_AbstractComponent
 		        spaceBetween: 1,
 	    	});
 			";
-		}
-		$js .= parent::js();
-		return $js;
-	}
+        }
+        $js .= parent::js();
+        return $js;
+    }
+
 }
