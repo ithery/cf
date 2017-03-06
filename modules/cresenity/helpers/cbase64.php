387763390afb01ef0cbc3bf2d0ef549e1c332b98
hery <hery@ittron.co.id>
2017-03-06 09:26:42 +0700

    reformatting

--- modules/cresenity/helpers/cbase64.php
+++ modules/cresenity/helpers/cbase64.php
@@ -1,24 +1,27 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
 /**
  * base64 helper class.
  */
 class cbase64 {
-	
-	public static function encode($data){
-		return base64_encode($data);
-	}
-	
-	
-	public static function decode( $data ) {
-		return base64_decode($data);
-	}
-	
-	function is_encoded() {
-		if (preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $data)) {
-			return TRUE;
-		} 
-		return FALSE;
-	}
 
-	
-} // End cbase64
+    public static function encode($data) {
+        return base64_encode($data);
+    }
+
+    public static function decode($data) {
+        return base64_decode($data);
+    }
+
+    function is_encoded() {
+        if (preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $data)) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+}
+
+// End cbase64
--- modules/cresenity/helpers/cdate.php
+++ modules/cresenity/helpers/cdate.php
@@ -1,23 +1,23 @@
 <?php
 
-    /**
-     *
-     * @author Raymond Sugiarto
-     * @since  Dec 12, 2014
-     * @license http://piposystem.com Piposystem
-     */
+/**
+ *
+ * @author Raymond Sugiarto
+ * @since  Dec 12, 2014
+ * @license http://piposystem.com Piposystem
+ */
+class cdate {
 
-    class cdate {
-        
-        public static function age($birthday,$return_year = true){
+    public static function age($birthday, $return_year = true) {
 
-            $diff_year = (date("md",  strtotime($birthday)) > date("md") ? (date("Y")-date("Y",strtotime($birthday))-1):(date("Y")-date("Y",strtotime($birthday))));
-            $diff_month = (date("d",  strtotime($birthday)) > date("d") ? (date("m")-date("m",strtotime($birthday))-1):(date("m")-date("m",strtotime($birthday))));
-            $result = $diff_year;
-            if($return_year !== true) {
-                $result = ($diff_year*12)+$diff_month;
-            echo $birthday ."::" .$result ."===";
-            }
-            return $result;
+        $diff_year = (date("md", strtotime($birthday)) > date("md") ? (date("Y") - date("Y", strtotime($birthday)) - 1) : (date("Y") - date("Y", strtotime($birthday))));
+        $diff_month = (date("d", strtotime($birthday)) > date("d") ? (date("m") - date("m", strtotime($birthday)) - 1) : (date("m") - date("m", strtotime($birthday))));
+        $result = $diff_year;
+        if ($return_year !== true) {
+            $result = ($diff_year * 12) + $diff_month;
+            echo $birthday . "::" . $result . "===";
         }
+        return $result;
     }
+
+}
