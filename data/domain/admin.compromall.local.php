0f31ad80d30b5dc90f21f3a793b4c1bb7809b7da
ali <ali@ittron-indonesia.com>
2017-03-30 10:27:46 +0700

    update

---
+++ data/domain/admin.compromall.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'891',
+	'app_code'=>'adminittronmall',
+	'org_id'=>'620562',
+	'org_code'=>'compromall',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'admin.compromall.local',
+);
