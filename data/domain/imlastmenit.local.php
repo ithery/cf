320fd8acf557b6addf7c26ec2a4ffbd5f4dd4d0d
imam <imam@ittron-indonesia.com>
2017-05-22 09:18:28 +0700

    imlastmenit

---
+++ data/domain/imlastmenit.local.php
@@ -0,0 +1,12 @@
+<?php 
+return array(
+	'app_id'=>'892',
+	'app_code'=>'ittronmall',
+	'org_id'=>'620586',
+	'org_code'=>'imlastmenit',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'imlastmenit.local',
+	
+	'theme'=>'imlastmenit',
+);
---
+++ data/org/imlastmenit.php
@@ -0,0 +1,6 @@
+<?php 
+return array(
+	'org_id'=>'620586',
+	'org_code'=>'imlastmenit',
+	'org_name'=>'Last Menit',
+);
