6ac94767546a6538032976c8c837e609c411bbcb
imam <imam@ittron-indonesia.com>
2017-05-22 09:19:44 +0700

    admin.imlastmenit

---
+++ data/domain/admin.imlastmenit.local.php
@@ -0,0 +1,11 @@
+<?php 
+return array(
+	'app_id'=>'891',
+	'app_code'=>'adminittronmall',
+	'org_id'=>'620586',
+	'org_code'=>'imlastmenit',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'admin.imlastmenit.local',
+	'theme'=>'imlastmenit',
+);
