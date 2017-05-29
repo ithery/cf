07cdf0a18824e7ba302d8578eeedc93439d60677
iema <iema@ittron-indonesia.com>
2017-05-29 08:49:39 +0700

    upload

---
+++ data/domain/spread.local.php
@@ -0,0 +1,10 @@
+<?php 
+return array(
+	'app_id'=>'800',
+	'app_code'=>'spread',
+	'org_id'=>'620028',
+	'org_code'=>'ittronmall',
+	'store_id'=>NULL,
+	'store_code'=>NULL,
+	'domain'=>'spread.local',
+);
