aa7b022a501d4503aaf384ee856f6f1c9bd17f8a
Hery <hery.cresenity@gmail.com>
2017-10-07 09:04:12 +0700

    fix CTable don't query when ajax

--- modules/cresenity/controllers/documentation/icon.php
+++ modules/cresenity/controllers/documentation/icon.php
@@ -7,9 +7,9 @@ class Controller_Documentation_Icon extends CController {
     public function index() {
         $app = CApp::instance();
         $app->title(clang::__("Fontawesome Icon List"));
-        $html = View::factory('documentation/icon_list/fontawesome/html');
+        $html = CView::factory('documentation/icon_list/fontawesome/html');
         $html = $html->render();
-        $js = View::factory('documentation/icon_list/fontawesome/js');
+        $js = CView::factory('documentation/icon_list/fontawesome/js');
         $js = $js->render();
         $app->add($html);
         $app->add_js($js);
