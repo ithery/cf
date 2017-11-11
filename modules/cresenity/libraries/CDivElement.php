ba5bb6960053b7b8ec892303253c8118a8528ca5
Hery <hery.cresenity@gmail.com>
2017-11-12 03:20:36 +0700

    update CElement create default html and js + PseudoElement

--- modules/cresenity/libraries/CDivElement.php
+++ modules/cresenity/libraries/CDivElement.php
@@ -2,46 +2,10 @@
 
 defined('SYSPATH') OR die('No direct access allowed.');
 
-class CDivElement extends CElement {
+class CDivElement extends CElement_Div {
 
-    public function __construct($id = "", $tag = "div") {
-
-        parent::__construct($id,$tag);
-        $this->tag = "div";
-    }
-
-    public static function factory($id = "") {
-        return new CDivElement($id);
-    }
-
-    public function html($indent = 0) {
-        $html = new CStringBuilder();
-        $html->set_indent($indent);
-        $disabled = "";
-        $classes = $this->classes;
-        $classes = implode(" ", $classes);
-        if (strlen($classes) > 0)
-            $classes = " " . $classes;
-        $custom_css = $this->custom_css;
-        $custom_css = crenderer::render_style($custom_css);
-        if (strlen($custom_css) > 0) {
-            $custom_css = ' style="' . $custom_css . '"';
-        }
-        $addition_attribute = "";
-        foreach ($this->attr as $k => $v) {
-            $addition_attribute.=" " . $k . '="' . $v . '"';
-        }
-        $html->appendln('<div id="' . $this->id . '" class="' . $classes . '"' . $custom_css . $addition_attribute . '>');
-
-
-        $html->appendln(parent::html($html->get_indent()))->br();
-        $html->appendln('</div>');
-
-        return $html->text();
-    }
-
-    public function js($indent = 0) {
-        return parent::js($indent);
-    }
+    
+    
+    
 
 }
--- modules/cresenity/libraries/CElement.php
+++ modules/cresenity/libraries/CElement.php
@@ -16,11 +16,10 @@ abstract class CElement extends CObservable {
     protected $theme_style = array();
     protected $client_modules = array();
     protected $theme_data = array();
-    
     protected $before;
     protected $after;
     protected $is_builded = false;
-    protected $is_empty = false;
+    protected $is_onetag = false;
     private $is_build = false;
 
     public static function valid_tag() {
@@ -34,12 +33,23 @@ abstract class CElement extends CObservable {
         $this->attr = array();
         $this->custom_css = array();
         $this->text = '';
+
         $this->tag = $tag;
+        $this->theme = ctheme::get_current_theme();
+
+        $this->before = null;
+        $this->after = null;
+
+        $this->is_builded = false;
+        $this->is_onetag = false;
+
+
+
         $this->bootstrap = ccfg::get('bootstrap');
         if (strlen($this->bootstrap) == 0) {
             $this->bootstrap = '2';
         }
-        $this->theme = ccfg::get('theme');
+
         $theme_data = CManager::instance()->get_theme_data();
         $this->theme_data = $theme_data;
         if (isset($theme_data)) {
@@ -110,9 +120,14 @@ abstract class CElement extends CObservable {
         return null;
     }
 
+    public function onetag() {
+
+        return '<' . $this->tag . ' ' . $this->html_attr() . ' />';
+    }
+
     public function pretag() {
 
-        return '<' . $this->tag . '>';
+        return '<' . $this->tag . ' ' . $this->html_attr() . ' >';
     }
 
     public function posttag() {
@@ -126,6 +141,25 @@ abstract class CElement extends CObservable {
             $classes = " " . $classes;
         }
         return $classes;
+    }
+
+    protected function html_attr() {
+        $classes = $this->classes;
+        $classes = implode(" ", $classes);
+
+
+        $custom_css = $this->custom_css;
+        $custom_css = crenderer::render_style($custom_css);
+        if (strlen($custom_css) > 0) {
+            $custom_css = ' style="' . $custom_css . '"';
+        }
+        $addition_attribute = "";
+        foreach ($this->attr as $k => $v) {
+            $addition_attribute .= " " . $k . '="' . $v . '"';
+        }
+        $class_attr = ' class="' . $classes . '"';
+        $html_attr = 'id="' . $this->id . '" ' . $class_attr . $custom_css . $addition_attribute;
+        return $html_attr;
     }
 
     public static function is_instanceof($val) {
@@ -147,6 +181,88 @@ abstract class CElement extends CObservable {
         }
         $data = array_merge_recursive($data, parent::toarray());
         return $data;
+    }
+
+    protected function build_once() {
+        //just build once
+        if (!$this->is_builded) {
+            $this->build();
+            $this->is_builded = true;
+        }
+    }
+
+    protected function html_child($indent = 0) {
+        return parent::html($indent);
+    }
+
+    protected function js_child($indent = 0) {
+        return parent::js($indent);
+    }
+
+    public function before_html($indent = 0) {
+        return $this->before()->html($indent);
+    }
+
+    public function after_html($indent = 0) {
+        return $this->after()->html($indent);
+    }
+
+    public function before_js($indent = 0) {
+        return $this->before()->js($indent);
+    }
+
+    public function after_js($indent = 0) {
+        return $this->after()->js($indent);
+    }
+
+    public function before() {
+        if ($this->before == null) {
+            $this->before = CElement_PseudoElement::factory();
+        }
+        return $this->before;
+    }
+
+    public function after() {
+        if ($this->after == null) {
+            $this->after = CElement_PseudoElement::factory();
+        }
+        return $this->after;
+    }
+
+    protected function build() {
+        
+    }
+
+    public function html($indent = 0) {
+        $html = new CStringBuilder();
+
+        $html->set_indent($indent);
+        $this->build_once();
+        $html->appendln($this->before_html($indent));
+        if ($this->is_onetag) {
+            $html->appendln($this->onetag());
+        } else {
+            $html->appendln($this->pretag())->br();
+            $html->inc_indent();
+            $html->appendln($this->html_child($html->get_indent()))->br();
+            $html->dec_indent();
+            $html->appendln($this->posttag())->br();
+        }
+        $html->appendln($this->after_html($indent));
+
+        return $html->text();
+    }
+
+    public function js($indent = 0) {
+        $js = new CStringBuilder();
+        $js->set_indent($indent);
+        $this->build_once();
+
+        $js->appendln($this->before_js($js->get_indent()));
+        $js->appendln($this->js_child($js->get_indent()))->br();
+        $js->appendln($this->after_js($js->get_indent()));
+
+        return $js->text();
     }
 
     public function __to_string() {
