a9ff8cc118c324f1192cad1d21f03f06271f4bdc
Hery <hery.cresenity@gmail.com>
2017-12-09 21:08:17 +0700

    fix pass to elastic when mapping column is date type

--- modules/cresenity/libraries/CElastic/Document.php
+++ modules/cresenity/libraries/CElastic/Document.php
@@ -13,16 +13,24 @@ class CElastic_Document {
     protected $document_type;
 
     /*
-     * @var Elasticsearch\Client
+     * @var CElastic
      */
     protected $elastic;
+    /*
+     * @var Elasticsearch\Client
+     */
     protected $client;
+    /*
+     * @var CElastic_Indices
+     */
+    protected $indices;
 
     public function __construct(CElastic $elastic, $index, $document_type) {
         $this->elastic = $elastic;
         $this->client = $elastic->client();
         $this->index = $index;
         $this->document_type = $document_type;
+        $this->indices = $this->elastic->indices($this->index, $this->document_type);
     }
 
     public function bulk($query, $index_field_name = "") {
@@ -37,14 +45,19 @@ class CElastic_Document {
         $results = $db->query($query);
         $list_field = $results->list_fields();
 
+        $mapping = $this->indices->get_mapping();
+
+        $properties = carr::path($mapping, $this->index . '.mappings.' . $this->document_type . '.properties');
+
+        $result = array();
         foreach ($results as $key => $value) {
             $row = (array) $value;
 
             $id = $row[$list_field[0]];
-            if(strlen($index_field_name) > 0) {
+            if (strlen($index_field_name) > 0) {
                 $id = $row[$index_field_name];
             }
-            
+
             $params['body'][] = [
                 'index' => [
                     '_index' => $this->index,
@@ -52,6 +65,14 @@ class CElastic_Document {
                     '_id' => $id
                 ]
             ];
+
+
+            foreach ($row as $key => $val) {
+                //we transform the mapping date to elastic date format
+                if (carr::path($properties, $key . '.type') == 'date') {
+                    $row[$key] = date_format(date_create($val), "Y-m-d") . "T" . date_format(date_create($val), "H:i:s") . ".000Z";
+                }
+            }
 
             $params['body'][] = $row;
 
@@ -121,6 +142,7 @@ class CElastic_Document {
                 throw new Exception('Elastic Error:' . $error_message);
             }
         }
+        return $result;
     }
 
     public function delete($id) {
@@ -131,8 +153,9 @@ class CElastic_Document {
 
         try {
             $this->client->delete($params);
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             throw new Exception($this->index . " - Delete Document Failed", 1);
         }
     }
+
 }
