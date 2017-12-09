816752c253043a69f09e2c5403208077ff741a05
Hery <hery.cresenity@gmail.com>
2017-12-09 19:59:53 +0700

    update element basic span

--- modules/cresenity/libraries/CBasicSpan.php
+++ modules/cresenity/libraries/CBasicSpan.php
@@ -2,32 +2,10 @@
 
 defined('SYSPATH') OR die('No direct access allowed.');
 
-class CBasicSpan extends CObservable {
-
-    public function __construct($id = "") {
-
-        parent::__construct($id);
-    }
+class CBasicSpan extends CElement_Element_Span {
 
     public static function factory($id = "") {
         return new CBasicSpan($id);
-    }
-
-    public function html($indent = 0) {
-        $html = new CStringBuilder();
-        $html->set_indent($indent);
-        $disabled = "";
-        $html->appendln('<span>');
-
-
-        $html->appendln(parent::html($html->get_indent()))->br();
-        $html->appendln('</span>');
-
-        return $html->text();
-    }
-
-    public function js($indent = 0) {
-        return parent::js($indent);
     }
 
 }
