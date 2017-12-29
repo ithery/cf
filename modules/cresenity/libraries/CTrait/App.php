891998ae7d4eb671e700f3a715b57d4e9ef48f11
Hery <hery.cresenity@gmail.com>
2017-12-30 03:18:26 +0700

    add trait App

---
+++ modules/cresenity/libraries/CTrait/App.php
@@ -0,0 +1,81 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Dec 30, 2017, 3:11:24 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+class CTrait_App {
+
+    private static $org = null;
+
+    /**
+     * 
+     * @return int
+     */
+    public static function org_id() {
+        $org_id = CF::org_id();
+        $app = CApp::instance();
+        if ($app->user() != null) {
+            if (strlen($app->user()->org_id) > 0) {
+                $org_id = $app->user()->org_id;
+            }
+        }
+        return $org_id;
+    }
+
+    /**
+     * 
+     * @param int $org_id optional, default using return values of SM::org_id()
+     * @return string Code of org
+     */
+    public static function org_code($org_id = null) {
+        $org = self::org($org_id);
+        return cobj::get($org, 'code');
+    }
+
+    /**
+     * 
+     * @param int $org_id optional, default using return values of SM::org_id()
+     * @return string Name of org
+     */
+    public static function org_name($org_id = null) {
+        $org = self::org($org_id);
+        return cobj::get($org, 'name');
+    }
+
+    /**
+     * 
+     * @param int $org_id optional, default using return values of SM::org_id()
+     * @return stdClass of org
+     */
+    public static function org($org_id = null) {
+        $db = CDatabase::instance();
+
+        if ($org_id == null) {
+            $org_id = self::org_id();
+        }
+        if (self::$org == null) {
+            self::$org = array();
+        }
+        if (!isset(self::$org[$org_id])) {
+            self::$org[$org_id] = cdbutils::get_row('select * from org where org_id = ' . $db->escape($org_id));
+        }
+        return self::$org[$org_id];
+    }
+
+   
+
+    /**
+     * 
+     * @return string value of current theme
+     */
+    public static function theme() {
+        $theme = CF::theme();
+
+        return $theme;
+    }
+
+}
