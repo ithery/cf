853c17f3eb6198030afa6ef6fe5457e555c891f5
Hery <hery.cresenity@gmail.com>
2018-02-16 02:24:47 +0700

    update icon documentation

--- modules/cresenity/controllers/documentation.php
+++ modules/cresenity/controllers/documentation.php
@@ -2,21 +2,21 @@
 
 defined('SYSPATH') OR die('No direct access allowed.');
 
-class Documentation_Controller extends CController {
+class Controller_Documentation extends CController {
 
     public function index() {
 
         $app = CApp::instance();
 
         $app->title(clang::__("CAPP Documentation"));
-		
-		$tabs = $app->add_tab_list();
-		$tabs->add_tab('documentation-object')->set_label('Object')->set_ajax_url(curl::base().'documentation/object');
-		$tabs->add_tab('documentation-database')->set_label('Database')->set_ajax_url(curl::base().'documentation/database');
-		$tabs->add_tab('documentation-icon')->set_label('Icon')->set_ajax_url(curl::base().'documentation/icon');
-		$tabs->add_tab('documentation-ftp')->set_label('FTP')->set_ajax_url(curl::base().'documentation/ftp');
-		
-		
+
+        $tabs = $app->add_tab_list();
+        $tabs->add_tab('documentation-object')->set_label('Object')->set_ajax_url(curl::base() . 'documentation/object');
+        $tabs->add_tab('documentation-database')->set_label('Database')->set_ajax_url(curl::base() . 'documentation/database');
+        $tabs->add_tab('documentation-icon')->set_label('Icon')->set_ajax_url(curl::base() . 'documentation/icon');
+        $tabs->add_tab('documentation-ftp')->set_label('FTP')->set_ajax_url(curl::base() . 'documentation/ftp');
+
+
 
         echo $app->render();
     }
