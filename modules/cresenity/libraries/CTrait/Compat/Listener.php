78fa7c923c66da2c3e3e42986c3a2bbbd49d91e2
Hery <hery.cresenity@gmail.com>
2018-05-12 20:18:41 +0700

    add Empty Trait

---
+++ modules/cresenity/libraries/CTrait/Compat/Listener.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Feb 16, 2018, 6:51:44 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+trait CTrait_Compat_Listener {
+    
+}
---
+++ modules/cresenity/libraries/CTrait/Compat/Object.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Feb 17, 2018, 3:42:16 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+trait CTrait_Compat_Object {
+
+
+}
