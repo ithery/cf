ec4d0e81776aa3e37890bbc24d0ed7bfdb42755c
hery <hery@ittron.co.id>
2018-06-04 18:38:03 +0700

    add trait Compat ClientModules

---
+++ modules/cresenity/libraries/CTrait/Compat/ClientModules.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Jun 3, 2018, 4:28:05 PM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+trait CTrait_Compat_ClientModules {
+    
+}
