0c87a4eb08e1c9c654fdb6c2554397d1f368f486
hery <hery@ittron.co.id>
2018-06-06 12:18:33 +0700

    fix php doc

--- modules/cresenity/libraries/CActionList.php
+++ modules/cresenity/libraries/CActionList.php
@@ -1,5 +1,14 @@
 <?php
 
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * 
+ * @deprecated this class is deprecated
+ * @author Hery Kurniawan
+ * @since Nov 12, 2017, 3:32:00 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
 class CActionList extends CElement_List_ActionList {
     
 }
