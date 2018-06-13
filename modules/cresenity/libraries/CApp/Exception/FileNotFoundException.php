5da53284086f27bac765a5d238e4feae0ef00787
Hery <hery.cresenity@gmail.com>
2018-06-14 05:30:07 +0700

    add helper filesystem

---
+++ modules/cresenity/libraries/CApp/Exception/FileNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Feb 16, 2018, 10:42:57 PM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+class CApp_Exception_FileNotFoundException extends CApp_Exception {
+    //
+}
