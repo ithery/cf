8d6d569f01945d7e2c4ccb33be440ff721bd7bbd
Hery <hery.cresenity@gmail.com>
2018-06-23 01:31:01 +0700

    add Org Model

--- modules/cresenity/libraries/CApp/Model/Org.php
+++ modules/cresenity/libraries/CApp/Model/Org.php
@@ -4,10 +4,10 @@ defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Jun 19, 2018, 3:37:54 AM
+ * @since Jun 23, 2018, 1:30:11 AM
  * @license Ittron Global Teknologi <ittron.co.id>
  */
-class CApp_Model_Roles extends CApp_Model {
+class CApp_Model_Org extends CApp_Model {
 
-    use CApp_Model_Trait_Users;
+    use CApp_Model_Trait_Org;
 }
--- modules/cresenity/libraries/CApp/Model/Trait/Org.php
+++ modules/cresenity/libraries/CApp/Model/Trait/Org.php
@@ -4,20 +4,16 @@ defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Jun 19, 2018, 11:36:57 PM
+ * @since Jun 23, 2018, 1:29:45 AM
  * @license Ittron Global Teknologi <ittron.co.id>
  */
-trait CApp_Model_Trait_Roles {
+trait CApp_Model_Trait_Org {
 
     public function __construct(array $attributes = array()) {
         parent::__construct($attributes);
-        $this->primaryKey = 'role_id';
-        $this->table = 'roles';
-        $this->guarded = array('role_id');
-    }
-
-    public function org() {
-        return $this->belongsTo('CMModel_Org');
+        $this->primaryKey = 'org_id';
+        $this->table = 'org';
+        $this->guarded = array('org_id');
     }
 
 }
