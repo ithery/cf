f97ef774807a43956f97b0230f3155a3e3824e66
Hery <hery.cresenity@gmail.com>
2018-06-23 01:37:16 +0700

    add model roleNav and rolePermission

---
+++ modules/cresenity/libraries/CApp/Model/Trait/RolePermission.php
@@ -0,0 +1,35 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Jun 23, 2018, 1:34:29 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+trait CApp_Model_Trait_RoleNav {
+
+    public function __construct(array $attributes = array()) {
+        parent::__construct($attributes);
+        $this->primaryKey = 'role_nav_id';
+        $this->table = 'role_nav';
+        $this->guarded = array('role_nav_id');
+    }
+
+    /**
+     * 
+     * @return CModel_Relation_BelongsTo
+     */
+    public function org() {
+        return $this->belongsTo('CMModel_Org');
+    }
+
+    /**
+     * 
+     * @return CModel_Relation_BelongsTo
+     */
+    public function roles() {
+        return $this->belongsTo('CMModel_Roles');
+    }
+
+}
