877a6c5b61bc2f9f8bfbccca770d601e084c03ba
hery <hery@ittron.co.id>
2018-07-13 15:25:04 +0700

    update css cropper for bs4 update css bs4 add CApp Data

--- modules/cresenity/templates/CElement/Helper/Cropper.php
+++ modules/cresenity/templates/CElement/Helper/Cropper.php
@@ -8,24 +8,25 @@
 <div id="modal-cropper-<?php echo $id ?>" class="modal " >
     <div class="modal-dialog"><div class="modal-content animated bounceInRight">
             <div class="modal-header">
-                <div class="btn-group btn-group-header-modal">
-                            <button type="button" class="btn btn-primary btn-zoom-in" data-method="zoom" data-option="0.1" title="Zoom In">
-                                <span class="docs-tooltip" data-toggle="tooltip" title="cropper('zoom', 0.1)">
-                                    <span class="fa fa-search-plus"></span> 
-                                </span>
-                            </button>
-                            
-                            <button type="button" class="btn btn-primary btn-zoom-out" data-method="zoom" data-option="-0.1" title="Zoom Out">
-                                <span class="docs-tooltip" data-toggle="tooltip" title="cropper('zoom', -0.1)">
-                                    <span class="fa fa-search-minus"></span> 
-                                </span>
-                            </button>
-                            
-                           
-                        </div>
-                
-                <a href="#" class="close">&times;</a><span class="loader"></span>
+
                 <h3>Cropper</h3>
+
+                <div class="btn-group btn-group-header-modal">
+                    <button type="button" class="btn btn-primary btn-zoom-in" data-method="zoom" data-option="0.1" title="Zoom In">
+                        <span class="docs-tooltip" data-toggle="tooltip" title="cropper('zoom', 0.1)">
+                            <span class="fa fa-search-plus"></span> 
+                        </span>
+                    </button>
+
+                    <button type="button" class="btn btn-primary btn-zoom-out" data-method="zoom" data-option="-0.1" title="Zoom Out">
+                        <span class="docs-tooltip" data-toggle="tooltip" title="cropper('zoom', -0.1)">
+                            <span class="fa fa-search-minus"></span> 
+                        </span>
+                    </button>
+
+
+                </div>
+                <a href="#" class="close">&times;</a><span class="loader"></span>
             </div>
             <div class="modal-body opened">
                 <div class="cropper-image-container">
@@ -33,10 +34,10 @@
                 </div>
             </div>
             <div class="modal-footer">
-                 
-                
+
+
                 <div class="row" id="actions">
-                    
+
                     <div class="col-md-12 docs-buttons">
                         <!--
                         <div class="btn-group">
@@ -52,9 +53,9 @@
                             </button>
                         </div>
                         -->
-                       
+
                         <div class="btn-group">
-                           
+
                             <button type="button" class="btn btn-primary btn-crop" data-method="crop" data-option="crop" title="Crop">
                                 <span class="docs-tooltip" data-toggle="tooltip" title="cropper.setDragMode(&quot;crop&quot;)">
                                     <span class="fa fa-crop"></span> OK
@@ -70,22 +71,22 @@
 </div>
 
 <script>
-(function($) {
-    var Cropper = window.Cropper;
-    var URL = window.URL || window.webkitURL;
-    var modalCropper = $('#modal-cropper-<?php echo $id ?>');
-    var container = modalCropper.find('.cropper-image-container');
-    var image = container.find('img');
-    $('.btn-zoom-in').click(function(event){
-       
-        image.cropper('zoom', 0.1);
-        console.log(image.length);
-    });
-    $('.btn-zoom-out').click(function(event){
-        image.cropper('zoom', -0.1);
-    });
-    modalCropper.find('.close').click(function (e) {
-        modalCropper.modal('hide');
-    });
-})(jQuery);    
+    (function ($) {
+        var Cropper = window.Cropper;
+        var URL = window.URL || window.webkitURL;
+        var modalCropper = $('#modal-cropper-<?php echo $id ?>');
+        var container = modalCropper.find('.cropper-image-container');
+        var image = container.find('img');
+        $('.btn-zoom-in').click(function (event) {
+
+            image.cropper('zoom', 0.1);
+            console.log(image.length);
+        });
+        $('.btn-zoom-out').click(function (event) {
+            image.cropper('zoom', -0.1);
+        });
+        modalCropper.find('.close').click(function (e) {
+            modalCropper.modal('hide');
+        });
+    })(jQuery);
 </script>
