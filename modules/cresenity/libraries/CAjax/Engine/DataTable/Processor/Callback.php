66b8ae887fb043997a376bacfb65fe9263659d8f
hery <hery@ittron.co.id>
2018-07-13 21:28:47 +0700

    update Processor Callback DataTable Engine

--- modules/cresenity/libraries/CAjax/Engine/DataTable/Processor/Callback.php
+++ modules/cresenity/libraries/CAjax/Engine/DataTable/Processor/Callback.php
@@ -11,6 +11,11 @@ class CAjax_Engine_DataTable_Processor_Callback extends CAjax_Engine_DataTable_P
 
     public function process() {
         $data = $this->engine->getData();
+
+        $table = carr::get($data, 'table');
+        $keyField = carr::get($data, 'key_field');
+        $table = unserialize($table);
+        $request = $this->input;
         $callbackRequire = carr::get($data, 'callback_require');
         if (strlen($callbackRequire) > 0 && is_file($callbackRequire)) {
             require_once $callbackRequire;
@@ -18,7 +23,70 @@ class CAjax_Engine_DataTable_Processor_Callback extends CAjax_Engine_DataTable_P
         $callback = carr::get($data, 'query');
         $params = array();
         $params['options'] = carr::get($data, 'callback_options');
-        return call_user_func_array($callback, $params);
+        $resultCallback = call_user_func_array($callback, $params);
+
+        $tableData = carr::get($resultCallback, 'data', array());
+        $totalRecord = carr::get($resultCallback, 'total_record', count($tableData));
+        $totalFilteredRecord = carr::get($resultCallback, 'total_filtered_record', count($tableData));
+
+        $rowActionList = $table->getRowActionList();
+        $js = '';
+        if (count($rowActionList) > 0) {
+            foreach ($tableData as $kRow => $row) {
+                $html = new CStringBuilder();
+                $key = carr::get($row, $keyField);
+                $html->appendln('<td class="low-padding align-center cell-action td-action">')->inc_indent()->br();
+                foreach ($row as $k => $v) {
+                    $jsparam[$k] = $v;
+                }
+                $jsparam["param1"] = $key;
+                if ($table->getRowActionList()->getStyle() == "btn-dropdown") {
+                    $table->getRowActionList()->add_class("pull-right");
+                }
+                $rowActionList->regenerateId(true);
+                $rowActionList->apply("jsparam", $jsparam);
+
+                $rowActionList->apply("set_handler_url_param", $jsparam);
+
+                if (($table->filter_action_callback_func) != null) {
+                    $actions = $rowActionList->childs();
+
+                    foreach ($actions as $action) {
+                        $visibility = CDynFunction::factory($table->filter_action_callback_func)
+                                ->add_param($table)
+                                ->add_param($col->get_fieldname())
+                                ->add_param($row)
+                                ->add_param($action)
+                                ->set_require($table->requires)
+                                ->execute();
+
+                        $action->set_visibility($visibility);
+                    }
+                }
+
+                $html->appendln($table->getRowActionList()->html($html->get_indent()));
+                $js .= $table->getRowActionList()->js();
+                $html->decIndent()->appendln('</td>')->br();
+                //$arr[] = '';
+                $tableData[$kRow][] = $html->text();
+                $tableData[$kRow]["DT_RowId"] = $key;
+            }
+        }
+
+
+
+        $output = array(
+            "sEcho" => intval(carr::get($request, 'sEcho')),
+            "iTotalRecords" => $totalRecord,
+            "iTotalDisplayRecords" => $totalFilteredRecord,
+            "aaData" => $tableData,
+        );
+        $data = array(
+            "datatable" => $output,
+            "js" => cbase64::encode($js),
+        );
+        $response = json_encode($data);
+        return $response;
     }
 
 }
