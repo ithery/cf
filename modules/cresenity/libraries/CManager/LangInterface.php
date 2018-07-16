48190fa886a6ebb2730649e5050fc1cb70c551e7
Hery <hery.cresenity@gmail.com>
2018-07-16 23:40:35 +0700

    update phpdoc for LangInterface

--- modules/cresenity/libraries/CManager/LangInterface.php
+++ modules/cresenity/libraries/CManager/LangInterface.php
@@ -9,11 +9,32 @@ defined('SYSPATH') OR die('No direct access allowed.');
  */
 interface CManager_LangInterface {
 
+    /**
+     * main method for the translation logic to be implemented
+     * 
+     * @param string $message string to be translated
+     * @param array $params parameter for variable translation
+     * @param string $lang optional, if null must call the function getLang
+     */
     public static function getTranslation($message, $params = array(), $lang = null);
 
+    /**
+     * Get Current Lang
+     * @return string
+     */
     public static function getLang();
 
+    /**
+     * Set Current Lang
+     * @param string $langKey
+     */
     public static function setLang($langKey);
 
+    /**
+     * Get current available lang 
+     * must return assoc array with array key langCode and array value langName
+     * 
+     * @return array
+     */
     public static function getLangAvailable();
 }
