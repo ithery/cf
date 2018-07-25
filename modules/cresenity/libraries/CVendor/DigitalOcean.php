3872da0e191b881f97dfc558ea514785b778bc39
hery <hery@ittron.co.id>
2018-07-26 00:15:28 +0700

    update DO

--- modules/cresenity/libraries/CVendor/DigitalOcean.php
+++ modules/cresenity/libraries/CVendor/DigitalOcean.php
@@ -6,6 +6,28 @@
  * and open the template in the editor.
  */
 
+use DigitalOceanV2\Adapter\GuzzleAdapter;
+use DigitalOceanV2\DigitalOceanV2;
+
 class CVendor_DigitalOcean {
-    
+
+    protected $do;
+
+    public function __construct($accessToken) {
+        // create an adapter with your access token which can be
+        // generated at https://cloud.digitalocean.com/settings/applications
+        $adapter = new GuzzleAdapter($accessToken);
+
+        // create a digital ocean object with the previous adapter
+        $this->do = new DigitalOceanV2($adapter);
+    }
+
+    /**
+     * 
+     * @return DigitalOceanV2\DigitalOceanV2
+     */
+    public function getObject() {
+        return $this->do;
+    }
+
 }
