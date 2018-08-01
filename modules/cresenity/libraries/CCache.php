42acbc9335a913bc5e0e2abb2f2c2dced8cf0042
hery <hery@ittron.co.id>
2018-08-01 13:22:56 +0700

    update Cache and GeoIP (Not Done)

--- modules/cresenity/libraries/CCache.php
+++ modules/cresenity/libraries/CCache.php
@@ -1,44 +1,11 @@
 <?php
 
-    defined('SYSPATH') OR die('No direct access allowed.');
-    require_once dirname(__FILE__) . "/Lib/csysinfo/CSysInfo.php";
+defined('SYSPATH') OR die('No direct access allowed.');
 
-    class CCache extends CObject {
+class CCache {
 
-        protected static $_instance;
-
-        public function __construct($id = "") {
-            parent::__construct($id);
-        }
-
-        public static function instance($id = "") {
-            if (self::$_instance == null) self::$_instance = new CCache($id);
-            return self::$_instance;
-        }
-
-        public function get($id) {
-            return Cache::instance()->get($id);
-        }
-
-        public function find($tag) {
-            return Cache::instance()->find($tag);
-        }
-
-        public function set($id, $data, $tags = NULL, $lifetime = NULL) {
-            return Cache::instance()->set($id, $data, $tags, $lifetime);
-        }
-
-        public function delete($id) {
-            return Cache::instance()->delete($id);
-        }
-
-        public function delete_tag($tag) {
-            return Cache::instance()->delete_tag($tag);
-        }
-
-        public function delete_all($tag) {
-            return Cache::instance()->delete_all($tag);
-        }
-
+    public static function manager() {
+        return new CCache_Manager();
     }
-    
+
+}
