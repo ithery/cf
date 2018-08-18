82ee194fe7382d3f248ecffde2ce0a9053e8ec75
Hery <hery.cresenity@gmail.com>
2018-08-18 16:56:07 +0700

    Sql Formatter

---
+++ modules/cresenity/libraries/CSql.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Aug 18, 2018, 4:36:16 PM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+class CSql {
+
+    public static function format($query) {
+        return CSql_Formatter::format($query, false);
+    }
+
+    public static function highlight($query) {
+        return CSql_Formatter::highlight($query);
+    }
+
+}
