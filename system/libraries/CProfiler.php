16a6211fa56bab80c090c63fe77d0f194728cb82
Hery <hery.cresenity@gmail.com>
2018-08-22 18:12:35 +0700

    update bar varDumper with symfoni

---
+++ system/libraries/CProfiler.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Aug 22, 2018, 4:00:17 PM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+class CProfiler {
+    protected $data = array();
+    
+}
