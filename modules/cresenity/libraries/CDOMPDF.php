5a634af68e47a7b777b845fb73a3ff6bca01cbb8
hery <hery@ittron.co.id>
2018-08-28 19:58:10 +0700

    upgrade class CDOMPDF

--- modules/cresenity/libraries/CDOMPDF.php
+++ modules/cresenity/libraries/CDOMPDF.php
@@ -1,17 +1,17 @@
 <?php
+
 defined('SYSPATH') or die('No direct access allowed.');
-require_once dirname(__FILE__) . "/Lib/dompdf/dompdf_config.inc.php";
 
+use Dompdf\Dompdf;
 
-class CDOMPDF extends DOMPDF{
-	
-	public function __construct() {
-		parent::__construct();
-	}
-	
-	public static function factory() {
-		return new CDOMPDF();
-	}
-	
-	
+class CDOMPDF extends DOMPDF {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public static function factory() {
+        return new CDOMPDF();
+    }
+
 }
