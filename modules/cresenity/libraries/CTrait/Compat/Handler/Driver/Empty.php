1554340099f42d3b32b9b9223ef885a83b824681
Hery <hery.cresenity@gmail.com>
2018-09-01 16:31:38 +0700

    add handler

---
+++ modules/cresenity/libraries/CTrait/Compat/Handler/Driver/Empty.php
@@ -0,0 +1,52 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Sep 1, 2018, 4:29:43 PM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+trait CTrait_Compat_Handler_Driver_Empty {
+
+    /**
+     * 
+     * @deprecated, please use setTarget
+     * @param string $target
+     * @return $this
+     */
+    public function set_target($target) {
+        return $this->setTarget($target);
+    }
+
+    /**
+     * 
+     * @deprecated, please use setMethod
+     * @param string $method
+     * @return $this
+     */
+    public function set_method($method) {
+        return $this->setMethod($method);
+    }
+
+    /**
+     * 
+     * @deprecated, please use addParamInput
+     * @param type $inputs
+     * @return type
+     */
+    public function add_param_input($inputs) {
+        return $this->addParamInput($inputs);
+    }
+
+    /**
+     * 
+     * @deprecated, please use addParamInputByName
+     * @param type $inputs
+     * @return type
+     */
+    public function add_param_input_by_name($inputs) {
+        return $this->addParamInputByName($inputs);
+    }
+
+}
