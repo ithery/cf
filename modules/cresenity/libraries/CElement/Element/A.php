2723b137d96e6df191c8d0c387d65f484b41c93e
Alvin <alvin@ittron-indonesia.com>
2018-11-14 10:13:25 +0700

    Update: CElement Element A -> add Function setTarget

--- modules/cresenity/libraries/CElement/Element/A.php
+++ modules/cresenity/libraries/CElement/Element/A.php
@@ -5,23 +5,37 @@ defined('SYSPATH') OR die('No direct access allowed.');
 class CElement_Element_A extends CElement_Element {
 
     protected $href;
+    protected $target;
 
     public function __construct($id = "") {
 
         parent::__construct($id);
         $this->tag = "a";
         $this->href = "";
+        $this->target = "";
     }
-
     
     public function set_href($href) {
         $this->href = $href;
         return $this;
     }
 
+    public function setHref($href) {
+        $this->href = $href;
+        return $this;
+    }
+
+    public function setTarget($target = '_blank') {
+        $this->target = $target;
+        return $this;
+    }
+
     public function build($indent = 0) {
         if (strlen($this->href) > 0) {
-            $this->add_attr('href', $this->href);
+            $this->addAttr('href', $this->href);
+        }
+        if ($this->target) {
+            $this->addAttr('target', $this->target);
         }
     }
 
