36666fce7e816a8e36f17fe2ee8040248033580c
hery <hery@ittron.co.id>
2018-12-10 09:43:07 +0700

    update safe code

--- modules/cresenity/controllers/cresenity.php
+++ modules/cresenity/controllers/cresenity.php
@@ -447,8 +447,8 @@ class Cresenity_Controller extends CController {
     public function logout() {
         $session = CSession::instance();
         $session->delete("user");
-        unset($_SESSION['current_position']);
-        unset($_SESSION['completed_position']);        
+        $session->delete("current_position");
+        $session->delete("completed_position");
         //$session->destroy();
         curl::redirect("");
     }
