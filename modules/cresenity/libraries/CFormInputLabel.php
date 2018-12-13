9f5347f130039d66eccdb946f8a36567bd8d416c
hery <hery@ittron.co.id>
2018-12-13 18:52:13 +0700

    fix parent js label form input

--- modules/cresenity/libraries/CFormInputLabel.php
+++ modules/cresenity/libraries/CFormInputLabel.php
@@ -1,64 +1,70 @@
 <?php
 
-    class CFormInputLabel extends CFormInput {
+class CFormInputLabel extends CFormInput {
 
-        public function __construct($id) {
-            parent::__construct($id);
+    public function __construct($id) {
+        parent::__construct($id);
 
-            $this->type = "label";
+        $this->type = "label";
+    }
+
+    public static function factory($id) {
+        return new CFormInputLabel($id);
+    }
+
+    public function html($indent = 0) {
+        $html = new CStringBuilder();
+        $html->set_indent($indent);
+        $disabled = "";
+
+        if ($this->disabled)
+            $disabled = ' disabled="disabled"';
+
+        $classes = $this->classes;
+        $classes = implode(" ", $classes);
+        if (strlen($classes) > 0)
+            $classes = " " . $classes;
+        $custom_css = $this->custom_css;
+        $custom_css = crenderer::render_style($custom_css);
+        if (strlen($custom_css) > 0) {
+            $custom_css = ' style="' . $custom_css . '"';
         }
-
-        public static function factory($id) {
-            return new CFormInputLabel($id);
+        $bootstrap_class = '';
+        if ($this->bootstrap >= '3') {
+            $bootstrap_class = ' label-default ';
+            $html->appendln('<h4>');
         }
-
-        public function html($indent = 0) {
-            $html = new CStringBuilder();
-            $html->set_indent($indent);
-            $disabled = "";
-
-            if ($this->disabled) $disabled = ' disabled="disabled"';
-
-            $classes = $this->classes;
-            $classes = implode(" ", $classes);
-            if (strlen($classes) > 0) $classes = " " . $classes;
-            $custom_css = $this->custom_css;
-            $custom_css = crenderer::render_style($custom_css);
-            if (strlen($custom_css) > 0) {
-                $custom_css = ' style="' . $custom_css . '"';
-            }
-            $bootstrap_class = '';
-            if ($this->bootstrap >= '3') {
-                $bootstrap_class = ' label-default ';
-                $html->appendln('<h4>');
-            }
-            if (is_array($this->value)) {
-                $i = 0;
-                foreach ($this->value as $val) {
-                    $new_val = $val;
-                    foreach ($this->transforms as $trans) {
-                        $new_val = $trans->execute($new_val);
-                    }
-                    $html->appendln('<span class="label ' . $bootstrap_class . '" id="' . $this->id . '_' . $i . '">' . $new_val . '</span>')->br();
-                    $i++;
-                }
-            }
-            else {
-                $new_val = $this->value;
+        if (is_array($this->value)) {
+            $i = 0;
+            foreach ($this->value as $val) {
+                $new_val = $val;
                 foreach ($this->transforms as $trans) {
                     $new_val = $trans->execute($new_val);
                 }
-                $html->appendln('<span class="label ' . $bootstrap_class . $classes . '" name="' . $this->name . '" id="' . $this->id . '" ' . $custom_css . '>' . $new_val . '</span>')->br();
+                $html->appendln('<span class="label ' . $bootstrap_class . '" id="' . $this->id . '_' . $i . '">' . $new_val . '</span>')->br();
+                $i++;
             }
-            if ($this->bootstrap >= '3') {
-                $html->appendln('</h4>');
+        } else {
+            $new_val = $this->value;
+            foreach ($this->transforms as $trans) {
+                $new_val = $trans->execute($new_val);
             }
-            return $html->text();
+            $html->appendln('<span class="label ' . $bootstrap_class . $classes . '" name="' . $this->name . '" id="' . $this->id . '" ' . $custom_css . '>' . $new_val . '</span>')->br();
         }
-
-        public function js($indent = 0) {
-            return "";
+        if ($this->bootstrap >= '3') {
+            $html->appendln('</h4>');
         }
-
+        return $html->text();
     }
-    
+
+    public function js($indent = 0) {
+        $js = new CStringBuilder();
+
+
+        $js->append(parent::js());
+
+
+        return $js->text();
+    }
+
+}
