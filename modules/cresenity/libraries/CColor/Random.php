ac2e7db7f5bd9c1ddd6e9488eccac15d48326260
Hery <hery.cresenity@gmail.com>
2019-02-16 04:34:58 +0700

    add ColorInfoTrait

--- modules/cresenity/libraries/CColor/Random.php
+++ modules/cresenity/libraries/CColor/Random.php
@@ -9,49 +9,8 @@ defined('SYSPATH') OR die('No direct access allowed.');
  */
 class CColor_Random {
 
-    use CColor_Trait_OptionsTrait;
-    static public $dictionary = array(
-        'monochrome' => array(
-            'bounds' => array(array(0, 0), array(100, 0)),
-            'h' => NULL,
-            's' => array(0, 100)
-        ),
-        'red' => array(
-            'bounds' => array(array(20, 100), array(30, 92), array(40, 89), array(50, 85), array(60, 78), array(70, 70), array(80, 60), array(90, 55), array(100, 50)),
-            'h' => array(-26, 18),
-            's' => array(20, 100)
-        ),
-        'orange' => array(
-            'bounds' => array(array(20, 100), array(30, 93), array(40, 88), array(50, 86), array(60, 85), array(70, 70), array(100, 70)),
-            'h' => array(19, 46),
-            's' => array(20, 100)
-        ),
-        'yellow' => array(
-            'bounds' => array(array(25, 100), array(40, 94), array(50, 89), array(60, 86), array(70, 84), array(80, 82), array(90, 80), array(100, 75)),
-            'h' => array(47, 62),
-            's' => array(25, 100)
-        ),
-        'green' => array(
-            'bounds' => array(array(30, 100), array(40, 90), array(50, 85), array(60, 81), array(70, 74), array(80, 64), array(90, 50), array(100, 40)),
-            'h' => array(63, 178),
-            's' => array(30, 100)
-        ),
-        'blue' => array(
-            'bounds' => array(array(20, 100), array(30, 86), array(40, 80), array(50, 74), array(60, 60), array(70, 52), array(80, 44), array(90, 39), array(100, 35)),
-            'h' => array(179, 257),
-            's' => array(20, 100)
-        ),
-        'purple' => array(
-            'bounds' => array(array(20, 100), array(30, 87), array(40, 79), array(50, 70), array(60, 65), array(70, 59), array(80, 52), array(90, 45), array(100, 42)),
-            'h' => array(258, 282),
-            's' => array(20, 100)
-        ),
-        'pink' => array(
-            'bounds' => array(array(20, 100), array(30, 90), array(40, 86), array(60, 84), array(80, 80), array(90, 75), array(100, 73)),
-            'h' => array(283, 334),
-            's' => array(20, 100)
-        )
-    );
+    use CColor_Trait_OptionsTrait,
+        CColor_Trait_ColorInfoTrait;
 
     public function __construct($options = array()) {
         $optionsDefault = array(
@@ -200,18 +159,6 @@ class CColor_Random {
         return 0;
     }
 
-    private function getColorInfo($h) {
-        // Maps red colors to make picking hue easier
-        if ($h >= 334 && $h <= 360) {
-            $h -= 360;
-        }
-        foreach (self::$dictionary as $color) {
-            if ($color['h'] !== null && $h >= $color['h'][0] && $h <= $color['h'][1]) {
-                return $color;
-            }
-        }
-    }
-
     private function rand($bounds) {
 
         if ($this->haveOption('prng')) {
@@ -222,5 +169,4 @@ class CColor_Random {
         }
     }
 
-  
 }
--- modules/cresenity/libraries/CColor/String.php
+++ modules/cresenity/libraries/CColor/String.php
@@ -9,26 +9,74 @@ defined('SYSPATH') OR die('No direct access allowed.');
  */
 class CColor_String {
 
-    use CColor_Trait_OptionsTrait;
+    use CColor_Trait_OptionsTrait,
+        CColor_Trait_ColorInfoTrait;
 
     protected $string;
+    protected $int32;
 
     public function __construct($string, $options = array()) {
         $this->string = $string;
+        $this->int32 = hexdec(substr(md5($this->string), 0, 8));
         $this->options = array();
     }
 
-    private function toInt32() {
-        $hash = md5($this->string);
-        return unpack("L", substr($hash, 0, 4));
+    private function pickHue() {
+
+        return $this->int32 % 255;
+    }
+
+    private function pickSaturation($h) {
+        if ($this->getOption('hue') === 'monochrome') {
+            return 0;
+        }
+        if ($this->getOption('luminosity') === 'random') {
+            return $this->rand(array(0, 100));
+        }
+        $colorInfo = $this->getColorInfo($h);
+        $range = $colorInfo['s'];
+        if ($this->haveOption('luminosity')) {
+            switch ($this->getOption('luminosity')) {
+                case 'bright':
+                    $range[0] = 55;
+                    break;
+                case 'dark':
+                    $range[0] = $range[1] - 10;
+                    break;
+                case 'light':
+                    $range[1] = 55;
+                    break;
+            }
+        }
+
+        return $this->rand($range);
+    }
+
+    private function pickBrightness($h, $s) {
+        if ($this->getOption('luminosity') === 'random') {
+            $range = array(0, 100);
+        } else {
+            $range = array($this->getMinimumBrightness($h, $s), 100);
+            if ($this->haveOption('luminosity')) {
+                switch ($this->getOption('luminosity')) {
+                    case 'dark':
+                        $range[1] = $range[0] + 20;
+                        break;
+                    case 'light':
+                        $range[0] = ($range[1] + $range[0]) / 2;
+                        break;
+                }
+            }
+        }
+        return $this->rand($range);
     }
 
     /**
      * 
      * @return CColor_Format_Type_Hex
      */
-    public function getHex() {
-        $hsv = $this->getHsv();
+    public function toHex() {
+        $hsv = $this->toHsv();
         return $hsv->toHex();
     }
 
@@ -36,11 +84,11 @@ class CColor_String {
      * 
      * @return \CColor_Format_Type_Hsv
      */
-    public function getHsv() {
-        $number = $this->toInt32();
-        $h = $number % 256;
-        $s = $number % 100;
+    public function toHsv() {
+        $h = $this->pickHue();
+        $s = $this->pickSaturation($h);
         $v = $number % 100;
+
         return new CColor_Format_Type_Hsv(compact('h', 's', 'v'));
     }
 
