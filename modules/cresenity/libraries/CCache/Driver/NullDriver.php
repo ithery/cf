b4683bf2e37fdb48940c48e119c7b4560bef4ca0
Hery <hery.cresenity@gmail.com>
2019-02-16 14:50:13 +0700

    add null driver for CCache

--- modules/cresenity/libraries/CCache/Driver/NullDriver.php
+++ modules/cresenity/libraries/CCache/Driver/NullDriver.php
@@ -4,10 +4,17 @@ defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Feb 16, 2019, 1:07:23 PM
+ * @since Feb 16, 2019, 2:44:05 PM
  * @license Ittron Global Teknologi <ittron.co.id>
  */
-interface CCache_DriverInterface {
+class CCache_NullDriver extends CCache_DriverAbstract {
+
+    /**
+     * The array of stored values.
+     *
+     * @var array
+     */
+    protected $storage = [];
 
     /**
      * Retrieve an item from the cache by key.
@@ -15,44 +22,54 @@ interface CCache_DriverInterface {
      * @param  string|array  $key
      * @return mixed
      */
-    public function get($key);
+    public function get($key) {
+        return null;
+    }
 
     /**
-     * Store an item in the cache for a given number of minutes.
+     * Store an item in the cache for a given number of seconds.
      *
      * @param  string  $key
-     * @param  mixed   $value
-     * @param  float|int  $minutes
-     * @return void
+     * @param  mixed  $value
+     * @param  int  $seconds
+     * @return bool
      */
-    public function put($key, $value, $minutes);
+    public function put($key, $value, $minutes) {
+        return false;
+    }
 
     /**
      * Increment the value of an item in the cache.
      *
      * @param  string  $key
-     * @param  mixed  $value
+     * @param  mixed   $value
      * @return int|bool
      */
-    public function increment($key, $value = 1);
+    public function increment($key, $value = 1) {
+        return false;
+    }
 
     /**
      * Decrement the value of an item in the cache.
      *
      * @param  string  $key
-     * @param  mixed  $value
+     * @param  mixed   $value
      * @return int|bool
      */
-    public function decrement($key, $value = 1);
+    public function decrement($key, $value = 1) {
+        return false;
+    }
 
     /**
      * Store an item in the cache indefinitely.
      *
      * @param  string  $key
-     * @param  mixed  $value
+     * @param  mixed   $value
      * @return bool
      */
-    public function forever($key, $value);
+    public function forever($key, $value) {
+        return false;
+    }
 
     /**
      * Remove an item from the cache.
@@ -60,19 +77,26 @@ interface CCache_DriverInterface {
      * @param  string  $key
      * @return bool
      */
-    public function forget($key);
+    public function forget($key) {
+        return true;
+    }
 
     /**
      * Remove all items from the cache.
      *
      * @return bool
      */
-    public function flush();
+    public function flush() {
+        return true;
+    }
 
     /**
      * Get the cache key prefix.
      *
      * @return string
      */
-    public function getPrefix();
+    public function getPrefix() {
+        return '';
+    }
+
 }
