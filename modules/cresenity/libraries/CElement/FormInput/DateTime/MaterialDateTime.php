d190a218e4821ac93d4ab0470628c4abfc02a7d0
Hery <hery.cresenity@gmail.com>
2019-02-17 02:27:02 +0700

    add bootstrap-material-datetimepicker asset

--- modules/cresenity/libraries/CElement/FormInput/DateTime/MaterialDateTime.php
+++ modules/cresenity/libraries/CElement/FormInput/DateTime/MaterialDateTime.php
@@ -7,6 +7,16 @@ defined('SYSPATH') OR die('No direct access allowed.');
  * @since Feb 17, 2019, 2:08:19 AM
  * @license Ittron Global Teknologi <ittron.co.id>
  */
-class CElement_FormInput_DateTime_Material extends CElement_FormInput {
-    
+class CElement_FormInput_DateTime_MaterialDateTime extends CElement_FormInput_DateTime {
+
+    public function __construct($id) {
+        parent::__construct($id);
+        CManager::instance()->registerModule('bootstrap-4-material-datetimepicker');
+    }
+
+    public function build() {
+        parent::build();
+        $this->addClass('form-control');
+    }
+
 }
