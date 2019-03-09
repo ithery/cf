c5ec4a745bcfd0c099a30e2a0ed3ec32c1cf29ec
Hery <hery.cresenity@gmail.com>
2019-03-09 15:54:51 +0700

    update pseudo element

--- modules/cresenity/libraries/CElement/PseudoElement.php
+++ modules/cresenity/libraries/CElement/PseudoElement.php
@@ -1,17 +1,17 @@
 <?php
 
-class CElement_PseudoElement extends CMobile_Element {
+class CElement_PseudoElement extends CElement_Element {
 
     public static function factory($id = "", $tag = "div") {
         return new CElement_PseudoElement($id, $tag);
     }
 
     public function html($indent = 0) {
-        return parent::html_child();
+        return parent::htmlChild();
     }
 
     public function js($indent = 0) {
-        return parent::js_child();
+        return parent::jsChild();
     }
 
 }
