e087faa0c1b12d3ff9fd27cba7c04f5e58cf38aa
Alvin <alvin@ittron-indonesia.com>
2019-03-14 19:49:37 +0700

    Update: Model LogActivity

--- system/libraries/CModel/LogActivity/Trait.php
+++ system/libraries/CModel/LogActivity/Trait.php
@@ -28,7 +28,7 @@ trait CModel_LogActivity_Trait
 		return carr::get($GLOBALS, 'CModel_LogActivity', []);
 	}
 
-	public static function push(CModel $model)
+	public static function addActivity(CModel $model)
 	{
 		array_push($GLOBALS['CModel_LogActivity'], $model);
 	}
