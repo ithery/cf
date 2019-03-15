d2f3365bfe68e54df9ed33a9186874e988bdf75f
Alvin <alvin@ittron-indonesia.com>
2019-03-15 18:08:49 +0700

    Update: Model LogActivity

--- modules/cresenity/libraries/CApp/Model/LogActivity.php
+++ modules/cresenity/libraries/CApp/Model/LogActivity.php
@@ -5,7 +5,7 @@ defined('SYSPATH') OR die('No direct access allowed.');
 /**
  * 
  */
-class CApp_Model_Org extends CApp_Model
+class CApp_Model_LogActivity extends CApp_Model
 {
 	use CApp_Model_Trait_LogActivity;
 }
