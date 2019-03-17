303960b161ea55dac181218c3e2f41edba445de6
Hery <hery.cresenity@gmail.com>
2019-03-17 16:55:03 +0700

    add Listener Event

--- modules/cresenity/libraries/CDaemon/Event.php
+++ modules/cresenity/libraries/CDaemon/Event.php
@@ -8,5 +8,18 @@ defined('SYSPATH') OR die('No direct access allowed.');
  * @license Ittron Global Teknologi <ittron.co.id>
  */
 class CDaemon_Event {
-    
+    /**
+     * Events can be attached to each state using the on() method
+     * @var integer
+     */
+    const ON_ERROR = 'CDaemon.OnError';    // error() or fatalError() is called
+    const ON_SIGNAL = 'CDaemon.OnSignal';    // the daemon has received a signal
+    const ON_INIT = 'CDaemon.OnInit';    // the library has completed initialization, your setup() method is about to be called. Note: Not Available to Worker code.
+    const ON_PRE_EXECUTE = 'CDaemon.OnPreExecute';    // inside the event loop, right before your execute() method
+    const ON_POST_EXECUTE = 'CDaemon.OnPostExecute';    // and right after
+    const ON_FORK = 'CDaemon.OnFork';    // in a background process right after it has been forked from the daemon
+    const ON_PID_CHANGE = 'CDaemon.OnPidChange';    // whenever the pid changes -- in a background process for example
+    const ON_IDLE = 'CDaemon.OnIdle';    // called when there is idle time at the end of a loopInterval, or at the idleProbability when loopInterval isn't used
+    const ON_REAP = 'CDaemon.OnReap';    // notification from the OS that a child process of this application has exited
+    const ON_SHUTDOWN = 'CDaemon.OnShutdown';   // called at the top of the destructor
 }
--- modules/cresenity/libraries/CDaemon/ServiceAbstract.php
+++ modules/cresenity/libraries/CDaemon/ServiceAbstract.php
@@ -32,20 +32,7 @@ abstract class CDaemon_ServiceAbstract implements CDaemon_ServiceInterface {
      */
     const MIN_RESTART_SECONDS = 10;
 
-    /**
-     * Events can be attached to each state using the on() method
-     * @var integer
-     */
-    const ON_ERROR = 0;    // error() or fatalError() is called
-    const ON_SIGNAL = 1;    // the daemon has received a signal
-    const ON_INIT = 2;    // the library has completed initialization, your setup() method is about to be called. Note: Not Available to Worker code.
-    const ON_PREEXECUTE = 3;    // inside the event loop, right before your execute() method
-    const ON_POSTEXECUTE = 4;    // and right after
-    const ON_FORK = 5;    // in a background process right after it has been forked from the daemon
-    const ON_PIDCHANGE = 6;    // whenever the pid changes -- in a background process for example
-    const ON_IDLE = 7;    // called when there is idle time at the end of a loopInterval, or at the idleProbability when loopInterval isn't used
-    const ON_REAP = 8;    // notification from the OS that a child process of this application has exited
-    const ON_SHUTDOWN = 10;   // called at the top of the destructor
+    
 
     /**
      * The frequency of the event loop. In seconds.
