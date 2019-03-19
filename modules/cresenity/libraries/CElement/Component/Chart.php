9f107da6423bbf4e891d4668e731c3fa31ff68b4
Alvin <alvin@ittron-indonesia.com>
2019-03-19 21:15:51 +0700

    Update: Chart

--- modules/cresenity/libraries/CElement/Component/Chart.php
+++ modules/cresenity/libraries/CElement/Component/Chart.php
@@ -41,14 +41,32 @@ abstract class CElement_Component_Chart extends CElement_Component
     public function setData(array $val)
     {
         $data = [];
+        $data['labels'] = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
         $data['datasets'] = [];
         $data['datasets'][0] = [];
         $data['datasets'][0]['label'] = '# of Votes';
-        $data['datasets'][0]['data'] = [];
+        $data['datasets'][0]['data'] = [12, 19, 3, 5, 2, 3];
+        $data['datasets'][0]['backgroundColor'] = [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ];
+        $data['datasets'][0]['borderColor'] = [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ];
+        $data['datasets'][0]['borderWidth'] = 1;
 
-        foreach ($val as $k => $v) {
-            $data['datasets'][0]['data'][] = $v;
-        }
+        // foreach ($val as $k => $v) {
+        //     $data['datasets'][0]['data'][] = $v;
+        // }
 
         $this->data = json_encode($data);
         return $this;
