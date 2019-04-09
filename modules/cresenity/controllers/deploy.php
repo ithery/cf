cbea49ac0063c24694eae2a706d3ef9da10ab882
Hery <hery.cresenity@gmail.com>
2019-04-09 23:45:58 +0700

    fix controller deploy

--- modules/cresenity/controllers/deploy.php
+++ modules/cresenity/controllers/deploy.php
@@ -1,39 +1,37 @@
 <?php
 
-    class Deploy_Controller extends CController {
+class Controller_Deploy extends CController {
 
-        public function index(){
-            $app = CApp::instance();
-            
-            $post = $_POST;
-            
-            if ($post != NUlL) {
-                $res = $this->git_pull_test();
-                if ($res) {
-                    cmsg::add('success', 'Success');
-                }
-                else {
-                    cmsg::add('error', 'Error');
-                }
+    public function index() {
+        $app = CApp::instance();
+
+        $post = $_POST;
+
+        if ($post != NUlL) {
+            $res = $this->git_pull_test();
+            if ($res) {
+                cmsg::add('success', 'Success');
+            } else {
+                cmsg::add('error', 'Error');
             }
-            
-            $form = $app->add_form();
-//            $form->add_field()->set_label('Domain')->add_control('domain', 'text');
-            $form->add_action()->set_label('Submit')->set_submit(true);
-            
-            echo $app->render();
-        }
-        
-        public function git_pull_test() {
-            $post = $_POST;
-            $domain = CF::app_code();
-            
-            Git::set_bin('git');
-            $repo = Git::open("C:/xampp/htdocs_pippo/application/" .$domain ."/");
-            $res = $repo->run('fetch --progress --prune origin');
-            $res = $repo->run('merge --no-commit --ff origin/Test');
-            return $res;
         }
 
+        $form = $app->add_form();
+//            $form->add_field()->set_label('Domain')->add_control('domain', 'text');
+        $form->add_action()->set_label('Submit')->set_submit(true);
+
+        echo $app->render();
     }
-    
+
+    public function git_pull_test() {
+        $post = $_POST;
+        $domain = CF::app_code();
+
+        Git::set_bin('git');
+        $repo = Git::open("C:/xampp/htdocs_pippo/application/" . $domain . "/");
+        $res = $repo->run('fetch --progress --prune origin');
+        $res = $repo->run('merge --no-commit --ff origin/Test');
+        return $res;
+    }
+
+}
