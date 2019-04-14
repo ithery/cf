696b954bc2376590f646f3a7e11b8185be3661c9
Hery <hery.cresenity@gmail.com>
2019-04-14 21:45:43 +0700

    update listener

--- modules/cresenity/libraries/CDaemon/Service/ListenerInterface.php
+++ modules/cresenity/libraries/CDaemon/Service/ListenerInterface.php
@@ -4,9 +4,9 @@ defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Mar 12, 2019, 3:24:36 PM
+ * @since Apr 14, 2019, 9:43:58 PM
  * @license Ittron Global Teknologi <ittron.co.id>
  */
-interface CDaemon_ServiceInterface {
+interface CDaemon_Service_ListenerInterface {
     
 }
