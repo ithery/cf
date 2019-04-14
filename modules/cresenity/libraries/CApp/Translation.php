554419ccb3b55cf277e6771498bcf6052a7fea47
Hery <hery.cresenity@gmail.com>
2019-04-14 21:08:58 +0700

    move validation and translation to system

--- modules/cresenity/libraries/CApp/Translation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('SYSPATH') OR die('No direct access allowed.');
-
-/**
- * @author Hery Kurniawan
- * @since Sep 8, 2018, 4:45:25 AM
- * @license Ittron Global Teknologi <ittron.co.id>
- */
