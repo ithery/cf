2177c72925a89dbc99d8ac4775d5bd12cf045b7a
Hery <hery.cresenity@gmail.com>
2019-04-14 22:57:36 +0700

    update listener

--- modules/cresenity/libraries/CDaemon/Service/ListenerAbstract.php
+++ modules/cresenity/libraries/CDaemon/Service/ListenerAbstract.php
@@ -485,7 +485,6 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
         static::checkSapiEnv();
         static::init();
         static::lock();
-        static::parseCommand();
         static::daemonize();
         static::initWorkers();
         static::installSignal();
@@ -519,7 +518,7 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
      */
     protected static function init() {
         set_error_handler(function($code, $msg, $file, $line) {
-            Worker::safeEcho("$msg in file $file on line $line\n");
+            static::safeEcho("$msg in file $file on line $line\n");
         });
         // Start file.
         $backtrace = debug_backtrace();
@@ -548,7 +547,7 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
         // Init data for worker id.
         static::initId();
         // Timer init.
-        Timer::init();
+        CDaemon_Service_Listener_Timer::init();
     }
 
     /**
@@ -754,156 +753,6 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
         !defined('LINE_VERSIOIN_LENGTH') && define('LINE_VERSIOIN_LENGTH', 0);
         $total_length <= LINE_VERSIOIN_LENGTH && $total_length = LINE_VERSIOIN_LENGTH;
         return $total_length;
-    }
-
-    /**
-     * Parse command.
-     *
-     * @return void
-     */
-    protected static function parseCommand() {
-        if (static::$_OS !== CDaemon_Constant::OS_TYPE_LINUX) {
-            return;
-        }
-        global $argv;
-        // Check argv;
-        $start_file = $argv[0];
-        $available_commands = array(
-            'start',
-            'stop',
-            'restart',
-            'reload',
-            'status',
-            'connections',
-        );
-        $usage = "Usage: php yourfile <command> [mode]\nCommands: \nstart\t\tStart worker in DEBUG mode.\n\t\tUse mode -d to start in DAEMON mode.\nstop\t\tStop worker.\n\t\tUse mode -g to stop gracefully.\nrestart\t\tRestart workers.\n\t\tUse mode -d to start in DAEMON mode.\n\t\tUse mode -g to stop gracefully.\nreload\t\tReload codes.\n\t\tUse mode -g to reload gracefully.\nstatus\t\tGet worker status.\n\t\tUse mode -d to show live status.\nconnections\tGet worker connections.\n";
-        if (!isset($argv[1]) || !in_array($argv[1], $available_commands)) {
-            if (isset($argv[1])) {
-                static::safeEcho('Unknown command: ' . $argv[1] . "\n");
-            }
-            exit($usage);
-        }
-        // Get command.
-        $command = trim($argv[1]);
-        $command2 = isset($argv[2]) ? $argv[2] : '';
-        // Start command.
-        $mode = '';
-        if ($command === 'start') {
-            if ($command2 === '-d' || static::$daemonize) {
-                $mode = 'in DAEMON mode';
-            } else {
-                $mode = 'in DEBUG mode';
-            }
-        }
-        static::log("Workerman[$start_file] $command $mode");
-        // Get master process PID.
-        $master_pid = is_file(static::$pidFile) ? file_get_contents(static::$pidFile) : 0;
-        $master_is_alive = $master_pid && posix_kill($master_pid, 0) && posix_getpid() != $master_pid;
-        // Master is still alive?
-        if ($master_is_alive) {
-            if ($command === 'start') {
-                static::log("Workerman[$start_file] already running");
-                exit;
-            }
-        } elseif ($command !== 'start' && $command !== 'restart') {
-            static::log("Workerman[$start_file] not run");
-            exit;
-        }
-        // execute command.
-        switch ($command) {
-            case 'start':
-                if ($command2 === '-d') {
-                    static::$daemonize = true;
-                }
-                break;
-            case 'status':
-                while (1) {
-                    if (is_file(static::$_statisticsFile)) {
-                        @unlink(static::$_statisticsFile);
-                    }
-                    // Master process will send SIGUSR2 signal to all child processes.
-                    posix_kill($master_pid, SIGUSR2);
-                    // Sleep 1 second.
-                    sleep(1);
-                    // Clear terminal.
-                    if ($command2 === '-d') {
-                        static::safeEcho("\33[H\33[2J\33(B\33[m", true);
-                    }
-                    // Echo status data.
-                    static::safeEcho(static::formatStatusData());
-                    if ($command2 !== '-d') {
-                        exit(0);
-                    }
-                    static::safeEcho("\nPress Ctrl+C to quit.\n\n");
-                }
-                exit(0);
-            case 'connections':
-                if (is_file(static::$_statisticsFile) && is_writable(static::$_statisticsFile)) {
-                    unlink(static::$_statisticsFile);
-                }
-                // Master process will send SIGIO signal to all child processes.
-                posix_kill($master_pid, SIGIO);
-                // Waiting amoment.
-                usleep(500000);
-                // Display statisitcs data from a disk file.
-                if (is_readable(static::$_statisticsFile)) {
-                    readfile(static::$_statisticsFile);
-                }
-                exit(0);
-            case 'restart':
-            case 'stop':
-                if ($command2 === '-g') {
-                    static::$_gracefulStop = true;
-                    $sig = SIGTERM;
-                    static::log("Workerman[$start_file] is gracefully stopping ...");
-                } else {
-                    static::$_gracefulStop = false;
-                    $sig = SIGINT;
-                    static::log("Workerman[$start_file] is stopping ...");
-                }
-                // Send stop signal to master process.
-                $master_pid && posix_kill($master_pid, $sig);
-                // Timeout.
-                $timeout = 5;
-                $start_time = time();
-                // Check master process is still alive?
-                while (1) {
-                    $master_is_alive = $master_pid && posix_kill($master_pid, 0);
-                    if ($master_is_alive) {
-                        // Timeout?
-                        if (!static::$_gracefulStop && time() - $start_time >= $timeout) {
-                            static::log("Workerman[$start_file] stop fail");
-                            exit;
-                        }
-                        // Waiting amoment.
-                        usleep(10000);
-                        continue;
-                    }
-                    // Stop success.
-                    static::log("Workerman[$start_file] stop success");
-                    if ($command === 'stop') {
-                        exit(0);
-                    }
-                    if ($command2 === '-d') {
-                        static::$daemonize = true;
-                    }
-                    break;
-                }
-                break;
-            case 'reload':
-                if ($command2 === '-g') {
-                    $sig = SIGQUIT;
-                } else {
-                    $sig = SIGUSR1;
-                }
-                posix_kill($master_pid, $sig);
-                exit;
-            default :
-                if (isset($command)) {
-                    static::safeEcho('Unknown command: ' . $command . "\n");
-                }
-                exit($usage);
-        }
     }
 
     /**
@@ -1320,7 +1169,7 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
             Timer::init(static::$globalEvent);
         }
         $timer_id = Timer::add(0.1, function()use($std_handler) {
-                    Worker::safeEcho(fread($std_handler, 65535));
+                    static::safeEcho(fread($std_handler, 65535));
                 });
         // 保存子进程句柄
         static::$_processForWindows[$start_file] = array($process, $start_file, $timer_id);
@@ -1535,7 +1384,7 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
      * @return void
      */
     protected static function monitorWorkersForWindows() {
-        Timer::add(1, "\\Workerman\\Worker::checkWorkerStatusForWindows");
+        Timer::add(1, "\\Workerman\\static::checkWorkerStatusForWindows");
         static::$globalEvent->loop();
     }
 
@@ -1668,7 +1517,7 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
                     Timer::add(static::KILL_WORKER_TIMER_TIME, 'posix_kill', array($worker_pid, SIGKILL), false);
                 }
             }
-            Timer::add(1, "\\Workerman\\Worker::checkIfChildRunning");
+            Timer::add(1, "\\Workerman\\static::checkIfChildRunning");
             // Remove statistics file.
             if (is_file(static::$_statisticsFile)) {
                 @unlink(static::$_statisticsFile);
@@ -1919,21 +1768,6 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
     }
 
     /**
-     * Log.
-     *
-     * @param string $msg
-     * @return void
-     */
-    public static function log($msg) {
-        $msg = $msg . "\n";
-        if (!static::$daemonize) {
-            static::safeEcho($msg);
-        }
-        file_put_contents((string) static::$logFile, date('Y-m-d H:i:s') . ' ' . 'pid:'
-                . (static::$_OS === CDaemon_Constant::OS_TYPE_LINUX ? posix_getpid() : 1) . ' ' . $msg, FILE_APPEND | LOCK_EX);
-    }
-
-    /**
      * Safe Echo.
      * @param $msg
      * @param bool $decorated
@@ -2019,8 +1853,7 @@ class CDaemon_Service_ListenerAbstract extends CDaemon_ServiceAbstract implement
             return;
         }
         // Autoload.
-        Autoloader::setRootPath($this->_autoloadRootPath);
-        if (!$this->_mainSocket) {
+         if (!$this->_mainSocket) {
             // Get the application layer communication protocol and listening address.
             list($scheme, $address) = explode(':', $this->_socketName, 2);
             // Check application layer protocol class.
--- modules/cresenity/libraries/CDaemon/ServiceAbstract.php
+++ modules/cresenity/libraries/CDaemon/ServiceAbstract.php
@@ -8,5 +8,55 @@ defined('SYSPATH') OR die('No direct access allowed.');
  * @license Ittron Global Teknologi <ittron.co.id>
  */
 abstract class CDaemon_ServiceAbstract implements CDaemon_ServiceInterface {
-    
+
+    /**
+     * Log the $message to the filename returned by static::logFile() and/or optionally print to stdout.
+     * Multi-Line messages will be handled nicely.
+     *
+     * Note: Your logFile() method will be called every 5 minutes (at even increments, eg 00:05, 00:10, 00:15, etc) to
+     * allow you to rotate the filename based on time (one log file per month, day, hour, whatever) if you wish.
+     *
+     * Note: You may find value in overloading this method in your app in favor of a more fully-featured logging tool
+     * like log4php or Zend_Log. There are fantastic logging libraries available, and this simplistic home-grown option
+     * was chosen specifically to avoid forcing another dependency on you.
+     *
+     * @param string $message
+     * @param string $label Truncated at 12 chars
+     */
+    public function log($message, $label = '', $indent = 0) {
+        static $logFile = '';
+        static $logFile_check_at = 0;
+        static $logFile_error = false;
+        $header = "\nDate                  PID   Label         Message\n";
+        $date = date("Y-m-d H:i:s");
+        $pid = str_pad($this->pid, 5, " ", STR_PAD_LEFT);
+        $label = str_pad(substr($label, 0, 12), 13, " ", STR_PAD_RIGHT);
+        $prefix = "[$date] $pid $label" . str_repeat("\t", $indent);
+        if (time() >= $logFile_check_at && $this->logFile() != $logFile) {
+            $logFile = $this->logFile();
+            $logFile_check_at = mktime(date('H'), (date('i') - (date('i') % 5)) + 5, null);
+            @fclose(self::$log_handle);
+            self::$log_handle = $logFile_error = false;
+        }
+        if (self::$log_handle === false) {
+            if (strlen($logFile) > 0 && self::$log_handle = @fopen($logFile, 'a+')) {
+                if ($this->parent) {
+                    fwrite(self::$log_handle, $header);
+                    if ($this->stdout)
+                        echo $header;
+                }
+            } elseif (!$logFile_error) {
+                $logFile_error = true;
+                trigger_error(__CLASS__ . "Error: Could not write to logfile " . $logFile, E_USER_WARNING);
+            }
+        }
+        $message = $prefix . ' ' . str_replace("\n", "\n$prefix ", trim($message)) . "\n";
+        if (self::$log_handle) {
+            fwrite(self::$log_handle, $message);
+        }
+        if ($this->stdout) {
+            echo $message;
+        }
+    }
+
 }
