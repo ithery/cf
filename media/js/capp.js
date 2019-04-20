5b69cad2a0fbdb8c018a82a5e05814326791b6e6
Hery <hery.cresenity@gmail.com>
2019-04-20 16:16:56 +0700

    move block logic from $.cresenity to cresenity

--- media/js/capp.js
+++ media/js/capp.js
@@ -1720,39 +1720,17 @@ if (!window.cresenity) {
 
         },
         blockPage: function () {
-            $.blockUI({
-                message: '<div class="sk-folding-cube sk-primary"><div class="sk-cube1 sk-cube"></div><div class="sk-cube2 sk-cube"></div><div class="sk-cube4 sk-cube"></div><div class="sk-cube3 sk-cube"></div></div><h5 style="color: #444">LOADING...</h5>',
-                css: {
-                    backgroundColor: 'transparent',
-                    border: '0',
-                    zIndex: 9999999
-                },
-                overlayCSS: {
-                    backgroundColor: '#fff',
-                    opacity: 0.8,
-                    zIndex: 9999990
-                }
-            });
+            cresenity.blockPage();
         },
         unblockPage: function () {
-            $.unblockUI();
+            cresenity.unblockPage();
         },
         blockElement: function (selector) {
-            $(selector).block({
-                message: '<div class="sk-wave sk-primary"><div class="sk-rect sk-rect1"></div> <div class="sk-rect sk-rect2"></div> <div class="sk-rect sk-rect3"></div> <div class="sk-rect sk-rect4"></div> <div class="sk-rect sk-rect5"></div></div>',
-                css: {
-                    backgroundColor: 'transparent',
-                    border: '0'
-                },
-                overlayCSS: {
-                    backgroundColor: '#fff',
-                    opacity: 0.8
-                }
-            });
+            cresenity.blockElement(selector);
 
         },
         unblockElement: function (selector) {
-            $(selector).unblock();
+            cresenity.unblockElement(selector);
         },
     }
     String.prototype.format_currency = function () {
