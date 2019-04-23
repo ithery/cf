bc5b289c1db7573961126653c223135dfea57463
Hery <hery.cresenity@gmail.com>
2019-04-24 01:53:21 +0700

    add CGit

---
+++ modules/cresenity/libraries/CGit.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Apr 24, 2019, 1:53:09 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+
+class CGit {
+    
+}
