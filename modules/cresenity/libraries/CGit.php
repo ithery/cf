9cef390f5703212810ce9c2df1f31ec76bbcac00
Hery <hery.cresenity@gmail.com>
2019-04-24 01:56:20 +0700

    add get Repo

--- modules/cresenity/libraries/CGit.php
+++ modules/cresenity/libraries/CGit.php
@@ -7,7 +7,16 @@ defined('SYSPATH') OR die('No direct access allowed.');
  * @since Apr 24, 2019, 1:53:09 AM
  * @license Ittron Global Teknologi <ittron.co.id>
  */
-
 class CGit {
-    
+
+    /**
+     * 
+     * @param string $path
+     * @return CGit_Repository
+     */
+    public static function getRepository($path) {
+        $client = new CGit_Client();
+        return $client->getRepository($path);
+    }
+
 }
