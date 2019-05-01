d6b631f489d376b2f0254d671f9be5c2190b6b09
Hery <hery.cresenity@gmail.com>
2019-05-01 23:42:26 +0700

    update interface support

--- system/libraries/CInterface/Htmlable.php
+++ system/libraries/CInterface/Htmlable.php
@@ -4,16 +4,15 @@ defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Feb 16, 2018, 11:16:18 PM
+ * @since May 1, 2019, 11:39:29 PM
  * @license Ittron Global Teknologi <ittron.co.id>
  */
-interface CInterface_Jsonable {
+interface CInterface_Htmlable {
 
     /**
-     * Convert the object to its JSON representation.
+     * Get content as a string of HTML.
      *
-     * @param  int  $options
      * @return string
      */
-    public function toJson($options = 0);
+    public function toHtml();
 }
