3d6272d9a319ef868796672b41fa135aeebfa03a
Hery <hery.cresenity@gmail.com>
2019-05-02 00:57:12 +0700

    add class File to CResources

---
+++ modules/cresenity/libraries/CResources/Helpers/File.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since May 2, 2019, 12:39:25 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+class CResources_Helpers_File {
+
+    public static function renameInDirectory($fileNameWithDirectory, $newFileNameWithoutDirectory) {
+        $targetFile = pathinfo($fileNameWithDirectory, PATHINFO_DIRNAME) . '/' . $newFileNameWithoutDirectory;
+        rename($fileNameWithDirectory, $targetFile);
+        return $targetFile;
+    }
+
+    public static function getHumanReadableSize($sizeInBytes) {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        if ($sizeInBytes == 0) {
+            return '0 ' . $units[1];
+        }
+        for ($i = 0; $sizeInBytes > 1024; $i++) {
+            $sizeInBytes /= 1024;
+        }
+        return round($sizeInBytes, 2) . ' ' . $units[$i];
+    }
+
+    public static function getMimetype($path) {
+        $finfo = new Finfo(FILEINFO_MIME_TYPE);
+        return $finfo->file($path);
+    }
+
+}
