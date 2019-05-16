c9eddacfd5387de2d4c7d8124ba91d72e7e43be8
Hery <hery.cresenity@gmail.com>
2019-05-16 22:41:07 +0700

    add CEmoji

---
+++ modules/cresenity/libraries/CEmoji/AbstractAdapter.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since May 16, 2019, 9:57:36 PM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+abstract class CEmoji_AbstractAdapter implements CEmoji_AdapterInterface {
+    
+}
