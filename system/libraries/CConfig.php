55846157b9d2a2bcb0e2bb321663e1965a76b138
Hery <hery.cresenity@gmail.com>
2019-05-17 01:38:36 +0700

    add function to CConfig for get flatten information which config is loaded

--- system/libraries/CConfig.php
+++ system/libraries/CConfig.php
@@ -74,4 +74,79 @@ class CConfig {
         return $this;
     }
 
+    /**
+     * flatten the config array, retrieves information of default value and file which config is created
+     * 
+     * @throws CException
+     */
+    public function getConfigData() {
+        $files = CF::findFile('config', $this->group);
+
+        //add backward compatibility
+        //TODO: remove folder config in DOCROOT
+        if (!is_array($files)) {
+            $files = array();
+        }
+        if (file_exists(DOCROOT . 'config' . DS . $this->group . EXT)) {
+            $files[] = DOCROOT . 'config' . DS . $this->group . EXT;
+        }
+        //reverse ordering to set priority
+        if ($files == null) {
+            //var_dump(debug_backtrace());
+            //throw new CF_Exception('file config '.$group.' not found');
+            $files = array();
+        }
+
+        $resultFiles = array();
+        $resultData = array();
+        foreach ($files as $file) {
+            $cfg = include $file;
+            if (!is_array($cfg)) {
+                //backward compatibility with older config
+                if (isset($config)) {
+                    $cfg = $config;
+                }
+            }
+            if (!is_array($cfg)) {
+                //there is an invalid format
+                throw new CException("Invalid config format in :file", array(':file' => $file));
+            }
+            $cfgFiles = $cfg;
+            foreach ($cfgFiles as $key => $val) {
+                $cfgFiles[$key] = $file;
+            }
+            $resultFiles = carr::merge($resultFiles, $cfgFiles);
+            $resultData = carr::merge($resultData, $cfg);
+        }
+
+        //we will flatten the array of result Data
+        $result = array();
+        $addToResult = function($key, $value, &$result) use($resultFiles) {
+            $keyParts = explode('.', $key);
+            $resultData = array();
+            $resultData['key'] = $key;
+            $resultData['value'] = $value;
+            $resultData['file'] = carr::get($resultFiles, carr::first($keyParts));
+
+            $result[] = $resultData;
+        };
+        $flatten = function($array, $keyPath = '') use (&$flatten, $addToResult, &$result) {
+            foreach ($array as $key => $value) {
+                $keyDotted = strlen($keyPath) == 0 ? $key : $keyPath . '.' . $key;
+                if (is_array($value)) {
+                    if (carr::isAssoc($value)) {
+                        //need to be flatten
+                        $flatten($value, $keyDotted);
+                    } else {
+                        $addToResult($keyDotted, $value, $result);
+                    }
+                } else {
+                    $addToResult($keyDotted, $value, $result);
+                }
+            }
+        };
+        $flatten($resultData);
+        return $result;
+    }
+
 }
