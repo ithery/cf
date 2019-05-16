8012dda5d9f11c44590dbc3ec18e0cdcf203722a
Hery <hery.cresenity@gmail.com>
2019-05-16 16:23:26 +0700

    add League/OAuth1/Client libraries

---
+++ modules/cresenity/libraries/CSocialLogin/OAuth1/Provider/TwitterProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since May 16, 2019, 4:15:48 PM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
