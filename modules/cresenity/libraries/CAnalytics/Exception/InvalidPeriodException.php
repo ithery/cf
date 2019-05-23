f98670391cf67da5aa223e75265652381e0901b9
hery <hery@ittron.co.id>
2019-05-24 01:40:11 +0700

    add CAnalytics

---
+++ modules/cresenity/libraries/CAnalytics/Exception/InvalidPeriodException.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since May 24, 2019, 1:24:10 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+
