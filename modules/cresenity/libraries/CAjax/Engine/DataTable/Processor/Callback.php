b9cc7f3450e8f5070fce969188d8a73080d655e9
Hery <hery@ittron.co.id>
2019-05-31 02:35:16 +0700

    update logic dataTable callback processor

--- modules/cresenity/libraries/CAjax/Engine/DataTable/Processor/Callback.php
+++ modules/cresenity/libraries/CAjax/Engine/DataTable/Processor/Callback.php
@@ -25,17 +25,92 @@ class CAjax_Engine_DataTable_Processor_Callback extends CAjax_Engine_DataTable_P
         $params['options'] = carr::get($data, 'callback_options');
         $resultCallback = call_user_func_array($callback, $params);
 
-        $tableData = carr::get($resultCallback, 'data', array());
+        $data = carr::get($resultCallback, 'data', array());
         $totalRecord = carr::get($resultCallback, 'total_record', count($tableData));
         $totalFilteredRecord = carr::get($resultCallback, 'total_filtered_record', count($tableData));
 
-        $rowActionList = $table->getRowActionList();
+        
         $js = '';
-        if (count($rowActionList) > 0) {
-            foreach ($tableData as $kRow => $row) {
+        $output = array(
+            "sEcho" => intval(carr::get($request, 'sEcho')),
+            "iTotalRecords" => $totalRecord,
+            "iTotalDisplayRecords" => $totalFilteredRecord,
+            "aaData" => array(),
+        );
+        $no = carr::get($request, 'iDisplayStart', 0);
+        foreach ($data as $row) {
+            $arr = array();
+            $no++;
+            $key = carr::get($row, $table->key_field, '');
+
+
+            if ($table->numbering) {
+                $arr[] = $no;
+            }
+
+            if ($table->checkbox) {
+                $arr[] = '<input type="checkbox" name="' . $table->id() . '-check[]" id="' . $table->id() . '-' . $key . '" value="' . $key . '" class="checkbox-' . $table->id() . '">';
+            }
+            foreach ($table->columns as $col) {
+                $col_found = false;
+                $new_v = "";
+                $col_v = "";
+                $ori_v = "";
+                //do print from query
+                foreach ($row as $k => $v) {
+                    if ($k == $col->getFieldname()) {
+                        $col_v = $v;
+                        $ori_v = $col_v;
+                        foreach ($col->transforms as $trans) {
+                            $col_v = $trans->execute($col_v);
+                        }
+                    }
+                }
+                //if formatted
+                if (strlen($col->getFormat()) > 0) {
+                    $temp_v = $col->getFormat();
+                    foreach ($row as $k2 => $v2) {
+                        if (strpos($temp_v, "{" . $k2 . "}") !== false) {
+                            $temp_v = str_replace("{" . $k2 . "}", $v2, $temp_v);
+                        }
+                        $col_v = $temp_v;
+                    }
+                }
+
+                $new_v = $col_v;
+
+                if (($table->cellCallbackFunc) != null) {
+                    $new_v = CFunction::factory($table->cellCallbackFunc)
+                            ->addArg($table)
+                            ->addArg($col->getFieldname())
+                            ->addArg($row)
+                            ->addArg($new_v)
+                            ->setRequire($table->requires)
+                            ->execute();
+
+                    if (is_array($new_v) && isset($new_v['html']) && isset($new_v['js'])) {
+                        $js .= $new_v['js'];
+                        $new_v = $new_v['html'];
+                    }
+                }
+                $class = "";
+                switch ($col->getAlign()) {
+                    case CConstant::ALIGN_LEFT:
+                        $class .= " align-left";
+                        break;
+                    case CConstant::ALIGN_RIGHT:
+                        $class .= " align-right";
+                        break;
+                    case CConstant::ALIGN_CENTER:
+                        $class .= " align-center";
+                        break;
+                }
+                $arr[] = $new_v;
+            }
+            if ($rowActionList != null && $rowActionList->childCount() > 0) {
                 $html = new CStringBuilder();
-                $key = carr::get($row, $keyField);
-                $html->appendln('<td class="low-padding align-center cell-action td-action">')->inc_indent()->br();
+
+                $html->appendln('<td class="low-padding align-center cell-action td-action ">')->inc_indent()->br();
                 foreach ($row as $k => $v) {
                     $jsparam[$k] = $v;
                 }
@@ -51,7 +126,9 @@ class CAjax_Engine_DataTable_Processor_Callback extends CAjax_Engine_DataTable_P
                 if (($table->filterActionCallbackFunc) != null) {
                     $actions = $rowActionList->childs();
 
-                    foreach ($actions as $action) {
+                    foreach ($actions as &$action) {
+                        $action->removeClass('d-none');
+
                         $visibility = CFunction::factory($table->filterActionCallbackFunc)
                                 ->addArg($table)
                                 ->addArg($col->getFieldname())
@@ -60,18 +137,27 @@ class CAjax_Engine_DataTable_Processor_Callback extends CAjax_Engine_DataTable_P
                                 ->setRequire($table->requires)
                                 ->execute();
 
+
+                        if ($visibility == false) {
+                            $action->addClass('d-none');
+                        }
                         $action->setVisibility($visibility);
                     }
+
+
+                    //call_user_func($this->cellCallbackFunc,$this,$col->get_fieldname(),$row,$v);
                 }
 
-                $html->appendln($table->getRowActionList()->html($html->get_indent()));
+                $html->appendln($table->getRowActionList()->html($html->getIndent()));
                 $js .= $table->getRowActionList()->js();
                 $html->decIndent()->appendln('</td>')->br();
 
-                $tableData[$kRow][] = $html->text();
-                $tableData[$kRow]["DT_RowId"] = $key;
+                $arr[] = $html->text();
+                $arr["DT_RowId"] = $key;
             }
+            $output["aaData"][] = $arr;
         }
+
 
 
 
