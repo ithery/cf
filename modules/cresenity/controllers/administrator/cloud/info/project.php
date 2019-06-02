b11fe8fa09b91d049ab01d3215adc4f86ccdefcc
Hery <hery.cresenity@gmail.com>
2019-06-03 00:18:13 +0700

    update project information

--- modules/cresenity/controllers/administrator/cloud/info/project.php
+++ modules/cresenity/controllers/administrator/cloud/info/project.php
@@ -27,22 +27,43 @@ class Controller_Administrator_Cloud_Info_Project extends CApp_Administrator_Con
         $widgetTeams = $divCardColumns->addWidget()->setTitle('Teams')->addClass('card mb-3')->setNoPadding();
         $listGroupTeams = $widgetTeams->addListGroup();
         $listGroupTeams->setDataFromArray(carr::get($cloudData, 'project.teams'));
-        $listGroupTeams->setItemCallback(function($item,$data) {
+        $listGroupTeams->setItemCallback(function($item, $data) {
             $divMedia = $item->addDiv()->addClass('media pb-1 mb-3');
-            $divMedia->addImg()->setSrc(carr::get($data,'photoUrl'))->addClass('d-block ui-w-40 rounded-circle');
+            $divMedia->addImg()->setSrc(carr::get($data, 'photoUrl'))->addClass('d-block ui-w-40 rounded-circle');
             $divMediaBody = $divMedia->addDiv()->addClass('media-body flex-truncate ml-3');
-            $divMediaBody->addSpan()->addClass('badge badge-outline-success')->add(carr::get($data,'username'));
-            
-            
+            $divMediaBody->addSpan()->addClass('badge badge-outline-success')->add(carr::get($data, 'username'));
         }, __FILE__);
-        
-        $widgetApp = $divCardColumns->addWidget()->setTitle('App Available')->addClass('card mb-3')->setNoPadding();
-        
-        
+
+        $widgetApplications = $divCardColumns->addWidget()->setTitle('App Available')->addClass('card mb-3')->setNoPadding();
+        $listGroupApplications = $widgetApplications->addListGroup();
+        $listGroupApplications->setDataFromArray(carr::get($cloudData, 'project.applications'));
+        $listGroupApplications->setItemCallback(function($item, $data) {
+            $divMedia = $item->addDiv()->addClass('media pb-1 mb-3');
+            $divMedia->addImg()->setSrc(carr::get($data, 'imageUrl'))->addClass('d-block ui-w-40 rounded-circle');
+            $divMediaBody = $divMedia->addDiv()->addClass('media-body flex-truncate ml-3');
+            $divMediaBody->addSpan()->addClass('badge badge-outline-success')->add(carr::get($data, 'appName'));
+        }, __FILE__);
+
         $widgetMilestone = $divCardColumns->addWidget()->setTitle('Milestone')->addClass('card mb-3')->setNoPadding();
+        $listGroupMilestone = $widgetMilestone->addListGroup();
+        $listGroupMilestone->setDataFromArray(carr::get($cloudData, 'project.milestones'));
+        $listGroupMilestone->setItemCallback(function($item, $data) {
+            $divMedia = $item->addDiv()->addClass('media pb-1 mb-3');
+            $divMedia->addImg()->setSrc(carr::get($data, 'imageUrl'))->addClass('d-block ui-w-40 rounded-circle');
+            $divMediaBody = $divMedia->addDiv()->addClass('media-body flex-truncate ml-3');
+            $divMediaBody->addSpan()->addClass('badge badge-outline-success')->add(carr::get($data, 'name'));
+        }, __FILE__);
+
 
         $widgetEnvironment = $divCardColumns->addWidget()->setTitle('Environment')->addClass('card mb-3')->setNoPadding();
-
+        $listGroupEnvironment = $widgetEnvironment->addListGroup();
+        $listGroupEnvironment->setDataFromArray(carr::get($cloudData, 'project.environments'));
+        $listGroupEnvironment->setItemCallback(function($item, $data) {
+            $divMedia = $item->addDiv()->addClass('media pb-1 mb-3');
+            $divMedia->addImg()->setSrc(carr::get($data, 'imageUrl'))->addClass('d-block ui-w-40 rounded-circle');
+            $divMediaBody = $divMedia->addDiv()->addClass('media-body flex-truncate ml-3');
+            $divMediaBody->addSpan()->addClass('badge badge-outline-success')->add(carr::get($data, 'name'));
+        }, __FILE__);
         echo $app->render();
     }
 
--- modules/cresenity/libraries/CElement/Component/ListGroup.php
+++ modules/cresenity/libraries/CElement/Component/ListGroup.php
@@ -29,8 +29,8 @@ class CElement_Component_ListGroup extends CElement_Component {
         return $item;
     }
 
-    public function setItemCallback($callback,$require='') {
-        $this->itemCallback=$callback;
+    public function setItemCallback($callback, $require = '') {
+        $this->itemCallback = $callback;
         $this->itemCallbackRequire = $require;
         return $this;
     }
@@ -38,10 +38,12 @@ class CElement_Component_ListGroup extends CElement_Component {
     public function build() {
         $this->addClass('list-group');
         $this->setAttr('role', 'tablist');
-        foreach ($this->tableData as $rowData) {
-            $item = $this->addItem()->setData($rowData);
-            if ($this->itemCallback != null) {
-                $item->setCallback($this->itemCallback,$this->itemCallbackRequire);
+        if (is_array($this->tableData)) {
+            foreach ($this->tableData as $rowData) {
+                $item = $this->addItem()->setData($rowData);
+                if ($this->itemCallback != null) {
+                    $item->setCallback($this->itemCallback, $this->itemCallbackRequire);
+                }
             }
         }
     }
