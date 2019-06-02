eefa6324405cf5d1f3df4827f5e9976362569fc3
Hery <hery.cresenity@gmail.com>
2019-06-03 03:18:44 +0700

    add Ajax Engine ListGroup

---
+++ modules/cresenity/libraries/CAjax/Engine/ListGroup.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Jun 3, 2019, 3:18:29 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
