b22e489daf7686584a0c792946e035562e32b6de
Hery <hery.cresenity@gmail.com>
2019-06-03 01:48:25 +0700

    add element kanban list

---
+++ modules/cresenity/libraries/CElement/Component/Kanban/List.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @since Jun 3, 2019, 1:46:52 AM
+ * @license Ittron Global Teknologi <ittron.co.id>
+ */
+
+class CElement_Component_ListGroup_Item extends CElement_Component_Widget {
+    public function __construct($id) {
+        parent::__construct($id);
+    }
+    public function build() {
+        parent::build();
+    }
+    
+}
