c3fbaec55bd4864034fcf17a10d27029ce3f01a8
Hery <hery@ittron.co.id>
2019-12-24 10:40:36 +0700

    add CBase system library

---
+++ system/libraries/CBase.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use Symfony\Component\PropertyAccess\Exception\NoSuchIndexException;
+use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+
+class CBase {
+
+    public static function createRecursionContext() {
+        return new CBase_RecursionContext();
+    }
+
+}
