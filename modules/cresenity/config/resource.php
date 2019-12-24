12ab6cf444be5b225c649a425a8cc624ab5df725
Hery <hery@ittron.co.id>
2019-12-24 13:53:43 +0700

    update config resources to camel case

--- modules/cresenity/config/resource.php
+++ modules/cresenity/config/resource.php
@@ -17,21 +17,22 @@ return array(
      * The maximum file size of an item in bytes.
      * Adding a larger file will result in an exception.
      */
-    'max_file_size' => 1024 * 1024 * 10,
+    'maxFileSize' => 1024 * 1024 * 10,
     /*
      * This queue will be used to generate derived and responsive images.
      * Leave empty to use the default queue.
      */
-    'queue_name' => '',
+    'queueName' => '',
     /*
      * The fully qualified class name of the media model.
      */
+    'resourceModel' => 'CApp_Model_Resource',
     'resource_model' => 'CApp_Model_Resource',
     's3' => [
-        /*
-         * The domain that should be prepended when generating urls.
-         */
-        //'domain' => 'https://' . env('AWS_BUCKET') . '.s3.amazonaws.com',
+    /*
+     * The domain that should be prepended when generating urls.
+     */
+    //'domain' => 'https://' . env('AWS_BUCKET') . '.s3.amazonaws.com',
     ],
     'remote' => [
         /*
@@ -46,7 +47,7 @@ return array(
             'CacheControl' => 'max-age=604800',
         ],
     ],
-    'responsive_images' => [
+    'responsiveImages' => [
         /*
          * This class is responsible for calculating the target widths of the responsive
          * images. By default we optimize for filesize and create variations that each are 20%
@@ -59,44 +60,44 @@ return array(
          * By default rendering media to a responsive image will add some javascript and a tiny placeholder.
          * This ensures that the browser can already determine the correct layout.
          */
-        'use_tiny_placeholders' => true,
-        /*
-         * This class will generate the tiny placeholder used for progressive image loading. By default
-         * the medialibrary will use a tiny blurred jpg image.
-         */
-        //'tiny_placeholder_generator' => Spatie\MediaLibrary\ResponsiveImages\TinyPlaceholderGenerator\Blurred::class,
+        'useTinyPlaceholders' => true,
+    /*
+     * This class will generate the tiny placeholder used for progressive image loading. By default
+     * the medialibrary will use a tiny blurred jpg image.
+     */
+    //'tiny_placeholder_generator' => Spatie\MediaLibrary\ResponsiveImages\TinyPlaceholderGenerator\Blurred::class,
     ],
     /*
      * When urls to files get generated, this class will be called. Leave empty
      * if your files are stored locally above the site root or on s3.
      */
-    'url_generator' => null,
+    'urlGenerator' => null,
     /*
      * The class that contains the strategy for determining a media file's path.
      */
-    'path_generator' => null,
+    'pathGenerator' => null,
     /*
      * Medialibrary will try to optimize all converted images by removing
      * metadata and applying a little bit of compression. These are
      * the optimizers that will be used by default.
      */
-    'image_optimizers' => [
-//        Spatie\ImageOptimizer\Optimizers\Jpegoptim::class => [
-//            '--strip-all', // this strips out all text information such as comments and EXIF data
-//            '--all-progressive', // this will make sure the resulting image is a progressive one
-//        ],
-//        Spatie\ImageOptimizer\Optimizers\Pngquant::class => [
+    'imageOptimizers' => [
+        CImage_Optimizer_Jpegoptim::class => [
+            '--strip-all', // this strips out all text information such as comments and EXIF data
+            '--all-progressive', // this will make sure the resulting image is a progressive one
+        ],
+//        CImage_Optimizer_Pngquant::class => [
 //            '--force', // required parameter for this package
 //        ],
-//        Spatie\ImageOptimizer\Optimizers\Optipng::class => [
+//        CImage_Optimizer_Optipng::class => [
 //            '-i0', // this will result in a non-interlaced, progressive scanned image
 //            '-o2', // this set the optimization level to two (multiple IDAT compression trials)
 //            '-quiet', // required parameter for this package
 //        ],
-//        Spatie\ImageOptimizer\Optimizers\Svgo::class => [
+//        CImage_Optimizer_Svgo::class => [
 //            '--disable=cleanupIDs', // disabling because it is known to cause troubles
 //        ],
-//        Spatie\ImageOptimizer\Optimizers\Gifsicle::class => [
+//        CImage_Optimizer_Gifsicle::class => [
 //            '-b', // required parameter for this package
 //            '-O3', // this produces the slowest but best results
 //        ],
@@ -104,51 +105,36 @@ return array(
     /*
      * These generators will be used to create an image of media files.
      */
-    'image_generators' => [
-//        Spatie\MediaLibrary\ImageGenerators\FileTypes\Image::class,
-//        Spatie\MediaLibrary\ImageGenerators\FileTypes\Webp::class,
-//        Spatie\MediaLibrary\ImageGenerators\FileTypes\Pdf::class,
-//        Spatie\MediaLibrary\ImageGenerators\FileTypes\Svg::class,
-//        Spatie\MediaLibrary\ImageGenerators\FileTypes\Video::class,
+    'imageGenerators' => [
+        CResources_ImageGenerator_FileType_ImageType::class,
+//        CResources_ImageGenerator_FileType_WebpType::class,
+//        CResources_ImageGenerator_FileType_PdfType::class,
+//        CResources_ImageGenerator_FileType_SvgType::class,
+//        CResources_ImageGenerator_FileType_VideoType::class,
     ],
     /*
      * The engine that should perform the image conversions.
      * Should be either `gd` or `imagick`.
      */
-    'image_driver' => 'gd',
+    'imageDriver' => 'gd',
     /*
      * FFMPEG & FFProbe binaries paths, only used if you try to generate video
      * thumbnails and have installed the php-ffmpeg/php-ffmpeg composer
      * dependency.
      */
-    'ffmpeg_path' => '/usr/bin/ffmpeg',
-    'ffprobe_path' => '/usr/bin/ffprobe',
+    'ffmpegPath' => '/usr/bin/ffmpeg',
+    'ffprobePath' => '/usr/bin/ffprobe',
     /*
      * The path where to store temporary files while performing image conversions.
      * If set to null, storage_path('medialibrary/temp') will be used.
      */
-    'temporary_directory_path' => null,
+    'temporaryDirectoryPath' => null,
     /*
      * Here you can override the class names of the jobs used by this package. Make sure
      * your custom jobs extend the ones provided by the package.
      */
-    'jobs' => [
-//        'perform_conversions' => Spatie\MediaLibrary\Jobs\PerformConversions::class,
-//        'generate_responsive_images' => Spatie\MediaLibrary\Jobs\GenerateResponsiveImages::class,
-    ],
-    
-    'disks' => [
-
-        'local' => [
-            'driver' => 'local',
-          
-        ],
-
-      
-        'cloud' => [
-            'driver' => 's3',
-          
-        ],
-
+    'taskQueue' => [
+//        'performConversions' => CResources_TaskQueue_PerformConversions::class,
+//        'generate_responsive_images' => CResources_TaskQueue_PerformConversions_GenerateResponsiveImages::class,
     ],
 );
