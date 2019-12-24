ee33fb25adbe8e52d6e1f9241f7906e26644e062
Hery <hery@ittron.co.id>
2019-12-25 01:00:05 +0700

    add CParser Tokenizer

---
+++ modules/cresenity/libraries/CParser/HtmlParser/Tokenizer/Special.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class CParser_HtmlParser_Tokenizer_Special {
+
+    const None = 1;
+    const Script = 2;
+    const Style = 3;
+
+}
