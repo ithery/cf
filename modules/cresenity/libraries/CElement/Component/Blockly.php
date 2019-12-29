b50fa7c8511cc85542be9c2f68480fd450812f11
Hery <hery@ittron.co.id>
2019-12-29 13:51:39 +0700

    add blockly js

--- modules/cresenity/libraries/CElement/Component/Blockly.php
+++ modules/cresenity/libraries/CElement/Component/Blockly.php
@@ -14,25 +14,21 @@ class CElement_Component_Blockly extends CElement_Component {
     protected $textareaXml;
     protected $toolbarAction;
     protected $blocklyWrapper;
-    
 
     public function __construct($id = "", $tag = "div") {
         parent::__construct($id, $tag);
         $this->tag = 'div';
-        $this->height = '600';
-        $this->width = '400';
         $this->toolbox = new CElement_Component_Blockly_Toolbox();
         $this->toolbar = new CElement_List_ActionList();
         $this->blocklyWrapper = new CElement_Element_Div();
-        
-        
+
+
         $action = $this->toolbar->addAction()->setLabel('Load');
     }
 
     public function build() {
         $this->addClass('capp-blockly');
-        //$this->customCss('width', $this->width . 'px');
-        $this->customCss('height', $this->height . 'px');
+        $this->blocklyWrapper->customCss('height', '600px');
         $this->add($this->toolbar);
         $this->blocklyWrapper->add($this->toolbox);
         $this->add($this->blocklyWrapper);
