62a083cc051ee37b6d87d27614c51cc5e04c7407
Hery <hery@ittron.co.id>
2020-01-06 20:49:08 +0700

    Revert "Revert "add Beanstalkd""
    This reverts commit 96a3e7ec0a2f5cd21ced0dd3cb65cfb6dba876a4.

---
+++ modules/cresenity/libraries/CServer/Service/Beanstalkd.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class CServer_Service_Beanstalkd {
+
+    public function __construct() {
+        $this->__init();
+        $this->_main();
+    }
+
+}
