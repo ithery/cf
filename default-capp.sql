c1ebdf17db309bc9396d164ce8488568cd9e8ee5
Hery <hery@ittron.co.id>
2020-01-16 14:54:00 +0700

    fix default capp sql for superadmin

--- default-capp.sql
+++ default-capp.sql
@@ -261,7 +261,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`user_id`, `role_id`, `org_id`, `store_id`, `username`, `user_photo`, `first_name`, `last_name`, `password`, `email`, `last_login`, `last_request`, `login_count`, `description`, `activation_code`, `activation_date`, `have_notification`, `is_base`, `is_disabled`, `have_issued`, `created`, `createdby`, `updated`, `updatedby`, `status`) VALUES
-	(1, 1, 15, NULL, 'superadmin', NULL, NULL, NULL, '21232f297a57a5a743894a0e4a801fc3', NULL, '2016-01-13 21:13:17', '2016-01-13 22:07:39', 195, NULL, NULL, NULL, 0, 0, 0, 1, '2015-07-24 11:10:49', NULL, NULL, NULL, 1);
+	(1, 1, NULL, NULL, 'superadmin', NULL, NULL, NULL, '21232f297a57a5a743894a0e4a801fc3', NULL, '2016-01-13 21:13:17', '2016-01-13 22:07:39', 195, NULL, NULL, NULL, 0, 0, 0, 1, '2015-07-24 11:10:49', NULL, NULL, NULL, 1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 
