79c03e92bcfc85656aa20f5f38df109f14dafa16
Hery <hery@ittron.co.id>
2020-02-10 23:40:59 +0700

    update firebase exception

--- modules/cresenity/libraries/CVendor/Firebase/Messaging/Exception/ApiConnectionFailedException.php
+++ modules/cresenity/libraries/CVendor/Firebase/Messaging/Exception/ApiConnectionFailedException.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+final class CVendor_Firebase_Messaging_Exception_ApiConnectionFailedException extends RuntimeException implements CVendor_Firebase_Messaging_ExceptionInterface {
+
+    use CVendor_Firebase_Trait_ExceptionHasRequestAndResponseTrait;
+    use CVendor_Firebase_Trait_ExceptionHasErrorsTrait;
+}
