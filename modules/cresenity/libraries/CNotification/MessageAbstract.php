fbe3d4bffd96eedd97a94f5be5ce59c51d116718
Hery <hery@ittron.co.id>
2020-03-09 20:44:49 +0700

    update notification event

--- modules/cresenity/libraries/CNotification/MessageAbstract.php
+++ modules/cresenity/libraries/CNotification/MessageAbstract.php
@@ -9,12 +9,21 @@
 abstract class CNotification_MessageAbstract implements CNotification_MessageInterface {
 
     use CTrait_HasOptions;
+    use CNotification_Trait_MessageEventTrait;
 
     protected $config;
+
+    /**
+     * The event dispatcher instance.
+     *
+     * @var CEvent_DispatcherInterface
+     */
+    protected static $dispatcher;
 
     public function __construct($config, $options) {
         $this->options = $options;
         $this->config = $config;
+        $this->dispatcher = CEvent::dispatcher();
     }
 
 }
