1d335e22d3d5ea2565f461729b87ee4e77d8abd4
Hery <hery@ittron.co.id>
2020-03-29 16:14:57 +0700

    add Deleted Trait for CModel

---
+++ system/libraries/CModel/Deleted/DeletedTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+trait CModel_Deleted_DeletedTrait {
+
+    /**
+     * Get the name of the "deleted at" column.
+     *
+     * @return string
+     */
+    public function getDeletedAtColumn() {
+        return defined('static::DELETED') ? static::DELETED : 'deleted';
+    }
+
+    /**
+     * Get the fully qualified "deleted at" column.
+     *
+     * @return string
+     */
+    public function getQualifiedDeletedAtColumn() {
+        return $this->getTable() . '.' . $this->getDeletedAtColumn();
+    }
+
+}
