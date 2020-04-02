76e7cfb350bde1ef590300ac1109f901040839e5
Hery <hery@ittron.co.id>
2020-04-02 20:21:50 +0700

    add all data in log queue for easy debugging

--- modules/cresenity/libraries/CTracker/TaskQueue/TrackQueue.php
+++ modules/cresenity/libraries/CTracker/TaskQueue/TrackQueue.php
@@ -18,7 +18,7 @@ class CTracker_TaskQueue_TrackQueue extends CTracker_TaskQueueAbstract {
         $data = carr::get($this->params, 'data');
         $config = carr::get($this->params, 'config');
 
-        $this->logDaemon('Processing Tracker Queue from ip:' . carr::get($data,'request.clientIp'));
+        $this->logDaemon('Processing Tracker Queue from ip:' . carr::get($data,'request.clientIp') .' => '.json_encode($data));
         try {
             CTracker::populator()->setData($data);
             CTracker::config()->setData($config);
