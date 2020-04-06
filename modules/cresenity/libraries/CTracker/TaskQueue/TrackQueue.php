9449e4712253e37beb784559094e2ead9488c3e0
Hery <hery@ittron.co.id>
2020-04-06 18:50:09 +0700

    update trackqueue

--- modules/cresenity/libraries/CTracker/TaskQueue/TrackQueue.php
+++ modules/cresenity/libraries/CTracker/TaskQueue/TrackQueue.php
@@ -18,7 +18,8 @@ class CTracker_TaskQueue_TrackQueue extends CTracker_TaskQueueAbstract {
         $data = carr::get($this->params, 'data');
         $config = carr::get($this->params, 'config');
 
-        $this->logDaemon('Processing Tracker Queue from ip:' . carr::get($data,'request.clientIp') .' => '.json_encode($data).', config:'.json_encode($config));
+        //$this->logDaemon('Processing Tracker Queue from ip:' . carr::get($data,'request.clientIp') .' => '.json_encode($data).', config:'.json_encode($config));
+        $this->logDaemon('Processing Tracker Queue from ip:' . carr::get($data,'request.clientIp'));
         try {
             CTracker::populator()->setData($data);
             CTracker::config()->setData($config);
