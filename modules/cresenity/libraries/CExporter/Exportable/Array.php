61ddcec9321631dc8bd36f12a7122c2f4dd145dd
Hery <hery@ittron.co.id>
2020-04-10 12:34:55 +0700

    add exportable array

---
+++ modules/cresenity/libraries/CExporter/Exportable/Array.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class CExporter_Exportable_Collection extends CExporter_Exportable implements CExporter_Concern_FromArray {
+
+    protected $array;
+
+    /**
+     * 
+     * @param array $array
+     */
+    public function __construct(array $array) {
+        $this->array = $array;
+    }
+
+    /**
+     * 
+     * @inherit
+     */
+    public function getArray() {
+        return $this->array;
+    }
+
+}
