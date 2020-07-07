2455ab0c0265bbd27db4f9ede8de9d4cdf045fbb
Hery <hery@ittron.co.id>
2020-07-07 13:20:41 +0700

    add look feeature php info for server info trait

---
+++ modules/cresenity/libraries/CTrait/Controller/Application/Server/PhpInfo.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan <hery@itton.co.id>
+ * @since Jul 7, 2020 
+ * @license Ittron Global Teknologi
+ */
+trait CTrait_Controller_Application_Server_PhpInfo {
+
+    public function phpinfo() {
+        $app = CApp::instance();
+
+        $app->title(clang::__("PHP Info"));
+
+        $html = CView::factory('admin/page/phpinfo/html');
+        $html = $html->render();
+        $js = CView::factory('admin/page/phpinfo/js');
+        $js = $js->render();
+        $app->add($html);
+        $app->addJs($js);
+
+        echo $app->render();
+    }
+
+}
