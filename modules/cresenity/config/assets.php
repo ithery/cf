fb579c58c6073870ed1cd9f8232dad05da3f0c1b
Hery <hery@ittron.co.id>
2020-08-04 14:06:12 +0700

    add config assets

---
+++ modules/cresenity/config/assets.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan <hery@itton.co.id>
+ * @since Aug 4, 2020 
+ * @license Ittron Global Teknologi
+ */
+return [
+    'css' => [
+        'compile' => false,
+        'disk'=>'local',
+    ],
+];
