1bcb1fb744f7fc2e5f3a5bb1ccb4763b1355d929
Ecko <ecko.santoso@gmail.com>
2020-08-04 13:58:17 +0700

    add asset version

--- modules/cresenity/libraries/CManager/Asset/Helper.php
+++ modules/cresenity/libraries/CManager/Asset/Helper.php
@@ -13,17 +13,23 @@ class CManager_Asset_Helper {
         //return CResource::instance('css')->url($file);
         $docroot = str_replace(DS, "/", DOCROOT);
         $file = str_replace(DS, "/", $file);
+        $path = carr::first(explode('?',$file));
+        
         $base_url = curl::base();
         if (CManager::instance()->isMobile()) {
             $base_url = curl::base(false, 'http');
         }
         $file = str_replace($docroot, $base_url, $file);
+        
+        $separator = parse_url($file, PHP_URL_QUERY) ? '&' : '?';
+        $file.=$separator.'v='.filemtime($path);
 
         return $file;
     }
 
     public static function urlJsFile($file) {
-        
+        $path = $file;
+        $path = carr::first(explode('?',$file));
         $docroot = str_replace(DS, "/", DOCROOT);
         $file = str_replace(DS, "/", $file);
         $base_url = curl::base();
@@ -33,6 +39,9 @@ class CManager_Asset_Helper {
         }
         
         $file = str_replace($docroot, $base_url, $file);
+
+        $separator = parse_url($file, PHP_URL_QUERY) ? '&' : '?';
+        $file.=$separator.'v='.filemtime($path);
 
         return $file;
     }
