ccd9c5f5794085ca4f82246d4064670076833091
Hery <hery@ittron.co.id>
2020-11-29 21:26:36 +0700

    add DotEnv lib

---
+++ system/vendor/Dotenv/Repository/Adapter/ApacheAdapter.php
@@ -0,0 +1,89 @@
+<?php
+
+
+
+namespace Dotenv\Repository\Adapter;
+
+use PhpOption\None;
+use PhpOption\Option;
+use PhpOption\Some;
+
+final class ApacheAdapter implements AdapterInterface
+{
+    /**
+     * Create a new apache adapter instance.
+     *
+     * @return void
+     */
+    private function __construct()
+    {
+        //
+    }
+
+    /**
+     * Create a new instance of the adapter, if it is available.
+     *
+     * @return \PhpOption\Option<\Dotenv\Repository\Adapter\AdapterInterface>
+     */
+    public static function create()
+    {
+        if (self::isSupported()) {
+            /** @var \PhpOption\Option<AdapterInterface> */
+            return Some::create(new self());
+        }
+
+        return None::create();
+    }
+
+    /**
+     * Determines if the adapter is supported.
+     *
+     * This happens if PHP is running as an Apache module.
+     *
+     * @return bool
+     */
+    private static function isSupported()
+    {
+        return \function_exists('apache_getenv') && \function_exists('apache_setenv');
+    }
+
+    /**
+     * Read an environment variable, if it exists.
+     *
+     * @param string $name
+     *
+     * @return \PhpOption\Option<string>
+     */
+    public function read( $name)
+    {
+        /** @var \PhpOption\Option<string> */
+        return Option::fromValue(apache_getenv($name))->filter(static function ($value) {
+            return \is_string($value) && $value !== '';
+        });
+    }
+
+    /**
+     * Write to an environment variable, if possible.
+     *
+     * @param string $name
+     * @param string $value
+     *
+     * @return bool
+     */
+    public function write( $name,  $value)
+    {
+        return apache_setenv($name, $value);
+    }
+
+    /**
+     * Delete an environment variable, if possible.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function delete( $name)
+    {
+        return apache_setenv($name, '');
+    }
+}
