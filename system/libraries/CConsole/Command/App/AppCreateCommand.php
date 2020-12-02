f3aec23ed73f497caa96900858cf1c7f3417e146
hery <hery@ittron.co.id>
2020-12-03 03:30:34 +0700

    update AppCreateCommand

--- system/libraries/CConsole/Command/App/AppCreateCommand.php
+++ system/libraries/CConsole/Command/App/AppCreateCommand.php
@@ -6,42 +6,224 @@
  * and open the template in the editor.
  */
 
-class CConsole_Command_App_AppPresetCommand extends CConsole_Command {
+class CConsole_Command_App_AppCreateCommand extends CConsole_Command {
 
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:create {code} {--domain=?} {--prefix=?}';
+    protected $signature = 'app:create {code} {--domain=} {--prefix=} {--title=}';
+    protected $allOptions;
 
     public function handle() {
+        CConsole::devSuiteRequired($this);
 
         $appCode = $this->argument('code');
-        $prefix = $this->option('prefix');
+
+        $allOptions = $this->allOptions();
+        $domain = carr::get($allOptions, 'domain');
+        $prefix = carr::get($allOptions, 'prefix');
+
+        if ($domain == null) {
+            $defaultDomain = $appCode . '.test';
+            if (CF::domainExists($defaultDomain)) {
+                $domain = $defaultDomain;
+            } else {
+                $domain = $this->ask("Please input the domain for create this application", $defaultDomain);
+            }
+        }
+        if (CConsole::domain() != $domain) {
+            $this->call("domain:switch", [
+                'domain' => $domain,
+                '--no-interaction' => carr::get($this->allOptions(), 'no-interaction'),
+            ]);
+        }
+        if (!CF::domainExists($domain)) {
+            $this->call("domain:create", [
+                'domain' => $domain,
+                '--appCode' => $appCode,
+                '--no-interaction' => carr::get($this->allOptions(), 'no-interaction'),
+            ]);
+        }
+        if ($prefix == null) {
+            $defaultPrefix = cstr::toupper(substr($appCode, 0, 2));
+            if ($defaultPrefix == 'CF') {
+                $defaultPrefix = 'CC';
+            }
+
+            if (strlen(CF::config('app.prefix')) == 0) {
+                $prefix = $this->ask("Please input the prefix for this application", $defaultPrefix);
+            }
+        }
+
+
+        if ($prefix == 'CF') {
+            $this->error('Prefix CF is not available');
+            return CConsole::FAILURE_EXIT;
+        }
+
+
 
         $this->ensureAppDirectoryExists($appCode);
-        $this->buildBootstrapFiles($appCode);
+        $this->buildDefaultConfig($appCode, $prefix);
+
+        CConfig::instance('app')->refresh();
+        $prefix = CF::config('app.prefix');
+
+        //reload prefix
+        $this->buildMedia($appCode);
+        $this->buildTheme($appCode);
+        $this->buildLibraries($appCode, $prefix);
+
+        $this->buildViews($appCode);
         $this->buildControllers($appCode);
+        $this->buildNav($appCode);
+        $this->buildBootstrapFiles($appCode);
+
+
+        $this->devSuiteLinking($appCode);
+
+        $this->info('Application ' . $appCode . ' created sucessfully');
+        $this->call('devsuite:open', ['name' => $appCode]);
     }
 
-    public function buildControllers($appCode) {
-        $dir = $this->appDefaultPath($appCode).'controllers'.DS;
-        if(!CFile::isDirectory($dir)) {
-            CFile::makeDirectory($dir);
-        }
-        $homeController = $dir.'home.php';
-        if(!CFile::exists($homeController)) {
-            $this->call('make:controller home');
-        }
+    public function devSuiteLinking($appCode) {
+        $this->call('devsuite:link', ['name' => $appCode]);
     }
 
     public function buildBootstrapFiles($appCode) {
-        $path = $this->appPath($appCode) . 'bootstrap.php';
-        $content = CF::FILE_SECURITY . PHP_EOL . PHP_EOL;
-        if (!CFile::exists($path)) {
-            CFile::put($path, $content);
+        $bootstrapFile = $this->appPath($appCode) . 'bootstrap.php';
+        $stubFile = CF::findFile('stubs', 'bootstrap', true, 'stub');
+        if (!$stubFile) {
+            $this->error('bootstrap stub not found');
+            exit(1);
         }
+        $content = CFile::get($stubFile);
+        $content = str_replace('{theme}', $appCode, $content);
+        CFile::put($bootstrapFile, $content);
+        $this->info('Bootstrap ' . basename($bootstrapFile) . ' created on ' . $bootstrapFile);
+    }
+
+    public function buildViews($appCode) {
+        $viewsDir = $this->appDefaultPath($appCode) . 'views' . DS;
+        $this->ensureDirectoryExists($viewsDir);
+    }
+
+    public function buildLibraries($appCode, $prefix) {
+        $librariesDir = $this->appDefaultPath($appCode) . 'libraries' . DS;
+        $this->ensureDirectoryExists($librariesDir);
+
+        $baseFile = $librariesDir . $prefix . EXT;
+        if (!CFile::exists($baseFile)) {
+
+            $stubFile = CF::findFile('stubs', 'libraries/base/base', true, 'stub');
+            if (!$stubFile) {
+                $this->error('base stub not found');
+                exit(1);
+            }
+            $content = CFile::get($stubFile);
+            $content = str_replace('{prefix}', $prefix, $content);
+            CFile::put($baseFile, $content);
+            $this->info('Libraries ' . basename($baseFile) . ' created on ' . $baseFile);
+        }
+        //create base model
+        $baseModelFile = $librariesDir . $prefix . 'Model' . EXT;
+        $baseModelDir = $librariesDir . $prefix . 'Model';
+        $this->ensureDirectoryExists($baseModelDir);
+        if (!CFile::exists($baseModelFile)) {
+
+            $stubFile = CF::findFile('stubs', 'libraries/base/model', true, 'stub');
+            if (!$stubFile) {
+                $this->error('base model stub not found');
+                exit(1);
+            }
+            $content = CFile::get($stubFile);
+            $content = str_replace('{prefix}', $prefix, $content);
+            CFile::put($baseModelFile, $content);
+            $this->info('Libraries ' . basename($baseModelFile) . ' created on ' . $baseModelFile);
+        }
+
+
+
+        //create base utils
+        $baseUtilsFile = $librariesDir . $prefix . 'Utils' . EXT;
+        if (!CFile::exists($baseUtilsFile)) {
+
+            $stubFile = CF::findFile('stubs', 'libraries/base/utils', true, 'stub');
+            if (!$stubFile) {
+                $this->error('base utils stub not found');
+                exit(1);
+            }
+            $content = CFile::get($stubFile);
+            $content = str_replace('{prefix}', $prefix, $content);
+            CFile::put($baseUtilsFile, $content);
+            $this->info('Libraries ' . basename($baseUtilsFile) . ' created on ' . $baseUtilsFile);
+        }
+
+        //create base controllers
+        $baseControllerFile = $librariesDir . $prefix . 'Controller' . EXT;
+        $baseControllerDir = $librariesDir . $prefix . 'Controller';
+        $this->ensureDirectoryExists($baseControllerDir);
+        if (!CFile::exists($baseControllerFile)) {
+
+            $stubFile = CF::findFile('stubs', 'libraries/base/controller', true, 'stub');
+            if (!$stubFile) {
+                $this->error('base controller stub not found');
+                exit(1);
+            }
+            $content = CFile::get($stubFile);
+            $content = str_replace('{prefix}', $prefix, $content);
+            CFile::put($baseControllerFile, $content);
+            $this->info('Libraries ' . basename($baseControllerFile) . ' created on ' . $baseControllerFile);
+        }
+    }
+
+    public function buildTheme($appCode) {
+        $this->call('make:theme', [
+            'theme' => $appCode,
+            '--no-interaction' => carr::get($this->allOptions(), 'no-interaction'),
+        ]);
+    }
+
+    public function buildMedia($appCode) {
+        $mediaDir = $this->appDefaultPath($appCode) . 'media' . DS;
+        $jsDir = $mediaDir . 'js' . DS;
+        $cssDir = $mediaDir . 'css' . DS;
+        $imgDir = $mediaDir . 'img' . DS;
+
+        $this->ensureDirectoryExists($mediaDir);
+        $this->ensureDirectoryExists($jsDir);
+        $this->ensureDirectoryExists($cssDir);
+        $this->ensureDirectoryExists($imgDir);
+    }
+
+    public function buildNav($appCode) {
+
+        $this->call('make:nav', [
+            'nav' => 'nav',
+            '--no-interaction' => carr::get($this->allOptions(), 'no-interaction'),
+        ]);
+    }
+
+    public function buildControllers($appCode) {
+        $this->call('make:controller', [
+            'controller' => 'home',
+            '--no-interaction' => carr::get($this->allOptions(), 'no-interaction'),
+        ]);
+    }
+
+    public function buildDefaultConfig($appCode, $prefix) {
+
+
+        $valueOptions = [
+            'prefix' => $prefix,
+        ];
+        $this->call('make:config', [
+            'config' => 'app',
+            '--value' => json_encode($valueOptions),
+            '--no-interaction' => carr::get($this->allOptions(), 'no-interaction'),
+        ]);
     }
 
     /**
@@ -50,13 +232,9 @@ class CConsole_Command_App_AppPresetCommand extends CConsole_Command {
      */
     public function ensureAppDirectoryExists($appCode) {
         $appPath = $this->appPath($appCode);
-        if (!CFile::isDirectory($appPath)) {
-            CFile::makeDirectory($path);
-        }
+        $this->ensureDirectoryExists($appPath);
         $appDefaultPath = $this->appDefaultPath($appCode);
-        if (!CFile::isDirectory($appDefaultPath)) {
-            CFile::makeDirectory($appDefaultPath);
-        }
+        $this->ensureDirectoryExists($appDefaultPath);
     }
 
     /**
@@ -75,7 +253,21 @@ class CConsole_Command_App_AppPresetCommand extends CConsole_Command {
      * @return string
      */
     public function appDefaultPath($appCode) {
-        return DOCROOT . 'application' . DS . $appCode . 'default' . DS;
+        return DOCROOT . 'application' . DS . $appCode . DS . 'default' . DS;
+    }
+
+    public function allOptions() {
+        if ($this->allOptions == null) {
+            $this->allOptions = $this->option();
+        }
+        return $this->allOptions;
+    }
+
+    protected function ensureDirectoryExists($directory) {
+        if (!CFile::isDirectory($directory)) {
+            CFile::makeDirectory($directory);
+            $this->info("Directory $directory created");
+        }
     }
 
 }
--- system/libraries/CConsole/Command/Make/MakeControllerCommand.php
+++ system/libraries/CConsole/Command/Make/MakeControllerCommand.php
@@ -25,8 +25,12 @@ class CConsole_Command_Make_MakeControllerCommand extends CConsole_Command {
         if (!CFile::isDirectory($controllerPath)) {
             CFile::makeDirectory($controllerPath);
         }
+        $prefix = CF::config('app.prefix');
+        if(strlen($prefix)==0) {
+            $prefix='C';
+        }
 
-
+        
         $file = array_pop($controllers);
         foreach ($controllers as $segment) {
             $controllerPath .= $segment . DS;
@@ -49,6 +53,8 @@ class CConsole_Command_Make_MakeControllerCommand extends CConsole_Command {
         }
         $content = CFile::get($stubFile);
         $content = str_replace('{ControllerClass}', $controllerClass, $content);
+        $content = str_replace('{prefix}', $prefix, $content);
+ 
         CFile::put($controllerFile, $content);
 
         $this->info('Controller ' . $controller . ' created on:' . $controllerFile);
