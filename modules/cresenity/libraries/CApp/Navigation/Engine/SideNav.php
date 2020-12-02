f208d2b4089356719b071bdd8affeeefe86310ef
hery <hery@ittron.co.id>
2020-12-02 20:18:06 +0700

    update nav now support key uri

--- modules/cresenity/libraries/CApp/Navigation/Engine/SideNav.php
+++ modules/cresenity/libraries/CApp/Navigation/Engine/SideNav.php
@@ -22,7 +22,7 @@ class CApp_Navigation_Engine_SideNav extends CApp_Navigation_Engine {
         }
         $html = "";
         $childCount = 0;
-        
+
         foreach ($navs as $d) {
 
             $child = 0;
@@ -33,22 +33,26 @@ class CApp_Navigation_Engine_SideNav extends CApp_Navigation_Engine {
             $label = carr::get($d, 'label');
             $icon = carr::get($d, 'icon');
             $class = carr::get($d, 'class');
-            
+
             $childHtml = "";
 
             if (isset($d["subnav"]) && is_array($d["subnav"])) {
                 $childHtml .= self::render(carr::get($d, 'subnav', array()), $level + 1, $child);
             }
+            $url = carr::get($d, 'uri');
+            if ($url == null) {
+                $url = Helper::url($d);
+            }
 
-            $url = Helper::url($d);
-
-            if (!isset($url) || $url == null)
+            if (!isset($url) || $url == null) {
                 $url = "";
+            }
+
+
 
             if (strlen($childHtml) > 0 || strlen($url) > 0) {
 
                 if (!Helper::accessAvailable($d, CF::appId(), $domain)) {
-
                     continue;
                 }
                 if (isset($d["controller"]) && $d["controller"] != "") {
@@ -107,6 +111,7 @@ class CApp_Navigation_Engine_SideNav extends CApp_Navigation_Engine {
                     }
                     $elem .= "</a>\r\n";
                 } else {
+                    $url = '/' . trim($url, '/');
                     $target = "";
                     $notif = "";
                     if (isset($d["target"]) && strlen($d["target"]) > 0) {
