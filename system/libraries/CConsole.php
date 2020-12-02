ceb658cf4c8e3c24d34c9a8ecf7598221e1d1a11
hery <hery@ittron.co.id>
2020-12-03 00:35:02 +0700

    add requirement for CConsole

--- system/libraries/CConsole.php
+++ system/libraries/CConsole.php
@@ -8,8 +8,35 @@
 
 class CConsole {
 
+    const SUCCESS_EXIT = 0;
+    const FAILURE_EXIT = 1;
+    const EXCEPTION_EXIT = 2;
+
     public static function domain() {
         return CF::cliDomain();
+    }
+
+    public static function domainRequired($console) {
+        $domain = CConsole::domain();
+        if (strlen($domain) == 0) {
+            $console->error('Domain is not set');
+            $console->error('Please set your domain using domain:switch command');
+            exit(static::FAILURE_EXIT);
+        }
+        if (!CF::domainExists($domain)) {
+            $console->error('Domain '.$domain.' is not exists on data file');
+            $console->error('Please recheck your domain or create the domain using domain:create command');
+            exit(static::FAILURE_EXIT);
+            
+        }
+    }
+
+    public static function devSuiteRequired($console) {
+        if (!CDevSuite::isInstalled()) {
+            $console->error('devsuite is not installed');
+            $console->error('Please install devsuite using devsuite:install command');
+            exit(static::FAILURE_EXIT);
+        }
     }
 
 }
