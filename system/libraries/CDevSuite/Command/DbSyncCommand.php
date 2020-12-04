968938cab9cd6c60dd8900eefa13640ab933d4df
hery <hery@ittron.co.id>
2020-12-04 15:59:28 +0700

    add option force to sync

--- system/libraries/CDevSuite/Command/DbSyncCommand.php
+++ system/libraries/CDevSuite/Command/DbSyncCommand.php
@@ -8,19 +8,22 @@
 class CDevSuite_Command_DbSyncCommand extends CDevSuite_CommandAbstract {
 
     public function getSignatureArguments() {
-        return '{--from=} {--to=}';
+        return '{--from=} {--to=} {--force=}';
     }
 
     public function run(CConsole_Command $cfCommand) {
         $from = $cfCommand->option('from');
         $to = $cfCommand->option('to');
+        $force = $cfCommand->option('force');
         CDevSuite::db()->existsOrExit($from);
         CDevSuite::db()->existsOrExit($to);
         
         CDevSuite::db()->compare($from, $to);
-
-        $choice = $cfCommand->choice('Are you sure execute sql on '.$to.':',['No','Yes'],0);
-        if($choice=='Yes') {
+        $choice = true;
+        if(!$force) {
+            $choice = $cfCommand->confirm('Are you sure execute sql on '.$to.':',false);
+        }
+        if($choice) {
             CDevSuite::db()->sync($from,$to);
         } else {
             CDevSuite::info('User cancelled command');
