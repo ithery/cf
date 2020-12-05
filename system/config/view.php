deab4dd67c99da87905df4a9e853a96c48b5f9e0
Hery <hery@ittron.co.id>
2020-12-06 04:57:20 +0700

    add config view

---
+++ system/config/view.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan <hery@itton.co.id>
+ * @since Dec 6, 2020 
+ * @license Ittron Global Teknologi
+ */
+return [
+    'compiled' => DOCROOT . 'temp' . DS . 'views' . DS . 'blade',
+];
