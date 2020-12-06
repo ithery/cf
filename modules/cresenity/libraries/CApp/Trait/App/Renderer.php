3c6c5d5aa8787e49f57c29086d8da96887d3f489
Hery <hery@ittron.co.id>
2020-12-06 09:46:17 +0700

    update with alpine js

--- modules/cresenity/libraries/CApp/Trait/App/Renderer.php
+++ modules/cresenity/libraries/CApp/Trait/App/Renderer.php
@@ -17,6 +17,25 @@ trait CApp_Trait_App_Renderer {
         return carr::get($viewData, 'content');
     }
 
+    public function renderNavigation($expression = null) {
+        if ($expression != null) {
+            
+            $expression = str_replace(['(', ')'], '', $expression);
+            $expression = str_replace(['"', '\''], '', $expression);
+            $expression = str_replace(',', ' ', $expression);
+        }
+        
+        $nav = $expression;
+        if ($nav == null) {
+            $nav = $this->nav;
+        }
+
+        $nav = $this->resolveNav($nav);
+        
+        $renderer = $this->resolveNavRenderer();
+        return $renderer->render($nav);
+    }
+
     public function renderStyles($options = []) {
         $viewData = $this->getViewData();
         $cresCss = curl::base() . 'media/js/cres/dist/cres.css?v=' . md5(CFile::lastModified(DOCROOT . 'media/js/cres/dist/cres.css'));
@@ -25,9 +44,28 @@ trait CApp_Trait_App_Renderer {
         $alpineScript = '<script src="'.$alpineJs.'"></script>';
         $cresStyle = '<link href="'.$cresCss.'" rel="stylesheet" />'.PHP_EOL;
 
-        return $alpineScript.$cresStyle.carr::get($viewData, 'head_client_script');
+        $allStyles=carr::get($viewData, 'head_client_script');
         
-        
+        return <<<HTML
+<style>
+    [cf\:loading], [cf\:loading\.delay], [cf\:loading\.inline-block], [cf\:loading\.inline], [cf\:loading\.block], [cf\:loading\.flex], [cf\:loading\.table], [cf\:loading\.grid] {
+        display: none;
+    }
+    [cf\:offline] {
+        display: none;
+    }
+    [cf\:dirty]:not(textarea):not(input):not(select) {
+        display: none;
+    }
+    input:-webkit-autofill, select:-webkit-autofill, textarea:-webkit-autofill {
+        animation-duration: 50000s;
+        animation-name: livecfautofill;
+    }
+    @keyframes livecfautofill { from {} }
+</style>
+${cresStyle}
+${allStyles}
+HTML;
     }
 
     public function renderScripts($options = []) {
@@ -38,11 +76,37 @@ trait CApp_Trait_App_Renderer {
         $js = carr::get($viewData, 'js', '');
         $customJs = carr::get($viewData, 'custom_js', '');
 
-
+        $alpineJs = curl::base() . 'media/js/libs/alpine.js?v=' . md5(CFile::lastModified(DOCROOT . 'media/js/libs/alpine.js'));
+        $alpineScript = '<script src="'.$alpineJs.'"></script>';
+        
         $cresJs = curl::base() . 'media/js/cres/dist/cres.js?v=' . md5(CFile::lastModified(DOCROOT . 'media/js/cres/dist/cres.js'));
         return <<<HTML
             ${endClientScript}
             <script src="${cresJs}"></script>
+            <script>
+                window.cresenity = new Cresenity();
+                window.cresenity.init();
+                
+                if (window.Alpine) {
+                    /* Defer showing the warning so it doesn't get buried under downstream errors. */
+                    document.addEventListener("DOMContentLoaded", function () {
+                        setTimeout(function() {
+                            console.warn("Cresenity: It looks like AlpineJS has already been loaded. Make sure Creseniity\'s scripts are loaded before Alpine.")
+                        })
+                    });
+                }
+                /* Make Alpine wait until Livewire is finished rendering to do its thing. */
+                window.deferLoadingAlpine = function (callback) {
+                    window.addEventListener('cresenity:load', function () {
+                        callback();
+                    });
+                };
+                document.addEventListener("DOMContentLoaded", function () {
+                    window.cresenity.ui.start();
+                });
+
+            </script>
+            <script src="${alpineJs}"></script>
             <script>
                 ${js}
                 ${readyClientScript}
@@ -85,7 +149,7 @@ HTML;
             $viewData = array();
             $this->content = $this->element->html();
             $this->js = $this->element->js();
-            
+
             $viewData['content'] = $this->content;
             $viewData['header_body'] = $this->header_body;
             $viewData['headerBody'] = $this->header_body;
@@ -112,17 +176,17 @@ HTML;
 
             $js .= PHP_EOL . $this->js . $additional_js;
             $jsScriptFile = '';
-            
+
             if ($this->isUseRequireJs()) {
-                
+
                 $js = $asset->renderJsRequire($js);
             } else {
-               
-                $jsScriptFile = PHP_EOL.'<script>' . $asset->varJs() . '</script>';
-                $jsScriptFile .= PHP_EOL.'<script>if(typeof define === "function") define=undefined;</script>';
+
+                $jsScriptFile = PHP_EOL . '<script>' . $asset->varJs() . '</script>';
+                $jsScriptFile .= PHP_EOL . '<script>if(typeof define === "function") define=undefined;</script>';
                 //$jsScriptFile .= '<script src="/media/js/capp.js?v='.uniqid().'"></script>';
-                $jsScriptFile .= PHP_EOL.$asset->render(CManager_Asset::POS_END, CManager_Asset::TYPE_JS_FILE);
-                
+                $jsScriptFile .= PHP_EOL . $asset->render(CManager_Asset::POS_END, CManager_Asset::TYPE_JS_FILE);
+
                 $js = $asset->wrapJs($js, true);
             }
 
