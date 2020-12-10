cd766b8d64c788c4c97f927805222e208b995f10
hery <hery@ittron.co.id>
2020-12-11 00:15:01 +0700

    add middleware bootstrapper class

---
+++ system/libraries/CBootstrap/MiddlewareBootstrapper.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Description of MiddlewareBootstrapper
+ *
+ * @author Hery
+ */
+class CBootstrap_MiddlewareBootstrapper extends CBootstrap_BootstrapperAbstract {
+
+    /**
+     * Bootstrap the given application.
+     *
+     * @return void
+     */
+    public function bootstrap() {
+        $sessionMiddleware = new CSession_Middleware_SessionMiddleware();
+        CMiddleware::manager()->pushMiddleware($sessionMiddleware);
+    }
+
+}
