fd598e32438756f0f9176a61f357b7b52db876a3
hery <hery@ittron.co.id>
2020-12-11 00:18:36 +0700

    update session library (not done)

---
+++ system/libraries/CSession/Handler/RedisSessionHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of RedisSessionHandler
+ *
+ * @author Hery
+ */
+class CSession_Handler_RedisSessionHandler extends CSession_Driver_CacheBased {
+    
+}
---
+++ system/libraries/CSession/Manager.php
@@ -0,0 +1,296 @@
+<?php
+
+/**
+ * Description of Manager
+ *
+ * @author Hery
+ */
+class CSession_Manager {
+
+    /**
+     *
+     * @var CSession_Manager 
+     */
+    private static $instance;
+
+    /**
+     * The array of created "drivers".
+     *
+     * @var array
+     */
+    protected $drivers = [];
+
+    /**
+     * 
+     * @return CSession_Manager
+     */
+    public static function instance() {
+        if (static::$instance == null) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+
+    public function __construct() {
+        $this->config = CConfig::instance('session');
+    }
+
+    /**
+     * Get a driver instance.
+     *
+     * @param  string|null  $driver
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function driver($driver = null) {
+        $driver = $driver ?: $this->getDefaultDriver();
+
+        if (is_null($driver)) {
+            throw new InvalidArgumentException(sprintf(
+                            'Unable to resolve NULL driver for [%s].', static::class
+            ));
+        }
+
+        // If the given driver has not been created before, we will create the instances
+        // here and cache it so we can return it next time very quickly. If there is
+        // already a driver created by this name, we'll just return that instance.
+        if (!isset($this->drivers[$driver])) {
+            $this->drivers[$driver] = $this->createDriver($driver);
+        }
+
+        return $this->drivers[$driver];
+    }
+
+    /**
+     * Create a new driver instance.
+     *
+     * @param  string  $driver
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function createDriver($driver) {
+        // First, we will determine if a custom driver creator exists for the given driver and
+        // if it does not we will check for a creator method for the driver. Custom creator
+        // callbacks allow developers to build their own "drivers" easily using Closures.
+        if (isset($this->customCreators[$driver])) {
+            return $this->callCustomCreator($driver);
+        } else {
+            $method = 'create' . cstr::studly($driver) . 'Driver';
+
+            if (method_exists($this, $method)) {
+                return $this->$method();
+            }
+        }
+
+        throw new InvalidArgumentException("Driver [$driver] not supported.");
+    }
+
+    /**
+     * Create an instance of the "null" session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createNullDriver() {
+        return $this->buildSession(new CSession_Handler_NullSessionHandler);
+    }
+
+    /**
+     * Create an instance of the "array" session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createArrayDriver() {
+        return $this->buildSession(new CSession_Handler_ArraySessionHandler(
+                                $this->config->get('expiration')
+        ));
+    }
+
+    /**
+     * Create an instance of the "cookie" session driver.
+     *
+     * @return CSession_Store
+     */
+    protected function createCookieDriver() {
+        return $this->buildSession(new CSession_Handler_CookieSessionHandler(
+                                CHTTP::cookie(), $this->config->get('expiration')
+        ));
+    }
+
+    /**
+     * Create an instance of the file session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createFileDriver() {
+        return $this->createNativeDriver();
+    }
+
+    /**
+     * Create an instance of the file session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createNativeDriver() {
+        $lifetime = $this->config->get('expiration');
+
+        return $this->buildSession(new CSession_Handler_FileSessionHandler($this->config->get('storage'), $lifetime));
+    }
+
+    /**
+     * Create an instance of the database session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createDatabaseDriver() {
+        $table = $this->config->get('table', 'session');
+
+        $lifetime = $this->config->get('expiration');
+
+        return $this->buildSession(new CSession_Handler_DatabaseSessionHandler(
+                                $this->getDatabaseConnection(), $table, $lifetime
+        ));
+    }
+
+    /**
+     * Get the database connection for the database driver.
+     *
+     * @return CDatabase
+     */
+    protected function getDatabaseConnection() {
+        $connection = $this->config->get('storage');
+
+        return CDatabase::instance($connection);
+    }
+
+    /**
+     * Create an instance of the APC session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createApcDriver() {
+        return $this->createCacheBased('apc');
+    }
+
+    /**
+     * Create an instance of the Memcached session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createMemcachedDriver() {
+        return $this->createCacheBased('memcached');
+    }
+
+    /**
+     * Create an instance of the Redis session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createRedisDriver() {
+        $handler = $this->createCacheHandler('redis');
+
+        $redis = CRedis::instance(CF::config('session.storage'));
+        $driver = new CCache_Driver_RedisDriver($redis);
+        $redisStore = new CCache_Repository($driver);
+
+        $handler->getCache()->getStore()->setConnection(
+                $this->config->get('storage')
+        );
+
+        return $this->buildSession($handler);
+    }
+
+    /**
+     * Create an instance of the DynamoDB session driver.
+     *
+     * @return \Illuminate\Session\Store
+     */
+    protected function createDynamodbDriver() {
+        return $this->createCacheBased('dynamodb');
+    }
+
+    /**
+     * Create an instance of a cache driven driver.
+     *
+     * @param  string  $driver
+     * @return \Illuminate\Session\Store
+     */
+    protected function createCacheBased($driver) {
+        return $this->buildSession($this->createCacheHandler($driver));
+    }
+
+    /**
+     * Create the cache based session handler instance.
+     *
+     * @param  string  $driver
+     * @return \Illuminate\Session\CacheBasedSessionHandler
+     */
+    protected function createCacheHandler($driver) {
+        $store = $this->config->get('session.store') ?: $driver;
+
+        return new CacheBasedSessionHandler(
+                clone $this->container->make('cache')->store($store),
+                $this->config->get('session.lifetime')
+        );
+    }
+
+    /**
+     * Build the session instance.
+     *
+     * @param  \SessionHandlerInterface  $handler
+     * @return \Illuminate\Session\Store
+     */
+    protected function buildSession($handler) {
+        return $this->config->get('encrypt') ? $this->buildEncryptedSession($handler) : new CSession_Store($this->config->get('name'), $handler);
+    }
+
+    /**
+     * Build the encrypted session instance.
+     *
+     * @param  \SessionHandlerInterface  $handler
+     * @return \Illuminate\Session\EncryptedStore
+     */
+    protected function buildEncryptedSession($handler) {
+        return new EncryptedStore(
+                $this->config->get('session.cookie'), $handler, $this->container['encrypter']
+        );
+    }
+
+    /**
+     * Determine if requests for the same session should wait for each to finish before executing.
+     *
+     * @return bool
+     */
+    public function shouldBlock() {
+        return $this->config->get('session.block', false);
+    }
+
+    /**
+     * Get the name of the cache store / driver that should be used to acquire session locks.
+     *
+     * @return string|null
+     */
+    public function blockDriver() {
+        return $this->config->get('session.block_store');
+    }
+
+    /**
+     * Get the session configuration.
+     *
+     * @return array
+     */
+    public function getSessionConfig() {
+        return $this->config->all();
+    }
+
+    /**
+     * Get the default session driver name.
+     *
+     * @return string
+     */
+    public function getDefaultDriver() {
+        return $this->config->get('driver', 'native');
+    }
+
+}
