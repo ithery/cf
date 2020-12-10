6a6109794c5074ad56153a8a6205dcef3debafa9
hery <hery@ittron.co.id>
2020-12-11 00:17:06 +0700

    update middleware

--- system/libraries/CMiddleware.php
+++ system/libraries/CMiddleware.php
@@ -7,3 +7,20 @@ defined('SYSPATH') OR die('No direct access allowed.');
  * @since Sep 7, 2018, 11:34:26 PM
  * @license Ittron Global Teknologi <ittron.co.id>
  */
+final class CMiddleware {
+
+    
+    /**
+     * 
+     * @return CMiddleware_Manager
+     */
+    public static function manager() {
+        return CMiddleware_Manager::instance();
+    }
+
+    
+    public static function middleware() {
+   
+        return static::manager()->getMiddleware();
+    }
+}
