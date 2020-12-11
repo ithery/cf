9ada0253387c06d4af4a735245b17ccfc213f016
hery <hery@ittron.co.id>
2020-12-11 15:30:55 +0700

    update cache default to file

--- system/libraries/CCache.php
+++ system/libraries/CCache.php
@@ -20,19 +20,39 @@ class CCache {
      * @param array $options
      * @return CCache_Repository
      */
-    public static function repository($options = array()) {
-        $defaultOptions = array(
-            'driver' => 'array',
-        );
-
-        $options = array_merge($defaultOptions, $options);
-
-        $instanceKey = carr::hash($options);
+    public static function repository($name = null) {
+        $options = static::resolveConfig($name);
+        $instanceKey = $name;
+        if (!is_string($name)) {
+            $instanceKey = carr::hash($options);
+        }
         if (!isset(self::$repository[$instanceKey])) {
 
             self::$repository[$instanceKey] = new CCache_Repository($options);
         }
         return self::$repository[$instanceKey];
+    }
+
+    public static function resolveConfig($name) {
+        $config = CConfig::instance('cache');
+        $options = null;
+        if (is_array($name)) {
+            $options = $name;
+        }
+        if ($options == null) {
+            if ($name = null) {
+                $name = $config->get('default');
+            }
+
+            $options = $config->get($name);
+        }
+
+
+        $defaultOptions = array(
+            'driver' => 'array',
+        );
+
+        return $options ? $options : $defaultOptions;
     }
 
 }
