7a1f61489fdc18bd927f70f9f22d0dfc68b544da
Hery <hery@ittron.co.id>
2020-12-20 23:06:22 +0700

    add KernelInterface

---
+++ system/libraries/CHTTP/KernelInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+interface CHTTP_KernelInterface {
+    /**
+     * Bootstrap the application for HTTP requests.
+     *
+     * @return void
+     */
+    public function bootstrap();
+
+    /**
+     * Handle an incoming HTTP request.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle($request);
+
+    /**
+     * Perform any final actions for the request lifecycle.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request  $request
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     *
+     * @return void
+     */
+    public function terminate($request, $response);
+}
