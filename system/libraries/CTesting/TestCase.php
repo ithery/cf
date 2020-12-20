0d8b8ad4352e0b0786663996393e16efa383f610
Hery <hery@ittron.co.id>
2020-12-20 23:06:33 +0700

    add Basic TestCase for CF

---
+++ system/libraries/CTesting/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class CTesting_TestCase extends BaseTestCase {
+    /**
+     * Creates the application.
+     *
+     * Needs to be implemented by subclasses.
+     *
+     * @return \Symfony\Component\HttpKernel\HttpKernelInterface
+     */
+    protected function createApplication() {
+    }
+}
