cb779cfae9c45cff9a6f2bf19615bebef12d2e13
hery <hery@ittron.co.id>
2021-01-13 03:04:36 +0700

    remove deprecated function store and store_id on CApp

--- modules/cresenity/libraries/CTrait/Compat/App.php
+++ modules/cresenity/libraries/CTrait/Compat/App.php
@@ -1,40 +1,42 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Feb 16, 2018, 5:40:40 AM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Feb 16, 2018, 5:40:40 AM
  */
+//@codingStandardsIgnoreStart
 trait CTrait_Compat_App {
-
     /**
-     *
      * @var stdClass|null
+     *
      * @deprecated
      */
     private $_store = null;
 
     /**
-     *
      * @var stdClass|null
+     *
      * @deprecated
      */
     private $_admin = null;
 
     /**
-     *
      * @var stdClass|null
+     *
      * @deprecated
      */
     private $_member = null;
 
     /**
-     * 
      * @deprecated since version 1.2, please use function addBreadcrumb
+     *
      * @param string $caption
      * @param string $url
+     *
      * @return CApp
      */
     public function add_breadcrumb($caption, $url) {
@@ -42,8 +44,8 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated since version 1.2, please use function appId
+     *
      * @return int
      */
     public function app_id() {
@@ -51,9 +53,10 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated since version 1.2, please use function showTitle
+     *
      * @param bool $bool
+     *
      * @return $this
      */
     public function show_title($bool) {
@@ -61,9 +64,10 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated since version 1.2, please use function showBreadcrumb
+     *
      * @param bool $bool
+     *
      * @return $this
      */
     public function show_breadcrumb($bool) {
@@ -71,8 +75,8 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated since version 1.2, please use function isAdmin
+     *
      * @return bool
      */
     public function is_admin() {
@@ -80,8 +84,8 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated since version 1.2, please use function orgId
+     *
      * @return bool
      */
     public function org_id() {
@@ -89,9 +93,10 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated since version 1.2, please use function isAdmin
+     *
      * @param bool $bool
+     *
      * @return $this
      */
     public function set_login_required($bool) {
@@ -99,9 +104,10 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated since version 1.2
+     *
      * @param string $js
+     *
      * @return $this
      */
     public function add_custom_js($js) {
@@ -109,10 +115,11 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated since version 1.2
+     *
      * @param int $roleId
      * @param int $orgId
+     *
      * @return array
      */
     public function get_role_child_list($roleId = null, $orgId = null) {
@@ -120,9 +127,10 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
      * @deprecated
+     *
      * @param array $data
+     *
      * @return $this
      */
     public function set_custom_data($data) {
@@ -131,14 +139,14 @@ trait CTrait_Compat_App {
 
     /**
      * @deprecated
+     *
      * @param string $module
      */
     public function register_client_module($module) {
-        CManager::instance()->register_module($module);
+        CManager::instance()->registerModule($module);
     }
 
     /**
-     * 
      * @return bool
      */
     public function is_user_login() {
@@ -149,43 +157,43 @@ trait CTrait_Compat_App {
      * @deprecated
      */
     public function set_view_html() {
-        
     }
 
     /**
-     * 
      * @return type
+     *
      * @deprecated
      */
     public function admin() {
         if ($this->_admin == null) {
             $session = CSession::instance();
-            $admin = $session->get("admin");
-            if (!$admin)
+            $admin = $session->get('admin');
+            if (!$admin) {
                 $admin = null;
+            }
             $this->_admin = $admin;
         }
         return $this->_admin;
     }
 
     /**
-     * 
      * @return deprecated
      */
     public function member() {
         if ($this->_member = null) {
             $session = CSession::instance();
-            $member = $session->get("member");
-            if (!$member)
+            $member = $session->get('member');
+            if (!$member) {
                 $member = null;
+            }
             $this->_member = $member;
         }
         return $this->_member;
     }
 
     /**
-     * 
-     * @return boolean
+     * @return bool
+     *
      * @deprecated
      */
     public function is_admin_login() {
@@ -193,40 +201,12 @@ trait CTrait_Compat_App {
     }
 
     /**
-     * 
-     * @return boolean
+     * @return bool
+     *
      * @deprecated
      */
     public function is_member_login() {
         return $this->member() != null;
     }
-
-    /**
-     * 
-     * @return stdClass
-     * @deprecated
-     */
-    public function store() {
-        if ($this->_store == null) {
-            $store_id = CF::store_id();
-
-            if ($store_id != "") {
-                $this->_store = cstore::get(CF::orgCode(), CF::store_code());
-            }
-        }
-        return $this->_store;
-    }
-
-    /**
-     * 
-     * @return int
-     */
-    public function store_id() {
-        $store = $this->store();
-        if ($store == null) {
-            return null;
-        }
-        return $store->store_id;
-    }
-
 }
+//@codingStandardsIgnoreEnd
