d3ff8605d02f38193cffdc412c70cff74649347a
hery <hery@ittron.co.id>
2021-01-13 04:22:34 +0700

    update utils trait

--- system/libraries/CBase/Trait/UtilsTrait.php
+++ system/libraries/CBase/Trait/UtilsTrait.php
@@ -18,4 +18,40 @@ trait CBase_Trait_UtilsTrait {
         }
         return $prefix . $folder . '_' . $name;
     }
+
+    public static function formatCurrency($value) {
+        return number_format($value, 2);
+    }
+
+    public static function formatNumber($value) {
+        return number_format($value, 0);
+    }
+
+    public static function unformatCurrency($number, $force_number = true, $dec_point = '.', $thousands_sep = ',') {
+        if ($force_number) {
+            $number = preg_replace('/^[^\d]+/', '', $number);
+        } elseif (preg_match('/^[^\d]+/', $number)) {
+            return false;
+        }
+        $type = (strpos($number, $dec_point) === false) ? 'int' : 'float';
+        $number = str_replace([$dec_point, $thousands_sep], ['.', ''], $number);
+        settype($number, $type);
+        return $number;
+    }
+
+    public static function unformatNumber($number, $force_number = true, $dec_point = '.', $thousands_sep = ',') {
+        if ($force_number) {
+            $number = preg_replace('/^[^\d]+/', '', $number);
+        } elseif (preg_match('/^[^\d]+/', $number)) {
+            return false;
+        }
+        $type = (strpos($number, $dec_point) === false) ? 'int' : 'float';
+        $number = str_replace([$dec_point, $thousands_sep], ['.', ''], $number);
+        settype($number, $type);
+        return $number;
+    }
+
+    public static function formatDate($date) {
+        return ctransform::format_date($date);
+    }
 }
