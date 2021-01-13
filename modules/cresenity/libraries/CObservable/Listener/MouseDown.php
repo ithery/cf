1b8ee994179d581db120396660d8b96430227e34
hery <hery@ittron.co.id>
2021-01-13 15:48:48 +0700

    coding standards

--- modules/cresenity/libraries/CObservable/Listener/MouseDown.php
+++ modules/cresenity/libraries/CObservable/Listener/MouseDown.php
@@ -1,18 +1,17 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Apr 20, 2019, 3:12:36 PM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Apr 20, 2019, 3:12:36 PM
  */
 
 class CObservable_Listener_MouseDownListener extends CObservable_Listener {
-
     public function __construct($owner) {
         parent::__construct($owner);
         $this->event = 'mousedown';
     }
-
 }
