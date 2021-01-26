4813d2e8b2294d115e2e045e43829ba9eb93806a
hery <hery@ittron.co.id>
2021-01-27 01:42:04 +0700

    lint

--- media/js/cres/src/cui/MessageBus.js
+++ media/js/cres/src/cui/MessageBus.js
@@ -1,24 +1,24 @@
 
 export default class MessageBus {
     constructor() {
-        this.listeners = {}
+        this.listeners = {};
     }
 
     register(name, callback) {
-        if (! this.listeners[name]) {
-            this.listeners[name] = []
+        if (!this.listeners[name]) {
+            this.listeners[name] = [];
         }
 
-        this.listeners[name].push(callback)
+        this.listeners[name].push(callback);
     }
 
     call(name, ...params) {
         (this.listeners[name] || []).forEach(callback => {
-            callback(...params)
-        })
+            callback(...params);
+        });
     }
 
     has(name) {
-        return Object.keys(this.listeners).includes(name)
+        return Object.keys(this.listeners).includes(name);
     }
 }
