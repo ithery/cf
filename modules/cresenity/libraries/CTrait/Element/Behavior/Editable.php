c8228299fc57dbbc3b70ab7905652c967664cacb
hery <hery@ittron.co.id>
2021-01-26 22:23:59 +0700

    fix Editable Behaviour trait

--- modules/cresenity/libraries/CTrait/Element/Behavior/Editable.php
+++ modules/cresenity/libraries/CTrait/Element/Behavior/Editable.php
@@ -1,44 +1,40 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Feb 17, 2018, 1:30:41 AM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Feb 17, 2018, 1:30:41 AM
  */
 trait CTrait_Element_Behavior_Editable {
-
     /**
-     *
      * @var bool
      */
     protected $editable;
 
     /**
-     * 
      * @param bool $bool
+     *
      * @return $this
      */
-    public function setEditable($bool) {
-        $this->editable = true;
+    public function setEditable($bool = true) {
+        $this->editable = $bool;
         return $this;
     }
 
     /**
-     * 
      * @return bool
      */
-    public function getEditble() {
+    public function getEditable() {
         return $this->editable;
     }
 
     /**
-     * 
      * @return bool
      */
     public function isEditable() {
         return $this->editable == true;
     }
-
 }
