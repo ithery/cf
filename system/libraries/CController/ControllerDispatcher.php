416cf57de6cd752c54c041d22742f579eb454ac9
hery <hery@ittron.co.id>
2021-01-27 03:55:35 +0700

    psr

--- system/libraries/CController/ControllerDispatcher.php
+++ system/libraries/CController/ControllerDispatcher.php
@@ -5,11 +5,9 @@
  *
  * @author Hery
  */
-class CController_ControllerDispatcher /*implements ControllerDispatcherContract*/ {
-
+class CController_ControllerDispatcher /*implements ControllerDispatcherContract*/
+{
     use CRouting_Concern_RouteDependencyResolverTrait;
-
-   
 
     /**
      * Create a new controller dispatcher instance.
@@ -17,38 +15,39 @@ class CController_ControllerDispatcher /*implements ControllerDispatcherContract
      * @return void
      */
     public function __construct() {
-
     }
 
     /**
      * Dispatch a request to a given controller and method.
      *
-     * @param  CRouting_Route  $route
-     * @param  mixed  $controller
-     * @param  string  $method
+     * @param CRouting_Route $route
+     * @param mixed          $controller
+     * @param string         $method
+     *
      * @return mixed
      */
     public function dispatch(CRouting_Route $route, $controller, $method) {
-        
         $parameters = $this->resolveClassMethodDependencies(
-                $route->parametersWithoutNulls(), $controller, $method
+            $route->parametersWithoutNulls(),
+            $controller,
+            $method
         );
 
-        
         if (method_exists($controller, 'callAction')) {
             return $controller->callAction($method, $parameters);
         }
-        
-        $response =  $controller->{$method}(...array_values($parameters));
-        
+
+        $response = $controller->{$method}(...array_values($parameters));
+
         return $response;
     }
 
     /**
      * Get the middleware for the controller instance.
      *
-     * @param  \Illuminate\Routing\Controller  $controller
-     * @param  string  $method
+     * @param \Illuminate\Routing\Controller $controller
+     * @param string                         $method
+     *
      * @return array
      */
     public function getMiddleware($controller, $method) {
@@ -56,21 +55,21 @@ class CController_ControllerDispatcher /*implements ControllerDispatcherContract
             return [];
         }
 
-        return collect($controller->getMiddleware())->reject(function ($data) use ($method) {
-                    return static::methodExcludedByOptions($method, $data['options']);
-                })->pluck('middleware')->all();
+        return c::collect($controller->getMiddleware())->reject(function ($data) use ($method) {
+            return static::methodExcludedByOptions($method, $data['options']);
+        })->pluck('middleware')->all();
     }
 
     /**
      * Determine if the given options exclude a particular method.
      *
-     * @param  string  $method
-     * @param  array  $options
+     * @param string $method
+     * @param array  $options
+     *
      * @return bool
      */
     protected static function methodExcludedByOptions($method, array $options) {
-        return (isset($options['only']) && !in_array($method, (array) $options['only'])) ||
-                (!empty($options['except']) && in_array($method, (array) $options['except']));
+        return (isset($options['only']) && !in_array($method, (array) $options['only']))
+                || (!empty($options['except']) && in_array($method, (array) $options['except']));
     }
-
 }
