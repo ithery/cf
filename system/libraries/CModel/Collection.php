31501e6c7cb52d44f34b098fd251507f45ef96a5
hery <hery@ittron.co.id>
2021-01-26 21:55:05 +0700

    update CModel_Collection to work with aggregate and refactor some logic

--- system/libraries/CModel/Collection.php
+++ system/libraries/CModel/Collection.php
@@ -14,6 +14,10 @@ class CModel_Collection extends CCollection implements CQueue_QueueableCollectio
             $key = $key->getKey();
         }
 
+        if ($key instanceof CInterface_Arrayable) {
+            $key = $key->toArray();
+        }
+
         if (is_array($key)) {
             if ($this->isEmpty()) {
                 return new static;
@@ -40,10 +44,219 @@ class CModel_Collection extends CCollection implements CQueue_QueueableCollectio
                 $relations = func_get_args();
             }
 
-            $query = $this->first()->newQuery()->with($relations);
+            $query = $this->first()->newQueryWithoutRelationships()->with($relations);
 
             $this->items = $query->eagerLoadRelations($this->items);
         }
+
+        return $this;
+    }
+
+    /**
+     * Load a set of aggregations over relationship's column onto the collection.
+     *
+     * @param array|string $relations
+     * @param string       $column
+     * @param string       $function
+     *
+     * @return $this
+     */
+    public function loadAggregate($relations, $column, $function = null) {
+        if ($this->isEmpty()) {
+            return $this;
+        }
+
+        $models = $this->first()->newModelQuery()
+            ->whereKey($this->modelKeys())
+            ->select($this->first()->getKeyName())
+            ->withAggregate($relations, $column, $function)
+            ->get()
+            ->keyBy($this->first()->getKeyName());
+
+        $attributes = carr::except(
+            array_keys($models->first()->getAttributes()),
+            $models->first()->getKeyName()
+        );
+
+        $this->each(function ($model) use ($models, $attributes) {
+            $extraAttributes = carr::only($models->get($model->getKey())->getAttributes(), $attributes);
+
+            $model->forceFill($extraAttributes)->syncOriginalAttributes($attributes);
+        });
+
+        return $this;
+    }
+
+    /**
+     * Load a set of relationship counts onto the collection.
+     *
+     * @param array|string $relations
+     *
+     * @return $this
+     */
+    public function loadCount($relations) {
+        return $this->loadAggregate($relations, '*', 'count');
+    }
+
+    /**
+     * Load a set of relationship's max column values onto the collection.
+     *
+     * @param array|string $relations
+     * @param string       $column
+     *
+     * @return $this
+     */
+    public function loadMax($relations, $column) {
+        return $this->loadAggregate($relations, $column, 'max');
+    }
+
+    /**
+     * Load a set of relationship's min column values onto the collection.
+     *
+     * @param array|string $relations
+     * @param string       $column
+     *
+     * @return $this
+     */
+    public function loadMin($relations, $column) {
+        return $this->loadAggregate($relations, $column, 'min');
+    }
+
+    /**
+     * Load a set of relationship's column summations onto the collection.
+     *
+     * @param array|string $relations
+     * @param string       $column
+     *
+     * @return $this
+     */
+    public function loadSum($relations, $column) {
+        return $this->loadAggregate($relations, $column, 'sum');
+    }
+
+    /**
+     * Load a set of relationship's average column values onto the collection.
+     *
+     * @param array|string $relations
+     * @param string       $column
+     *
+     * @return $this
+     */
+    public function loadAvg($relations, $column) {
+        return $this->loadAggregate($relations, $column, 'avg');
+    }
+
+    /**
+     * Load a set of relationships onto the collection if they are not already eager loaded.
+     *
+     * @param array|string $relations
+     *
+     * @return $this
+     */
+    public function loadMissing($relations) {
+        if (is_string($relations)) {
+            $relations = func_get_args();
+        }
+
+        foreach ($relations as $key => $value) {
+            if (is_numeric($key)) {
+                $key = $value;
+            }
+
+            $segments = explode('.', explode(':', $key)[0]);
+
+            if (cstr::contains($key, ':')) {
+                $segments[count($segments) - 1] .= ':' . explode(':', $key)[1];
+            }
+
+            $path = [];
+
+            foreach ($segments as $segment) {
+                $path[] = [$segment => $segment];
+            }
+
+            if (is_callable($value)) {
+                $path[count($segments) - 1][end($segments)] = $value;
+            }
+
+            $this->loadMissingRelation($this, $path);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Load a relationship path if it is not already eager loaded.
+     *
+     * @param CModel_Collection $models
+     * @param array             $path
+     *
+     * @return void
+     */
+    protected function loadMissingRelation(self $models, array $path) {
+        $relation = array_shift($path);
+
+        $name = explode(':', key($relation))[0];
+
+        if (is_string(reset($relation))) {
+            $relation = reset($relation);
+        }
+
+        $models->filter(function ($model) use ($name) {
+            return !is_null($model) && !$model->relationLoaded($name);
+        })->load($relation);
+
+        if (empty($path)) {
+            return;
+        }
+
+        $models = $models->pluck($name);
+
+        if ($models->first() instanceof CCollection) {
+            $models = $models->collapse();
+        }
+
+        $this->loadMissingRelation(new static($models), $path);
+    }
+
+    /**
+     * Load a set of relationships onto the mixed relationship collection.
+     *
+     * @param string $relation
+     * @param array  $relations
+     *
+     * @return $this
+     */
+    public function loadMorph($relation, $relations) {
+        $this->pluck($relation)
+            ->filter()
+            ->groupBy(function ($model) {
+                return get_class($model);
+            })
+            ->each(function ($models, $className) use ($relations) {
+                static::make($models)->load(carr::get($relations, $className, []));
+            });
+
+        return $this;
+    }
+
+    /**
+     * Load a set of relationship counts onto the mixed relationship collection.
+     *
+     * @param string $relation
+     * @param array  $relations
+     *
+     * @return $this
+     */
+    public function loadMorphCount($relation, $relations) {
+        $this->pluck($relation)
+            ->filter()
+            ->groupBy(function ($model) {
+                return get_class($model);
+            })
+            ->each(function ($models, $className) use ($relations) {
+                static::make($models)->loadCount(carr::get($relations, $className, []));
+            });
 
         return $this;
     }
@@ -124,10 +337,26 @@ class CModel_Collection extends CCollection implements CQueue_QueueableCollectio
     public function map(callable $callback) {
         $result = parent::map($callback);
 
-        $a = $result->contains(function ($item) {
+        return $result->contains(function ($item) {
             return !$item instanceof CModel;
         }) ? $result->toBase() : $result;
-        return $a;
+    }
+
+    /**
+     * Run an associative map over each of the items.
+     *
+     * The callback should return an associative array with a single key / value pair.
+     *
+     * @param callable $callback
+     *
+     * @return CCollection|static
+     */
+    public function mapWithKeys(callable $callback) {
+        $result = parent::mapWithKeys($callback);
+
+        return $result->contains(function ($item) {
+            return !$item instanceof CModel;
+        }) ? $result->toBase() : $result;
     }
 
     /**
@@ -149,9 +378,10 @@ class CModel_Collection extends CCollection implements CQueue_QueueableCollectio
             ->whereIn($model->getKeyName(), $this->modelKeys())
             ->get()
             ->getDictionary();
-
-        return $this->map(function ($model) use ($freshModels) {
-            return $model->exists && isset($freshModels[$model->getKey()]) ? $freshModels[$model->getKey()] : null;
+        return $this->filter(function ($model) use ($freshModels) {
+            return $model->exists && isset($freshModels[$model->getKey()]);
+        })->map(function ($model) use ($freshModels) {
+            return $freshModels[$model->getKey()];
         });
     }
 
@@ -186,6 +416,10 @@ class CModel_Collection extends CCollection implements CQueue_QueueableCollectio
     public function intersect($items) {
         $intersect = new static;
 
+        if (empty($items)) {
+            return $intersect;
+        }
+
         $dictionary = $this->getDictionary($items);
 
         foreach ($this->items as $item) {
@@ -203,7 +437,7 @@ class CModel_Collection extends CCollection implements CQueue_QueueableCollectio
      * @param string|callable|null $key
      * @param bool                 $strict
      *
-     * @return static|\Illuminate\Support\Collection
+     * @return static|CCollection
      */
     public function unique($key = null, $strict = false) {
         if (!is_null($key)) {
