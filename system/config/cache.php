51e1f7b6d4c6b23a93c4e5d305dce9b8c608d4b5
hery <hery@ittron.co.id>
2021-01-27 03:07:35 +0700

    update CCache to read through config

--- system/config/cache.php
+++ system/config/cache.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 /**
  * @package  Cache
  *
@@ -25,9 +25,54 @@ defined('SYSPATH') OR die('No direct access allowed.');
  *             caches are deleted. This is commonly referred to as "garbage collection".
  *             Setting this to 0 or a negative number will disable automatic garbage collection.
  */
-return array(
-    'driver' => 'file',
-    'params' => APPPATH . 'cache',
-    'lifetime' => 1800,
-    'requests' => 1000
-);
+return [
+    'default' => 'file',
+    'stores' => [
+        'apc' => [
+            'driver' => 'apc',
+        ],
+        'array' => [
+            'driver' => 'array',
+            'serialize' => false,
+        ],
+        'database' => [
+            'driver' => 'database',
+            'table' => 'cache',
+            'connection' => null,
+        ],
+        'file' => [
+            'driver' => 'file',
+            'path' => DOCROOT . 'temp' . DS . 'cache',
+        ],
+        'memcached' => [
+            'driver' => 'memcached',
+            'persistent_id' => c::env('MEMCACHED_PERSISTENT_ID'),
+            'sasl' => [
+                c::env('MEMCACHED_USERNAME'),
+                c::env('MEMCACHED_PASSWORD'),
+            ],
+            'options' => [
+                // Memcached::OPT_CONNECT_TIMEOUT => 2000,
+            ],
+            'servers' => [
+                [
+                    'host' => c::env('MEMCACHED_HOST', '127.0.0.1'),
+                    'port' => c::env('MEMCACHED_PORT', 11211),
+                    'weight' => 100,
+                ],
+            ],
+        ],
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'cache',
+        ],
+        'dynamodb' => [
+            'driver' => 'dynamodb',
+            'key' => c::env('AWS_ACCESS_KEY_ID'),
+            'secret' => c::env('AWS_SECRET_ACCESS_KEY'),
+            'region' => c::env('AWS_DEFAULT_REGION', 'us-east-1'),
+            'table' => c::env('DYNAMODB_CACHE_TABLE', 'cache'),
+            'endpoint' => c::env('DYNAMODB_ENDPOINT'),
+        ],
+    ],
+];
--- system/libraries/CCache.php
+++ system/libraries/CCache.php
@@ -4,29 +4,51 @@ defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
+ * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Feb 16, 2019, 1:06:35 PM
  */
 class CCache {
     /**
      * @var CCache_Repository[]
      */
-    protected static $repository;
+    protected $repository;
 
     /**
-     * @param array $options
+     * @param null|mixed $name
      *
      * @return CCache_Repository
      */
-    public static function repository($options = []) {
-        $defaultOptions = [
-            'driver' => 'array',
-        ];
-
-        $options = array_merge($defaultOptions, $options);
-
-        $instanceKey = carr::hash($options);
+    public static function repository($name = null) {
+        $options = static::resolveConfig($name);
+        $instanceKey = $name;
+        if (!is_string($name)) {
+            $instanceKey = carr::hash($options);
+        }
         if (!isset(self::$repository[$instanceKey])) {
             self::$repository[$instanceKey] = new CCache_Repository($options);
         }
         return self::$repository[$instanceKey];
+    }
+
+    public static function resolveConfig($name) {
+        $config = CConfig::instance('cache');
+        $options = null;
+        if (is_array($name)) {
+            $options = $name;
+        }
+        if ($options == null) {
+            if ($name = null) {
+                $name = $config->get('default');
+            }
+
+            $options = $config->get($name);
+        }
+
+        $defaultOptions = [
+            'driver' => 'array',
+        ];
+
+        return $options ? $options : $defaultOptions;
     }
 }
