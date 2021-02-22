f3a0891778b27acd39ad7a67094d43755acaefda
hery <hery@ittron.co.id>
2021-02-22 17:12:14 +0700

    psr deprecated helpers

--- modules/cresenity/helpers/cuser.php
+++ modules/cresenity/helpers/cuser.php
@@ -1,22 +1,26 @@
 <?php
 
+//@codingStandardsIgnoreStart
+/**
+ * @deprecated
+ */
 class cuser {
-
     public static function get($id) {
         $db = CDatabase::instance();
-        $query = "select * " .
-                "from users " .
-                "where status > 0 and user_id = " . $db->escape($id) . "";
+        $query = 'select * '
+                . 'from users '
+                . 'where status > 0 and user_id = ' . $db->escape($id) . '';
         $result = $db->query($query);
         $value = null;
-        if ($result->count() > 0)
+        if ($result->count() > 0) {
             $value = $result[0];
+        }
         return $value;
     }
 
     public static function hit_count($user_id) {
         $db = CDatabase::instance();
-        return cdbutils::get_value("select count(*) from log_request where user_id=" . $db->escape($user_id));
+        return cdbutils::get_value('select count(*) from log_request where user_id=' . $db->escape($user_id));
     }
-
 }
+//@codingStandardsIgnoreEnd
