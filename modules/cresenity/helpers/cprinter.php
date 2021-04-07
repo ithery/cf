a250c2c95a9d8d82839a7fdc4fafdc543ac0bb7d
hery <hery@ittron.co.id>
2021-04-07 13:06:23 +0700

    psr

--- modules/cresenity/helpers/cprinter.php
+++ modules/cresenity/helpers/cprinter.php
@@ -1,15 +1,14 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php
+defined('SYSPATH') or die('No direct access allowed.');
 
+//@codingStandardsIgnoreStart
 class cprinter {
-	public static function client_protocol() {
-		$app = CApp::instance();
-		$protocol = ccfg::get('printer_protocol_name');
-		if(strlen($protocol)==0) {
-			$protocol = "cwebrawprint";
-		}
-		return $protocol;
-	}
-	
-	
-	
+    public static function client_protocol() {
+        $app = CApp::instance();
+        $protocol = ccfg::get('printer_protocol_name');
+        if (strlen($protocol) == 0) {
+            $protocol = 'cwebrawprint';
+        }
+        return $protocol;
+    }
 }
