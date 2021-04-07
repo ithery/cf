84a05a6025ec5c5e78fa814ae6b06f9dbd824d34
hery <hery@ittron.co.id>
2021-04-07 13:05:43 +0700

    psr

--- modules/cresenity/helpers/cminify.php
+++ modules/cresenity/helpers/cminify.php
@@ -1,22 +1,24 @@
 <?php
 
+//@codingStandardsIgnoreStart
 class cminify {
-
     /**
      * Minimizes and compresses the provided string. Removes comments, tabs, spaces, and newlines.
      * Warning: Does not work with double slash (//) comments
+     *
      * @param string $buffer The string to minimize.
+     *
      * @return string The minimized string.
      */
     public static function str($buffer) {
         // remove comments
         $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
         // remove tabs, spaces, newlines, etc
-        $buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '     '), '', $buffer);
-        // remove other spaces before/after 
-        $buffer = preg_replace(array('(( )+{)', '({( )+)'), '{', $buffer);
-        $buffer = preg_replace(array('(( )+})', '(}( )+)', '(;( )*})'), '}', $buffer);
-        $buffer = preg_replace(array('(;( )+)', '(( )+;)'), ';', $buffer);
+        $buffer = str_replace(["\r\n", "\r", "\n", "\t", '  ', '    ', '     '], '', $buffer);
+        // remove other spaces before/after
+        $buffer = preg_replace(['(( )+{)', '({( )+)'], '{', $buffer);
+        $buffer = preg_replace(['(( )+})', '(}( )+)', '(;( )*})'], '}', $buffer);
+        $buffer = preg_replace(['(;( )+)', '(( )+;)'], ';', $buffer);
         // return string
         return $buffer;
     }
@@ -24,7 +26,9 @@ class cminify {
     /**
      * Minimizes and compresses the provided $filename. Removes comments, tabs, spaces, and newlines.
      * Warning: Does not work with double slash (//) comments
+     *
      * @param string $filename The string to minimize.
+     *
      * @return int Returns the number of bytes that were written to the file, or FALSE on failure.
      */
     public static function file($filename) {
@@ -35,5 +39,4 @@ class cminify {
         // store minimized contents back into file and return
         return file_put_contents($filename, $buffer);
     }
-
 }
