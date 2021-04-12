650ba28e858efb9aac93a02954ae45f7c1079f90
hery <hery@ittron.co.id>
2021-04-12 15:17:14 +0700

    psr

--- modules/cresenity/libraries/CAnalytics/Exception/InvalidPeriodException.php
+++ modules/cresenity/libraries/CAnalytics/Exception/InvalidPeriodException.php
@@ -1,10 +1,10 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since May 24, 2019, 1:24:10 AM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since May 24, 2019, 1:24:10 AM
  */
-
--- modules/cresenity/libraries/CDebug/Bar/RequestIdGenerator.php
+++ modules/cresenity/libraries/CDebug/Bar/RequestIdGenerator.php
@@ -1,18 +1,18 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Aug 22, 2018, 4:37:40 PM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Aug 22, 2018, 4:37:40 PM
  */
 
 /**
  * Basic request ID generator
  */
 class CDebug_Bar_RequestIdGenerator implements CDebug_Bar_Interface_RequestIdGeneratorInterface {
-
     protected $index = 0;
 
     /**
@@ -22,7 +22,7 @@ class CDebug_Bar_RequestIdGenerator implements CDebug_Bar_Interface_RequestIdGen
         if (function_exists('random_bytes')) {
             // PHP 7 only
             return 'X' . bin2hex(random_bytes(16));
-        } else if (function_exists('openssl_random_pseudo_bytes')) {
+        } elseif (function_exists('openssl_random_pseudo_bytes')) {
             // PHP >= 5.3.0, but OpenSSL may not always be available
             return 'X' . bin2hex(openssl_random_pseudo_bytes(16));
         } else {
@@ -37,5 +37,4 @@ class CDebug_Bar_RequestIdGenerator implements CDebug_Bar_Interface_RequestIdGen
             return 'X' . md5($entropy);
         }
     }
-
 }
