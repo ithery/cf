7a6f6a84f470682f316776045364af56b88815b3
hery <hery@ittron.co.id>
2021-05-06 16:51:29 +0700

    psr

--- system/libraries/CDatabase/Driver.php
+++ system/libraries/CDatabase/Driver.php
@@ -1,19 +1,18 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * Database API driver
  */
 abstract class CDatabase_Driver {
-
     protected $query_cache;
 
     /**
-     *
      * @var CDatabase
      */
     protected $db;
+
     /**
      * Connect to our database.
      * Returns FALSE on failure or a MySQL resource.
@@ -25,13 +24,14 @@ abstract class CDatabase_Driver {
     /**
      * Perform a query based on a manually written query.
      *
-     * @param  string  SQL query to execute
+     * @param string $sql SQL query to execute
+     *
      * @return CDatabase_Result
      */
     abstract public function query($sql);
-    
+
     /**
-     * closing connection.
+     * Closing connection.
      *
      * @return void
      */
@@ -40,9 +40,10 @@ abstract class CDatabase_Driver {
     /**
      * Builds a DELETE query.
      *
-     * @param   string  table name
-     * @param   array   where clause
-     * @return  string
+     * @param string $table table name
+     * @param array  $where where clause
+     *
+     * @return string
      */
     public function delete($table, $where) {
         return 'DELETE FROM ' . $this->escape_table($table) . ' WHERE ' . implode(' ', $where);
@@ -51,10 +52,11 @@ abstract class CDatabase_Driver {
     /**
      * Builds an UPDATE query.
      *
-     * @param   string  table name
-     * @param   array   key => value pairs
-     * @param   array   where clause
-     * @return  string
+     * @param string $table  table name
+     * @param array  $values key => value pairs
+     * @param array  $where  where clause
+     *
+     * @return string
      */
     public function update($table, $values, $where) {
         foreach ($values as $key => $val) {
@@ -66,25 +68,27 @@ abstract class CDatabase_Driver {
     /**
      * Set the charset using 'SET NAMES <charset>'.
      *
-     * @param  string  character set to use
+     * @param string $charset character set to use
      */
     public function set_charset($charset) {
-        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', array(':method', __FUNCTION__));
+        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', [':method', __FUNCTION__]);
     }
 
     /**
      * Wrap the tablename in backticks, has support for: table.field syntax.
      *
-     * @param   string  table name
-     * @return  string
+     * @param string $table table name
+     *
+     * @return string
      */
     abstract public function escape_table($table);
 
     /**
      * Escape a column/field name, has support for special commands.
      *
-     * @param   string  column name
-     * @return  string
+     * @param string $column column name
+     *
+     * @return string
      */
     abstract public function escape_column($column);
 
@@ -95,8 +99,14 @@ abstract class CDatabase_Driver {
      * @param   string   value
      * @param   string   type
      * @param   int      number of where clauses
-     * @param   boolean  escape the value
-     * @return  string
+     * @param   bool  escape the value
+     * @param mixed $key
+     * @param mixed $value
+     * @param mixed $type
+     * @param mixed $num_wheres
+     * @param mixed $quote
+     *
+     * @return string
      */
     public function where($key, $value, $type, $num_wheres, $quote) {
         $prefix = ($num_wheres == 0) ? '' : $type;
@@ -104,7 +114,7 @@ abstract class CDatabase_Driver {
         if ($quote === -1) {
             $value = '';
         } else {
-            if ($value === NULL) {
+            if ($value === null) {
                 if (!$this->has_operator($key)) {
                     $key .= ' IS';
                 }
@@ -115,18 +125,18 @@ abstract class CDatabase_Driver {
                     $key .= ' =';
                 }
 
-                $value = ($value == TRUE) ? ' 1' : ' 0';
+                $value = ($value == true) ? ' 1' : ' 0';
             } else {
-                if (!$this->has_operator($key) AND ! empty($key)) {
+                if (!$this->has_operator($key) and !empty($key)) {
                     $key = $this->escape_column($key) . ' =';
                 } else {
                     preg_match('/^(.+?)([<>!=]+|\bIS(?:\s+NULL))\s*$/i', $key, $matches);
-                    if (isset($matches[1]) AND isset($matches[2])) {
+                    if (isset($matches[1]) and isset($matches[2])) {
                         $key = $this->escape_column(trim($matches[1])) . ' ' . trim($matches[2]);
                     }
                 }
 
-                $value = ' ' . (($quote == TRUE) ? $this->escape($value) : $value);
+                $value = ' ' . (($quote == true) ? $this->escape($value) : $value);
             }
         }
 
@@ -138,17 +148,23 @@ abstract class CDatabase_Driver {
      *
      * @param   mixed    field name
      * @param   string   value to match with field
-     * @param   boolean  add wildcards before and after the match
+     * @param   bool  add wildcards before and after the match
      * @param   string   clause type (AND or OR)
      * @param   int      number of likes
-     * @return  string
+     * @param mixed $field
+     * @param mixed $match
+     * @param mixed $auto
+     * @param mixed $type
+     * @param mixed $num_likes
+     *
+     * @return string
      */
     public function like($field, $match, $auto, $type, $num_likes) {
         $prefix = ($num_likes == 0) ? '' : $type;
 
         $match = $this->escape_str($match);
 
-        if ($auto === TRUE) {
+        if ($auto === true) {
             // Add the start and end quotes
             $match = '%' . str_replace('%', '\\%', $match) . '%';
         }
@@ -163,14 +179,20 @@ abstract class CDatabase_Driver {
      * @param   string  value to match with field
      * @param   string  clause type (AND or OR)
      * @param   int     number of likes
-     * @return  string
+     * @param mixed $field
+     * @param mixed $match
+     * @param mixed $auto
+     * @param mixed $type
+     * @param mixed $num_likes
+     *
+     * @return string
      */
     public function notlike($field, $match, $auto, $type, $num_likes) {
         $prefix = ($num_likes == 0) ? '' : $type;
 
         $match = $this->escape_str($match);
 
-        if ($auto === TRUE) {
+        if ($auto === true) {
             // Add the start and end quotes
             $match = '%' . $match . '%';
         }
@@ -184,11 +206,16 @@ abstract class CDatabase_Driver {
      * @param   string   field name
      * @param   string   value to match with field
      * @param   string   clause type (AND or OR)
-     * @param   integer  number of regexes
-     * @return  string
+     * @param   int  number of regexes
+     * @param mixed $field
+     * @param mixed $match
+     * @param mixed $type
+     * @param mixed $num_regexs
+     *
+     * @return string
      */
     public function regex($field, $match, $type, $num_regexs) {
-        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', array(':method', __FUNCTION__));
+        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', [':method', __FUNCTION__]);
     }
 
     /**
@@ -197,11 +224,16 @@ abstract class CDatabase_Driver {
      * @param   string   field name
      * @param   string   value to match with field
      * @param   string   clause type (AND or OR)
-     * @param   integer  number of regexes
-     * @return  string
+     * @param   int  number of regexes
+     * @param mixed $field
+     * @param mixed $match
+     * @param mixed $type
+     * @param mixed $num_regexs
+     *
+     * @return string
      */
     public function notregex($field, $match, $type, $num_regexs) {
-        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', array(':method', __FUNCTION__));
+        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', [':method', __FUNCTION__]);
     }
 
     /**
@@ -210,7 +242,11 @@ abstract class CDatabase_Driver {
      * @param   string  table name
      * @param   array   keys
      * @param   array   values
-     * @return  string
+     * @param mixed $table
+     * @param mixed $keys
+     * @param mixed $values
+     *
+     * @return string
      */
     public function insert($table, $keys, $values) {
         // Escape the column names
@@ -226,29 +262,37 @@ abstract class CDatabase_Driver {
      * @param   string  table name
      * @param   array   keys
      * @param   array   values
-     * @return  string
+     * @param mixed $table
+     * @param mixed $keys
+     * @param mixed $values
+     *
+     * @return string
      */
     public function merge($table, $keys, $values) {
-        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', array(':method', __FUNCTION__));
+        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', [':method', __FUNCTION__]);
     }
 
     /**
      * Builds a LIMIT portion of a query.
      *
-     * @param   integer  limit
-     * @param   integer  offset
-     * @return  string
+     * @param   int  limit
+     * @param   int  offset
+     * @param mixed $limit
+     * @param mixed $offset
+     *
+     * @return string
      */
     abstract public function limit($limit, $offset = 0);
 
     /**
      * Creates a prepared statement.
      *
-     * @param   string  SQL query
-     * @return  Database_Stmt
+     * @param string $sql SQL query
+     *
+     * @return CDatabase_Stmt
      */
     public function stmt_prepare($sql = '') {
-        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', array(':method', __FUNCTION__));
+        throw new CDatabase_Exception('The method you called, :method, is not supported by this driver', [':method', __FUNCTION__]);
     }
 
     /**
@@ -257,15 +301,18 @@ abstract class CDatabase_Driver {
      *  Should not be called directly, the get() function calls it.
      *
      * @param   array   select query values
-     * @return  string
+     * @param mixed $database
+     *
+     * @return string
      */
     abstract public function compile_select($database);
 
     /**
      * Determines if the string has an arithmetic operator in it.
      *
-     * @param   string   string to check
-     * @return  boolean
+     * @param string $str string to check
+     *
+     * @return bool
      */
     public function has_operator($str) {
         return (bool) preg_match('/[<>!=]|\sIS(?:\s+NOT\s+)?\b|BETWEEN/i', trim($str));
@@ -275,11 +322,14 @@ abstract class CDatabase_Driver {
      * Escapes any input value.
      *
      * @param   mixed   value to escape
-     * @return  string
+     * @param mixed $value
+     *
+     * @return string
      */
     public function escape($value) {
-        if (!$this->dbConfig['escape'])
+        if (!$this->dbConfig['escape']) {
             return $value;
+        }
 
         switch (gettype($value)) {
             case 'string':
@@ -293,7 +343,7 @@ abstract class CDatabase_Driver {
                 $value = sprintf('%F', $value);
                 break;
             default:
-                $value = ($value === NULL) ? 'NULL' : $value;
+                $value = ($value === null) ? 'NULL' : $value;
                 break;
         }
 
@@ -303,38 +353,41 @@ abstract class CDatabase_Driver {
     /**
      * Escapes a string for a query.
      *
-     * @param   mixed   value to escape
-     * @return  string
+     * @param mixed $str value to escape
+     *
+     * @return string
      */
     abstract public function escape_str($str);
 
     /**
      * Lists all tables in the database.
      *
-     * @return  array
+     * @return array
      */
     abstract public function list_tables();
 
     /**
      * Lists all fields in a table.
      *
-     * @param   string  table name
-     * @return  array
+     * @param string $table table name
+     *
+     * @return array
      */
-    abstract function list_fields($table);
+    abstract public function list_fields($table);
 
     /**
      * Returns the last database error.
      *
-     * @return  string
+     * @return string
      */
     abstract public function show_error();
 
     /**
      * Returns field data about a table.
      *
-     * @param   string  table name
-     * @return  array
+     * @param string $table table name
+     *
+     * @return array
      */
     abstract public function field_data($table);
 
@@ -342,19 +395,21 @@ abstract class CDatabase_Driver {
      * Fetches SQL type information about a field, in a generic format.
      *
      * @param   string  field datatype
-     * @return  array
+     * @param mixed $str
+     *
+     * @return array
      */
     protected function sql_type($str) {
         static $sql_types;
 
-        if ($sql_types === NULL) {
+        if ($sql_types === null) {
             // Load SQL data types
             $sql_types = CF::config('sql_types');
         }
 
         $str = strtolower(trim($str));
 
-        if (($open = strpos($str, '(')) !== FALSE) {
+        if (($open = strpos($str, '(')) !== false) {
             // Find closing bracket
             $close = strpos($str, ')', $open) - 1;
 
@@ -365,11 +420,7 @@ abstract class CDatabase_Driver {
             $type = $str;
         }
 
-        empty($sql_types[$type]) and exit
-                        (
-                        'Unknown field type: ' . $type . '. ' .
-                        'Please report this: http://trac.cresenitytech.com/newticket'
-        );
+        empty($sql_types[$type]) and exit('Unknown field type: ' . $type);
 
         // Fetch the field definition
         $field = $sql_types[$type];
@@ -384,7 +435,7 @@ abstract class CDatabase_Driver {
                 break;
             case 'int':
                 // Add unsigned value
-                $field['unsigned'] = (strpos($str, 'unsigned') !== FALSE);
+                $field['unsigned'] = (strpos($str, 'unsigned') !== false);
                 break;
         }
 
@@ -395,10 +446,11 @@ abstract class CDatabase_Driver {
      * Clears the internal query cache.
      *
      * @param  string  SQL query
+     * @param null|mixed $sql
      */
-    public function clear_cache($sql = NULL) {
+    public function clear_cache($sql = null) {
         if (empty($sql)) {
-            $this->query_cache = array();
+            $this->query_cache = [];
         } else {
             unset($this->query_cache[$this->query_hash($sql)]);
         }
@@ -410,8 +462,9 @@ abstract class CDatabase_Driver {
      * Creates a hash for an SQL query string. Replaces newlines with spaces,
      * trims, and hashes.
      *
-     * @param   string  SQL query
-     * @return  string
+     * @param string $sql SQL query
+     *
+     * @return string
      */
     protected function query_hash($sql) {
         return sha1(str_replace("\n", ' ', trim($sql)));
@@ -426,13 +479,10 @@ abstract class CDatabase_Driver {
     }
 
     public function commit() {
-
         $this->query('COMMIT;');
     }
 
-
     /**
-     * 
      * @return CDatabase
      */
     public function db() {
@@ -441,4 +491,3 @@ abstract class CDatabase_Driver {
 }
 
 // End Database Driver Interface
-
