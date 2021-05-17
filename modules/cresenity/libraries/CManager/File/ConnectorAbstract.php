d06f9a26031a48bf46df869339b3ab7724e970db
hery <hery@ittron.co.id>
2021-05-17 15:23:41 +0700

    psr

--- modules/cresenity/libraries/CManager/File/ConnectorAbstract.php
+++ modules/cresenity/libraries/CManager/File/ConnectorAbstract.php
@@ -1,17 +1,16 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Mar 27, 2019, 2:49:51 PM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Mar 27, 2019, 2:49:51 PM
  */
 abstract class CManager_File_ConnectorAbstract {
-
     /**
-     *
-     * @var CManager_File_ConfigAbstract 
+     * @var CManager_File_ConfigAbstract
      */
     protected $config;
 
@@ -22,5 +21,4 @@ abstract class CManager_File_ConnectorAbstract {
     public function getConfig($key = null, $default = null) {
         return $this->config->getConfig($key, $default);
     }
-
 }
