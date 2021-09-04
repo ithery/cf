b4377e6e3b0447acf6ee91ae919ea775ca85ec67
Hery <hery@ittron.co.id>
2021-09-04 16:26:59 +0700

    psr element

--- modules/cresenity/libraries/CElement/Element/Button.php
+++ modules/cresenity/libraries/CElement/Element/Button.php
@@ -2,17 +2,13 @@
 
 /**
  * @author Hery Kurniawan
- * @since Apr 23, 2020, 2:25:21 AM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Apr 23, 2020, 2:25:21 AM
  */
 class CElement_Element_Button extends CElement_Element {
-
-    public function __construct($id = "") {
-
+    public function __construct($id = '') {
         parent::__construct($id);
-        $this->tag = "button";
+        $this->tag = 'button';
     }
-
-    
-
 }
--- modules/cresenity/libraries/CElement/Element/Ol.php
+++ modules/cresenity/libraries/CElement/Element/Ol.php
@@ -1,13 +1,10 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 class CElement_Element_Ol extends CElement_Element {
-
-    public function __construct($id = "") {
-
+    public function __construct($id = '') {
         parent::__construct($id);
-        $this->tag = "ol";
+        $this->tag = 'ol';
     }
-
 }
