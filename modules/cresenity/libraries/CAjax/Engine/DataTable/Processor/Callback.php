50ee228ae7c498979ab3f1252ba60f0148a1e0b8
Hery <hery@ittron.co.id>
2021-09-06 18:45:32 +0700

    fix callback php7

--- modules/cresenity/libraries/CAjax/Engine/DataTable/Processor/Callback.php
+++ modules/cresenity/libraries/CAjax/Engine/DataTable/Processor/Callback.php
@@ -1,14 +1,14 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Jul 8, 2018, 2:58:36 AM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Jul 8, 2018, 2:58:36 AM
  */
 class CAjax_Engine_DataTable_Processor_Callback extends CAjax_Engine_DataTable_Processor {
-
     use CAjax_Engine_DataTable_Trait_ProcessorTrait;
 
     public function process() {
@@ -23,36 +23,30 @@ class CAjax_Engine_DataTable_Processor_Callback extends CAjax_Engine_DataTable_P
             require_once $callbackRequire;
         }
         $callback = carr::get($data, 'query');
-        $params = array();
+        $params = [];
         $params['options'] = carr::get($data, 'callbackOptions');
         $params['processor'] = $this;
 
-
         $resultCallback = call_user_func_array($callback, $params);
 
-        $data = carr::get($resultCallback, 'data', array());
-        $totalRecord = carr::get($resultCallback, 'total_record', count($data));
-        $totalFilteredRecord = carr::get($resultCallback, 'total_filtered_record', count($data));
+        $data = carr::get($resultCallback, 'data', []);
 
+        $totalRecord = carr::get($resultCallback, 'total_record', is_array($data) ? count($data) : 0);
+        $totalFilteredRecord = carr::get($resultCallback, 'total_filtered_record', is_array($data) ? count($data) : 0);
 
         $js = '';
-        $output = array(
-            "sEcho" => intval(carr::get($request, 'sEcho')),
-            "iTotalRecords" => $totalRecord,
-            "iTotalDisplayRecords" => $totalFilteredRecord,
-            "aaData" => $this->populateAAData($data, $table, $request, $js),
-        );
+        $output = [
+            'sEcho' => intval(carr::get($request, 'sEcho')),
+            'iTotalRecords' => $totalRecord,
+            'iTotalDisplayRecords' => $totalFilteredRecord,
+            'aaData' => $this->populateAAData($data, $table, $request, $js),
+        ];
 
-
-
-
-        $data = array(
-            "datatable" => $output,
-            "js" => cbase64::encode($js),
-        );
-
+        $data = [
+            'datatable' => $output,
+            'js' => cbase64::encode($js),
+        ];
 
         return $data;
     }
-
 }
