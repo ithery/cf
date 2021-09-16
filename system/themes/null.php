6d76b9e855c5f052888f48c135645ba13b68f91d
Hery <hery@ittron.co.id>
2021-09-16 20:45:35 +0700

    move themes null

---
+++ system/themes/null.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('SYSPATH') or die('No direct access allowed.');
+
+/**
+ * @author Hery Kurniawan
+ * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Jun 29, 2019, 6:57:16 PM
+ */
+return [
+    'client_modules' => [
+    ],
+    'js' => [
+    ],
+    'css' => [
+    ],
+    'data' => [
+        'datatable' => [
+            'dom' => null,
+            'class' => null,
+        ],
+        'select2' => [
+            'version' => null,
+        ]
+    ],
+    'custom_js' => '',
+];
