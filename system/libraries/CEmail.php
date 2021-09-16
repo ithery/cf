42364f5c148e5d7b8e6cb4524359313e1823d699
Hery <hery@ittron.co.id>
2021-09-16 19:02:09 +0700

    fix CEmail

--- system/libraries/CEmail.php
+++ system/libraries/CEmail.php
@@ -9,10 +9,6 @@ class CEmail {
     }
 
     public static function sender(array $config = []) {
-        if (c::blank($config)) {
-            //config
-            $username = CF::config('app.email.username', CF::config('app.smtp_username'));
-        }
         return new CEmail_Sender($config);
     }
 }
