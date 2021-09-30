2b7ba5025f39ce3f8dfc57b6a4f8590b94b911fc
ari muntari <ari.muntari1@gmail.com>
2021-09-30 07:05:16 +0700

    update filemanager

--- system/libraries/CManager/File/Connector/FileManager.php
+++ system/libraries/CManager/File/Connector/FileManager.php
@@ -14,8 +14,18 @@ class CManager_File_Connector_FileManager extends CManager_File_ConnectorAbstrac
         'CropImage' => CManager_File_Connector_FileManager_Controller_CropImageController::class,
         'CropNewImage' => CManager_File_Connector_FileManager_Controller_CropNewImageController::class,
         'Delete' => CManager_File_Connector_FileManager_Controller_DeleteController::class,
+        'DoMove' => CManager_File_Connector_FileManager_Controller_DoMoveController::class,
+        'Download' => CManager_File_Connector_FileManager_Controller_DownloadController::class,
+        'Error' => CManager_File_Connector_FileManager_Controller_ErrorController::class,
+        'Folder' => CManager_File_Connector_FileManager_Controller_FolderController::class,
+        'Index' => CManager_File_Connector_FileManager_Controller_IndexController::class,
         'Item' => CManager_File_Connector_FileManager_Controller_ItemController::class,
-        //'Folder' => CManager_File_Connector_FileManager_Controller_FolderController::class,
+        'Move' => CManager_File_Connector_FileManager_Controller_MoveController::class,
+        'NewFolder' => CManager_File_Connector_FileManager_Controller_NewFolderController::class,
+        'Rename' => CManager_File_Connector_FileManager_Controller_RenameController::class,
+        'Resize' => CManager_File_Connector_FileManager_Controller_ResizeController::class,
+        'ResizeImage' => CManager_File_Connector_FileManager_Controller_ResizeImageController::class,
+        'Upload' => CManager_File_Connector_FileManager_Controller_UploadController::class,
     ];
 
     public function run($method = null) {
@@ -41,10 +51,6 @@ class CManager_File_Connector_FileManager extends CManager_File_ConnectorAbstrac
         if (!in_array($method, $availableMethod)) {
             //throw error method not available
             $defaultControllerClass = 'CManager_File_Connector_FileManager_Controller_' . $method . 'Controller';
-        }
-
-        if (!class_exists($defaultControllerClass)) {
-            throw new Exception('Method ' . $method . ' not available');
         }
         //get class name from config, otherwise default
 
