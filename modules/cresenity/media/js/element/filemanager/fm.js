29fa8f05e5634f628025c4881d0ec288df7e27ed
hery <hery@ittron.co.id>
2021-10-01 01:17:47 +0700

    fix for items length

--- modules/cresenity/media/js/element/filemanager/fm.js
+++ modules/cresenity/media/js/element/filemanager/fm.js
@@ -242,6 +242,9 @@ var CFileManager = function (options) {
 
 
     this.controllerMethod.move = (items) => {
+        if(items.length==0) {
+            return this.displayErrorResponse('No items selected, please select item');
+        }
         this.performFmRequest('move', {items: items.map(function (item) {
             return item.name;
         })}).done(this.refreshFoldersAndItems);
