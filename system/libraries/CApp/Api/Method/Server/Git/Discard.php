f05784e7857507cf86d97499b5a1e8273500bc33
albert <albert@ittron.co.id>
2021-10-04 18:36:49 +0700

    repair discard, clean, checkout

---
+++ system/libraries/CApp/Api/Method/Server/Git/Discard.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('SYSPATH') or die('No direct access allowed.');
+
+use Symfony\Component\Process\Process;
+
+/**
+ * @author Muhammad Harisuddin Thohir <me@harisuddin.com>
+ * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Mar 10, 2020, 11:13:37 AM
+ */
+class CApp_Api_Method_Server_Git_Discard extends CApp_Api_Method_Server {
+    public function execute() {
+        $output = '';
+        $successOutput = '';
+        $errorOutput = '';
+
+        if ($this->errCode == 0) {
+            try {
+                $pwd = '';
+                $execute = '';
+                $pwd = shell_exec('pwd');
+                $execute = 'git checkout -- .';
+
+                $output .= "working on directory $pwd";
+                $process = new Process($execute);
+                $process->run();
+
+                $output .= $process->getOutput();
+                $successOutput = $output;
+                $output .= $errorOutput = $process->getErrorOutput();
+                CView::blade()->clearCompiled();
+            } catch (Exception $ex) {
+                $this->errCode++;
+                $this->errMessage = $ex->getMessage();
+            }
+        }
+
+        $this->data = [
+            'output' => $output,
+            'successOutput' => $successOutput,
+            'errorOutput' => $errorOutput,
+        ];
+
+        return $this;
+    }
+}
