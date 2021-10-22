3068db8d27a40904096b1c84c1471bf32676b9c1
Haris Thohir <me@harisuddin.com>
2021-10-22 14:36:37 +0700

    administrator auth using cookies

--- system/libraries/CApp/Administrator/Controller/User.php
+++ system/libraries/CApp/Administrator/Controller/User.php
@@ -8,12 +8,13 @@ defined('SYSPATH') or die('No direct access allowed.');
  *
  * @since Sep 8, 2018, 12:47:25 AM
  */
+
 use CApp_Administrator as Administrator;
 
 class CApp_Administrator_Controller_User extends CApp_Administrator_Controller {
     public function __construct() {
         $app = CApp::instance();
-        if (!Administrator::isLogin()) {
+        if (!isset($_COOKIE['capp-administrator'])) {
             $app->setViewName('administrator/login');
         }
 
