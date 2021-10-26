8d03d9b7e891c6482e8df444091698750c030f40
hery <hery@ittron.co.id>
2021-10-26 16:58:12 +0700

    update deprecated

--- modules/cresenity/libraries/CMage/Mage/Trait/ValidationTrait.php
+++ modules/cresenity/libraries/CMage/Mage/Trait/ValidationTrait.php
@@ -1,17 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 trait CMage_Mage_Trait_ValidationTrait {
-
     /**
      * Validate a resource creation request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return void
      */
     public function validateForAdd(CMage_Request $request) {
@@ -21,51 +15,55 @@ trait CMage_Mage_Trait_ValidationTrait {
     /**
      * Create a validator instance for a resource creation request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return CValidation_Validator
      */
     public function validatorForAdd(CMage_Request $request) {
-        return CValidation_Factory::make($request->all(), $this->rulesForAdd($request))
-                        ->after(function ($validator) use ($request) {
-                            $this->afterValidation($request, $validator);
-                            $this->afterAddValidation($request, $validator);
-                        });
+        return CValidation::factory()->make($request->all(), $this->rulesForAdd($request))
+            ->after(function ($validator) use ($request) {
+                $this->afterValidation($request, $validator);
+                $this->afterAddValidation($request, $validator);
+            });
     }
 
     /**
      * Get the validation rules for a resource creation request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return array
      */
     public function rulesForAdd(CMage_Request $request) {
         return $this->formatRules($request, (new static($this->newModel()))
-                                ->creationFields($request)
-                                ->mapWithKeys(function ($field) use ($request) {
-                                    return $field->getCreationRules($request);
-                                })->all());
+            ->creationFields($request)
+            ->mapWithKeys(function ($field) use ($request) {
+                return $field->getCreationRules($request);
+            })->all());
     }
 
     /**
      * Get the creation validation rules for a specific field.
      *
-     * @param  CMage_Request  $request
-     * @param  string  $field
+     * @param CMage_Request $request
+     * @param string        $field
+     *
      * @return array
      */
     public function creationRulesFor(CMage_Request $request, $field) {
         return $this->formatRules($request, (new static($this->newModel()))
-                                ->availableFields($request)
-                                ->where('attribute', $field)
-                                ->mapWithKeys(function ($field) use ($request) {
-                                    return $field->getCreationRules($request);
-                                })->all());
+            ->availableFields($request)
+            ->where('attribute', $field)
+            ->mapWithKeys(function ($field) use ($request) {
+                return $field->getCreationRules($request);
+            })->all());
     }
 
     /**
      * Validate a resource update request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return void
      */
     public function validateForEdit(CMage_Request $request) {
@@ -75,51 +73,55 @@ trait CMage_Mage_Trait_ValidationTrait {
     /**
      * Create a validator instance for a resource update request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return CValidation_Validator
      */
     public function validatorForEdit(CMage_Request $request) {
-        return CValidation_Factory::make($request->all(), $this->rulesForUpdate($request))
-                        ->after(function ($validator) use ($request) {
-                            $this->afterValidation($request, $validator);
-                            $this->afterEditValidation($request, $validator);
-                        });
+        return CValidation::factory()->make($request->all(), $this->rulesForUpdate($request))
+            ->after(function ($validator) use ($request) {
+                $this->afterValidation($request, $validator);
+                $this->afterEditValidation($request, $validator);
+            });
     }
 
     /**
      * Get the validation rules for a resource update request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return array
      */
     public function rulesForUpdate(CMage_Request $request) {
         return $this->formatRules($request, (new static($this->newModel()))
-                                ->updateFields($request)
-                                ->mapWithKeys(function ($field) use ($request) {
-                                    return $field->getUpdateRules($request);
-                                })->all());
+            ->updateFields($request)
+            ->mapWithKeys(function ($field) use ($request) {
+                return $field->getUpdateRules($request);
+            })->all());
     }
 
     /**
      * Get the update validation rules for a specific field.
      *
-     * @param  CMage_Request  $request
-     * @param  string  $field
+     * @param CMage_Request $request
+     * @param string        $field
+     *
      * @return array
      */
     public function updateRulesFor(CMage_Request $request, $field) {
         return $this->formatRules($request, (new static($this->newModel()))
-                                ->availableFields($request)
-                                ->where('attribute', $field)
-                                ->mapWithKeys(function ($field) use ($request) {
-                                    return $field->getUpdateRules($request);
-                                })->all());
+            ->availableFields($request)
+            ->where('attribute', $field)
+            ->mapWithKeys(function ($field) use ($request) {
+                return $field->getUpdateRules($request);
+            })->all());
     }
 
     /**
      * Validate a resource attachment request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return void
      */
     public function validateForAttachment(CMage_Request $request) {
@@ -129,31 +131,34 @@ trait CMage_Mage_Trait_ValidationTrait {
     /**
      * Create a validator instance for a resource attachment request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return CValidation_Validator
      */
     public function validatorForAttachment(CMage_Request $request) {
-        return CValidation_Factory::make($request->all(), $this->rulesForAttachment($request));
+        return CValidation::factory()->make($request->all(), $this->rulesForAttachment($request));
     }
 
     /**
      * Get the validation rules for a resource attachment request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return array
      */
     public function rulesForAttachment(CMage_Request $request) {
         return $this->formatRules($request, (new static($this->newModel()))
-                                ->creationPivotFields($request, $request->relatedResource)
-                                ->mapWithKeys(function ($field) use ($request) {
-                                    return $field->getCreationRules($request);
-                                })->all());
+            ->creationPivotFields($request, $request->relatedResource)
+            ->mapWithKeys(function ($field) use ($request) {
+                return $field->getCreationRules($request);
+            })->all());
     }
 
     /**
      * Validate a resource attachment update request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return void
      */
     public function validateForAttachmentUpdate(CMage_Request $request) {
@@ -163,32 +168,35 @@ trait CMage_Mage_Trait_ValidationTrait {
     /**
      * Create a validator instance for a resource attachment update request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return CValidation_Validator
      */
     public function validatorForAttachmentUpdate(CMage_Request $request) {
-        return CValidation_Factory::make($request->all(), $this->rulesForAttachmentUpdate($request));
+        return CValidation::factory()->make($request->all(), $this->rulesForAttachmentUpdate($request));
     }
 
     /**
      * Get the validation rules for a resource attachment update request.
      *
-     * @param  CMage_Request  $request
+     * @param CMage_Request $request
+     *
      * @return array
      */
     public function rulesForAttachmentUpdate(CMage_Request $request) {
         return $this->formatRules($request, (new static($this->newModel()))
-                                ->updatePivotFields($request, $request->relatedResource)
-                                ->mapWithKeys(function ($field) use ($request) {
-                                    return $field->getUpdateRules($request);
-                                })->all());
+            ->updatePivotFields($request, $request->relatedResource)
+            ->mapWithKeys(function ($field) use ($request) {
+                return $field->getUpdateRules($request);
+            })->all());
     }
 
     /**
      * Perform any final formatting of the given validation rules.
      *
-     * @param  CMage_Request  $request
-     * @param  array  $rules
+     * @param CMage_Request $request
+     * @param array         $rules
+     *
      * @return array
      */
     protected static function formatRules(CMage_Request $request, array $rules) {
@@ -200,58 +208,58 @@ trait CMage_Mage_Trait_ValidationTrait {
             return $rules;
         }
 
-        return CF::collect($rules)->map(function ($rules) use ($replacements) {
-                    return CF::collect($rules)->map(function ($rule) use ($replacements) {
-                                return is_string($rule) ? str_replace(array_keys($replacements), array_values($replacements), $rule) : $rule;
-                            })->all();
-                })->all();
+        return c::collect($rules)->map(function ($rules) use ($replacements) {
+            return c::collect($rules)->map(function ($rule) use ($replacements) {
+                return is_string($rule) ? str_replace(array_keys($replacements), array_values($replacements), $rule) : $rule;
+            })->all();
+        })->all();
     }
 
     /**
      * Get the validation attribute for a specific field.
      *
-     * @param  CMage_Request  $request
-     * @param  string  $field
+     * @param CMage_Request $request
+     * @param string        $field
+     *
      * @return string
      */
     public function validationAttributeFor(CMage_Request $request, $field) {
         return (new static($this->newModel()))
-                        ->availableFields($request)
-                        ->firstWhere('resourceName', $field)
-                        ->getValidationAttribute($request);
+            ->availableFields($request)
+            ->firstWhere('resourceName', $field)
+            ->getValidationAttribute($request);
     }
 
     /**
      * Handle any post-validation processing.
      *
-     * @param  CMage_Request  $request
-     * @param  CValidation_Validator  $validator
+     * @param CMage_Request         $request
+     * @param CValidation_Validator $validator
+     *
      * @return void
      */
     protected static function afterValidation(CMage_Request $request, $validator) {
-        //
     }
 
     /**
      * Handle any post-creation validation processing.
      *
-     * @param  CMage_Request  $request
-     * @param  CValidation_Validator  $validator
+     * @param CMage_Request         $request
+     * @param CValidation_Validator $validator
+     *
      * @return void
      */
     protected static function afterAddValidation(CMage_Request $request, $validator) {
-        //
     }
 
     /**
      * Handle any post-update validation processing.
      *
-     * @param  CMage_Request  $request
-     * @param  CValidation_Validator  $validator
+     * @param CMage_Request         $request
+     * @param CValidation_Validator $validator
+     *
      * @return void
      */
     protected static function afterEditValidation(CMage_Request $request, $validator) {
-        //
     }
-
 }
