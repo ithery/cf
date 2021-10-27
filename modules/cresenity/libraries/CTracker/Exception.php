0e139e29add3371a0b3996adf0486cc0acf3c989
Hery <hery@ittron.co.id>
2021-10-28 04:37:09 +0700

    psr

--- modules/cresenity/libraries/CTracker/Exception.php
+++ modules/cresenity/libraries/CTracker/Exception.php
@@ -1,12 +1,12 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Jun 23, 2019, 4:02:27 AM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Jun 23, 2019, 4:02:27 AM
  */
-class CTracker_Exception extends CException {
-    
+class CTracker_Exception extends Exception {
 }
--- modules/cresenity/libraries/CTracker/GeoIp/GeoIpInterface.php
+++ modules/cresenity/libraries/CTracker/GeoIp/GeoIpInterface.php
@@ -1,13 +1,13 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Jun 23, 2019, 5:08:03 PM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Jun 23, 2019, 5:08:03 PM
  */
 interface CTracker_GeoIp_GeoIpInterface {
-
     public function searchAddr($addr);
 }
