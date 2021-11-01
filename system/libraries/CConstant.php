773463bbc54d943889946e1b17da32f15278d7fb
Hery <hery@ittron.co.id>
2021-11-02 02:14:10 +0700

    fix CConstant

--- system/libraries/CConstant.php
+++ system/libraries/CConstant.php
@@ -8,38 +8,60 @@ defined('SYSPATH') or die('No direct access allowed.');
  *
  * @since Sep 1, 2018, 4:20:26 PM
  */
-class CConstant implements CApp_Interface_ConstantInterface {
-    use CApp_Trait_ConstantTrait;
+class CConstant {
+    const ALL = 'ALL';
+
+    const NONE = 'NONE';
+
+    const NO_LABEL = 'NO';
+
+    const YES_LABEL = 'YES';
 
     /**
-     * Handler Constant
+     * Handler Constant.
      */
     const HANDLER_TYPE_DIALOG = CObservable_Listener_Handler::TYPE_DIALOG;
+
     const HANDLER_TYPE_REMOVE = CObservable_Listener_Handler::TYPE_REMOVE;
+
     const HANDLER_TYPE_SUBMIT = CObservable_Listener_Handler::TYPE_SUBMIT;
+
     const HANDLER_TYPE_EMPTY = CObservable_Listener_Handler::TYPE_EMPTY;
+
     const HANDLER_TYPE_CUSTOM = CObservable_Listener_Handler::TYPE_CUSTOM;
+
     const HANDLER_TYPE_APPEND = CObservable_Listener_Handler::TYPE_APPEND;
+
     const HANDLER_TYPE_RELOAD = CObservable_Listener_Handler::TYPE_RELOAD;
 
     /**
-     * Path Constant
+     * Path Constant.
      */
     const CRESENITY_PATH = DOCROOT . '/modules/cresenity';
+
     const CRESENITY_MEDIA_PATH = DOCROOT . '/modules/cresenity/media';
+
     const CRESENITY_FONT_PATH = DOCROOT . '/modules/cresenity/media/font';
+
     const CRESENITY_IMAGE_PATH = DOCROOT . '/modules/cresenity/media/img';
 
     /**
-     * Align Constant
+     * Align Constant.
      */
     const ALIGN_LEFT = 'left';
+
     const ALIGN_RIGHT = 'right';
+
     const ALIGN_CENTER = 'center';
 
     /**
-     * Table View Constant
+     * Table View Constant.
      */
     const TABLE_VIEW_COL = 'col';
+
     const TABLE_VIEW_ROW = 'row';
+
+    public static function yesNoList() {
+        return [self::NO_LABEL, self::YES_LABEL];
+    }
 }
