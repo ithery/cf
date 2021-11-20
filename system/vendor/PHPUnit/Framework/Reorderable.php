c89bd3202f58eb2d577d8b49b54bf4d2e6b87b91
Hery <hery@ittron.co.id>
2021-11-20 22:03:56 +0700

    update phpunit

--- system/vendor/PHPUnit/Framework/Reorderable.php
+++ system/vendor/PHPUnit/Framework/Reorderable.php
@@ -7,13 +7,13 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace PHPUnit\Framework;
 
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
-interface Reorderable
-{
+interface Reorderable {
     public function sortId();
 
     /**
