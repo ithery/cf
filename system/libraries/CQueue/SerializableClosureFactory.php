103dd4df15d50ef68949976cbc78d5d6b54e45e7
Hery <hery@ittron.co.id>
2021-11-27 21:54:54 +0700

    add SerializableClosureFactory on CQueue

---
+++ system/libraries/CQueue/SerializableClosureFactory.php
@@ -0,0 +1,20 @@
+<?php
+use Opis\Closure\SerializableClosure as OpisSerializableClosure;
+
+class CQueue_SerializableClosureFactory {
+    /**
+     * Creates a new serializable closure from the given closure.
+     *
+     * @param \Closure $closure
+     *
+     * @return \OpisSerializableClosure
+     */
+    public static function make($closure) {
+        if (\PHP_VERSION_ID < 70400) {
+            return new OpisSerializableClosure($closure);
+        }
+
+        return new OpisSerializableClosure($closure);
+        //return new SerializableClosure($closure);
+    }
+}
