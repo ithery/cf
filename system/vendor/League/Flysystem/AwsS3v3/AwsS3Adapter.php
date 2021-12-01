81adec9112279e2caf10f0ddb4e37e8ffe943b40
hery <hery@ittron.co.id>
2021-12-02 00:24:52 +0700

    update AwsS3Adapter

--- system/vendor/League/Flysystem/AwsS3v3/AwsS3Adapter.php
+++ system/vendor/League/Flysystem/AwsS3v3/AwsS3Adapter.php
@@ -40,6 +40,7 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles {
         'ContentDisposition',
         'ContentEncoding',
         'ContentLength',
+        'ContentMD5',
         'ContentType',
         'Expires',
         'GrantFullControl',
@@ -74,18 +75,16 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles {
     protected $options = [];
 
     /**
-     * Constructor.
-     *
-     * @param S3ClientInterface $client
-     * @param string            $bucket
-     * @param string            $prefix
-     * @param array             $options
+     * @var bool
      */
-    public function __construct(S3ClientInterface $client, $bucket, $prefix = '', array $options = []) {
+    private $streamReads;
+
+    public function __construct(S3ClientInterface $client, $bucket, $prefix = '', array $options = [], $streamReads = true) {
         $this->s3Client = $client;
         $this->bucket = $bucket;
         $this->setPathPrefix($prefix);
         $this->options = $options;
+        $this->streamReads = $streamReads;
     }
 
     /**
@@ -396,18 +395,16 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles {
      * @return bool
      */
     public function copy($path, $newpath) {
-        $command = $this->s3Client->getCommand(
-            'copyObject',
-            [
-                'Bucket' => $this->bucket,
-                'Key' => $this->applyPathPrefix($newpath),
-                'CopySource' => S3Client::encodeKey($this->bucket . '/' . $this->applyPathPrefix($path)),
-                'ACL' => $this->getRawVisibility($path) === AdapterInterface::VISIBILITY_PUBLIC ? 'public-read' : 'private',
-            ] + $this->options
-        );
-
         try {
-            $this->s3Client->execute($command);
+            $this->s3Client->copy(
+                $this->bucket,
+                $this->applyPathPrefix($path),
+                $this->bucket,
+                $this->applyPathPrefix($newpath),
+                $this->getRawVisibility($path) === AdapterInterface::VISIBILITY_PUBLIC
+                    ? 'public-read' : 'private',
+                $this->options
+            );
         } catch (S3Exception $e) {
             return false;
         }
@@ -444,10 +441,10 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles {
         $options = [
             'Bucket' => $this->bucket,
             'Key' => $this->applyPathPrefix($path),
-        ];
+        ] + $this->options;
 
-        if (isset($this->options['@http'])) {
-            $options['@http'] = $this->options['@http'];
+        if ($this->streamReads && !isset($options['@http']['stream'])) {
+            $options['@http']['stream'] = true;
         }
 
         $command = $this->s3Client->getCommand('getObject', $options + $this->options);
@@ -511,7 +508,7 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles {
      * @inheritdoc
      */
     public function setPathPrefix($prefix) {
-        $prefix = ltrim($prefix, '/');
+        $prefix = ltrim((string) $prefix, '/');
 
         return parent::setPathPrefix($prefix);
     }
@@ -537,7 +534,9 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles {
 
         foreach ($result->get('Grants') as $grant) {
             if (
-                    isset($grant['Grantee']['URI']) && $grant['Grantee']['URI'] === self::PUBLIC_GRANT_URI && $grant['Permission'] === 'READ'
+                isset($grant['Grantee']['URI'])
+                && $grant['Grantee']['URI'] === self::PUBLIC_GRANT_URI
+                && $grant['Permission'] === 'READ'
             ) {
                 $visibility = AdapterInterface::VISIBILITY_PUBLIC;
 
@@ -682,7 +681,7 @@ class AwsS3Adapter extends AbstractAdapter implements CanOverwriteFiles {
 
             return $result['Contents'] || $result['CommonPrefixes'];
         } catch (S3Exception $e) {
-            if ($e->getStatusCode() === 403) {
+            if (in_array($e->getStatusCode(), [403, 404], true)) {
                 return false;
             }
 
