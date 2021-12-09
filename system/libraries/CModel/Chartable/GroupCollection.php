0bf3bb24049a658c3abad52ad5bda6f12c969c4d
Hery <hery@ittron.co.id>
2021-12-10 05:39:19 +0700

    add Chartable Trait to Model

---
+++ system/libraries/CModel/Chartable/GroupCollection.php
@@ -0,0 +1,40 @@
+<?php
+
+class CModel_Chartable_GroupCollection extends CCollection {
+    /**
+     * @param null|\Closure $closure
+     *
+     * @return array
+     */
+    public function toChart($closure = null) {
+        $closure = $closure ?? static function ($label) {
+            return $label;
+        };
+
+        return $this
+            ->sortByDesc('value')
+            ->pluck('label')
+            ->map(function ($name) use ($closure) {
+                return [
+                    'labels' => $this->pluck('label')->map($closure)->toArray(),
+                    'values' => $this->getChartsValues($name),
+                ];
+            })
+            ->toArray();
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return array
+     */
+    private function getChartsValues($name) {
+        return $this
+            ->map(static function ($item) use ($name) {
+                return $item->label === $name
+                    ? (int) $item->value
+                    : 0;
+            })
+            ->toArray();
+    }
+}
