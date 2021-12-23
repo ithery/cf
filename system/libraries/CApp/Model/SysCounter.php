c831dfa3bc3bdf42730640a61a6656bad687da44
Hery <hery@ittron.co.id>
2021-12-23 14:05:06 +0700

    add SysCounter

--- system/libraries/CApp/Model/SysCounter.php
+++ system/libraries/CApp/Model/SysCounter.php
@@ -6,7 +6,8 @@ defined('SYSPATH') or die('No direct access allowed.');
  * @author Hery Kurniawan
  * @license Ittron Global Teknologi <ittron.co.id>
  *
- * @since May 17, 2019, 3:02:59 AM
+ * @since Jun 19, 2018, 3:37:54 AM
  */
-trait CApp_Trait_ConstantTrait {
+class CApp_Model_SysCounter extends CApp_Model {
+    use CApp_Model_Trait_Roles;
 }
