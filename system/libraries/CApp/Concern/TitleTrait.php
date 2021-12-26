1eb4c6461d14774ba8a3b5ed52a86ea9f1df28ba
Hery <hery@ittron.co.id>
2021-12-26 22:20:16 +0700

    fix CApp Seo

--- system/libraries/CApp/Concern/TitleTrait.php
+++ system/libraries/CApp/Concern/TitleTrait.php
@@ -9,53 +9,9 @@ defined('SYSPATH') or die('No direct access allowed.');
  * @since Jul 27, 2019, 10:54:01 PM
  */
 trait CApp_Concern_TitleTrait {
-    protected $title;
-
-    protected $rawTitle;
+    use CTrait_Element_Property_Title;
 
     private $showTitle = true;
-
-    public function setTitle($title, $lang = true) {
-        /** @var CApp $this */
-        $this->rawTitle = $title;
-        if ($lang) {
-            $title = c::__($title);
-        }
-        $this->title = $title;
-
-        $this->seo()->setTitle($title);
-
-        return $this;
-    }
-
-    public function getTitle() {
-        return $this->rawTitle;
-    }
-
-    public function getTranslationTitle() {
-        return $this->title;
-    }
-
-    public function haveTitle() {
-        return strlen($this->title) > 0;
-    }
-
-    /**
-     * Call getTitle if parameter title is not passed
-     * Call setTitle if parameter title is passed.
-     *
-     * @param string $title
-     * @param bool   $lang
-     *
-     * @return mixed
-     */
-    public function title($title = null, $lang = true) {
-        if ($title != null) {
-            return $this->setTitle($title, $lang);
-        }
-
-        return $this->getTitle();
-    }
 
     public function showTitle($bool) {
         $this->showTitle = $bool;
