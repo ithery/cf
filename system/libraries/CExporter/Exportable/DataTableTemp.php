8c2daaf72aee79292d3a1b737083767345d280cd
hery <hery@ittron.co.id>
2022-01-25 19:50:01 +0700

    update datatabletemp

--- system/libraries/CExporter/Exportable/DataTableTemp.php
+++ system/libraries/CExporter/Exportable/DataTableTemp.php
@@ -1,32 +1,17 @@
 <?php
 
-class CExporter_Exportable_DataTableTemp extends CExporter_Exportable implements CExporter_Concern_FromDataTable, CExporter_Concern_WithHeadings, CExporter_Concern_WithMapping {
+class CExporter_Exportable_DataTableTemp extends CExporter_Exportable_DataTable {
     protected $file;
 
     protected $downloadId;
 
     public function __construct($file) {
         $this->file = $file;
-    }
-
-    protected function table() {
         $data = CAjax::getData($this->file);
 
         $table = unserialize(carr::get($data, 'data.table'));
-
-        return new CExporter_Exportable_DataTable($table);
-    }
-
-    public function dataTable() {
-        return $this->table()->dataTable();
-    }
-
-    public function map($data) {
-        return $this->table()->map($data);
-    }
-
-    public function headings() {
-        return $this->table()->headings();
+        $this->table = $table;
+        $this->columnFormats = [];
     }
 
     public function setDownloadId($downloadId) {
