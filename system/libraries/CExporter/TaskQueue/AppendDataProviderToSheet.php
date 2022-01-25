f499a7d0b08c501844826995f4dc6d9c487f2818
hery <hery@ittron.co.id>
2022-01-25 20:05:12 +0700

    debug

--- system/libraries/CExporter/TaskQueue/AppendDataProviderToSheet.php
+++ system/libraries/CExporter/TaskQueue/AppendDataProviderToSheet.php
@@ -79,7 +79,7 @@ class CExporter_TaskQueue_AppendDataProviderToSheet extends CQueue_AbstractTask 
         //CDaemon::log(json_encode($this->data));
         $paginationResult = $this->dataProvider->paginate($perPage, ['*'], 'page', $page);
         CDaemon::log('append row from data provider page:' . $page . ', perPage:' . $perPage . ' with total data:' . $paginationResult->total());
-        CDaemon::log('row:' . json_encode($paginationResult->items()));
+        //CDaemon::log('row:' . json_encode($paginationResult->items()));
         $sheet->appendRows($paginationResult->items(), $this->sheetExport);
         CDaemon::log('Memory Usage:' . memory_get_usage());
         $total = $paginationResult->total();
