bc58b298bf34e07f30e3dfc3899065e4896a8a34
Hery <hery@ittron.co.id>
2022-01-30 03:19:19 +0700

    update middleware

---
+++ system/libraries/CHTTP/Cookie/Middleware/AddQueuedCookiesToResponse.php
@@ -0,0 +1,37 @@
+<?php
+
+class CHTTP_Cookie_Middleware_AddQueuedCookiesToResponse {
+    /**
+     * The cookie jar instance.
+     *
+     * @var \CHTTP_Cookie
+     */
+    protected $cookies;
+
+    /**
+     * Create a new CookieQueue instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->cookies = CHTTP::cookie();
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \CHTTP_Request $request
+     * @param \Closure       $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        $response = $next($request);
+
+        foreach ($this->cookies->getQueuedCookies() as $cookie) {
+            $response->headers->setCookie($cookie);
+        }
+
+        return $response;
+    }
+}
