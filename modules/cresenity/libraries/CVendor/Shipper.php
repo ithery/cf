f827e0250e47edc4a4e011c43e03543de9074176
ari <ari.muntari1@gmail.com>
2022-02-11 14:11:19 +0700

    update shipper

--- modules/cresenity/libraries/CVendor/Shipper.php
+++ modules/cresenity/libraries/CVendor/Shipper.php
@@ -1520,8 +1520,8 @@ class CVendor_Shipper {
         if ($method == 'POST') {
             if ($data != null) {
                 $payload = json_encode($data);
-                echo $payload;
-                die;
+                // echo $payload;
+                // die;
                 curl_setopt($curl, CURLOPT_POST, true);
                 curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
             }
