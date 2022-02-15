5a240942aabacd8d1ccb611352ea4b2953c86e6d
Hery <hery@ittron.co.id>
2022-02-15 13:29:54 +0700

    add function inViewPort

--- media/js/cres/src/Cresenity.js
+++ media/js/cres/src/Cresenity.js
@@ -1210,4 +1210,19 @@ export default class Cresenity {
     showError(errMessage) {
         this.toast('error', errMessage);
     }
+
+    inViewPort(el) {
+        // Special bonus for those using jQuery
+        if (typeof jQuery === 'function' && el instanceof jQuery) {
+            el = el[0];
+        }
+
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+        );
+    }
 }
