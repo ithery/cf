4e9006d8bcd9965b676e989313bb291bdf8140ba
Hery <hery@ittron.co.id>
2022-03-04 00:11:55 +0700

    update label form input

--- system/libraries/CElement/FormInput/Label.php
+++ system/libraries/CElement/FormInput/Label.php
@@ -6,61 +6,29 @@ class CElement_FormInput_Label extends CElement_FormInput {
     public function __construct($id) {
         parent::__construct($id);
 
+        $this->tag = 'span';
         $this->type = 'label';
+        $this->isOneTag = false;
     }
 
-    public static function factory($id) {
-        return new static($id);
+    public static function factory($id = '') {
+        return CElement_Factory::create(static::class, $id);
     }
 
-    public function html($indent = 0) {
-        $html = new CStringBuilder();
-        $html->setIndent($indent);
-        $disabled = '';
-
-        if ($this->disabled) {
-            $disabled = ' disabled="disabled"';
+    protected function build() {
+        $value = $this->value;
+        if ($value instanceof CRenderable) {
+            $this->value = $value->html();
+            $this->addJs($value->js());
+            $value = $this->value;
         }
+        $this->value = (string) $value;
+        $this->addJs($this->getDependsOnContentJavascript());
+        $this->addClass('label');
+        $this->setAttr('name', $this->name);
+        parent::build();
 
-        $classes = $this->classes;
-        $classes = implode(' ', $classes);
-        if (strlen($classes) > 0) {
-            $classes = ' ' . $classes;
-        }
-        $custom_css = $this->custom_css;
-        $custom_css = $this->renderStyle($custom_css);
-        if (strlen($custom_css) > 0) {
-            $custom_css = ' style="' . $custom_css . '"';
-        }
-        $bootstrap_class = '';
-
-        if (is_array($this->value)) {
-            $i = 0;
-            foreach ($this->value as $val) {
-                $new_val = $val;
-                foreach ($this->transforms as $trans) {
-                    $new_val = $trans->execute($new_val);
-                }
-                $html->appendln('<span class="label ' . $bootstrap_class . '" id="' . $this->id . '_' . $i . '">' . $new_val . '</span>')->br();
-                $i++;
-            }
-        } else {
-            $new_val = $this->value;
-            foreach ($this->transforms as $trans) {
-                $new_val = $trans->execute($new_val);
-            }
-            $html->appendln('<span class="label ' . $bootstrap_class . $classes . '" name="' . $this->name . '" id="' . $this->id . '" ' . $custom_css . '>' . $new_val . '</span>')->br();
-        }
-
-        return $html->text();
-    }
-
-    public function js($indent = 0) {
-        $js = new CStringBuilder();
-
-        $js->append(parent::js());
-        $js->append($this->getDependsOnContentJavascript());
-
-        return $js->text();
+        $this->setAttr('value', '');
+        $this->add($this->value);
     }
 }
