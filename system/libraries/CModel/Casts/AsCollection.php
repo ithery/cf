02a3a58f5a0f34598366fa0c99df3407cc46cb03
Hery <hery@ittron.co.id>
2022-03-04 02:33:17 +0700

    add new CModel Object

---
+++ system/libraries/CModel/Casts/AsCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+class CModel_Casts_AsCollection implements CModel_Contract_CastableInterface {
+    /**
+     * Get the caster class to use when casting from / to this cast target.
+     *
+     * @param array $arguments
+     *
+     * @return object|string
+     */
+    public static function castUsing(array $arguments) {
+        return new class() implements CModel_Contract_CastsAttributesInterface {
+            public function get($model, $key, $value, $attributes) {
+                return isset($attributes[$key]) ? new CCollection(json_decode($attributes[$key], true)) : null;
+            }
+
+            public function set($model, $key, $value, $attributes) {
+                return [$key => json_encode($value)];
+            }
+        };
+    }
+}
