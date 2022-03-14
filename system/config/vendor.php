dedf1ecf09a9c1a386b42a9b777823bd641e0b6a
Hery <hery@ittron.co.id>
2022-03-15 01:05:02 +0700

    update default vendor config

--- system/config/vendor.php
+++ system/config/vendor.php
@@ -1,11 +1,20 @@
 <?php
 
+defined('SYSPATH') or die('No direct access allowed.');
+
 return [
-    'google' => [
-        'geocoding_api_key' => '',
-    ],
     'sendgrid' => [
+        'key' => c::env('SENDGRID_API_KEY'),
     ],
-    'nexmo' => [
+    'google' => [
+        'geocoding_api_key' => c::env('GEOCODING_API_KEY'),
+        'recaptcha_v3_site_key' => c::env('RECAPTCHA_V3_SITE_KEY'),
+        'recaptcha_v3_api_key' => c::env('RECAPTCHA_V3_API_KEY'),
+        'recaptcha_v2_site_key' => c::env('RECAPTCHA_V2_SITE_KEY'),
+        'recaptcha_v2_api_key' => c::env('RECAPTCHA_V2_API_KEY'),
+    ],
+    'zenziva' => [
+        'key' => c::env('ZENZIVA_API_KEY'),
+        'secret' => c::env('ZENZIVA_API_SECRET'),
     ],
 ];
