68d6ab632f86cd7ee5b2da4c3e747be1e96d2477
Hery <hery@ittron.co.id>
2022-04-10 01:28:20 +0700

    fix for php 7

--- system/vendor/League/Flysystem/UnableToCheckExistence.php
+++ system/vendor/League/Flysystem/UnableToCheckExistence.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace League\Flysystem;
 
-use RuntimeException;
 use Throwable;
+use RuntimeException;
 
-class UnableToCheckExistence extends RuntimeException implements FilesystemOperationFailed
-{
-    public static function forLocation(string $path, Throwable $exception = null): static
-    {
+class UnableToCheckExistence extends RuntimeException implements FilesystemOperationFailed {
+    public static function forLocation(string $path, Throwable $exception = null) {
         return new static("Unable to check existence for: ${path}", 0, $exception);
     }
 
-    public function operation(): string
-    {
+    public function operation(): string {
         return FilesystemOperationFailed::OPERATION_EXISTENCE_CHECK;
     }
 }
