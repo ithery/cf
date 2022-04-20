6b83dae61c9a946f9fe51f78c6eff1c5a233bd02
Hery <hery@ittron.co.id>
2022-04-20 22:33:34 +0700

    update 5.6

--- system/vendor/League/Flysystem/GoogleCloudStorage/PortableVisibilityHandler.php
+++ system/vendor/League/Flysystem/GoogleCloudStorage/PortableVisibilityHandler.php
@@ -1,21 +1,21 @@
 <?php
 
-declare(strict_types=1);
-
 namespace League\Flysystem\GoogleCloudStorage;
 
-use Google\Cloud\Core\Exception\NotFoundException;
 use Google\Cloud\Storage\Acl;
-use Google\Cloud\Storage\StorageObject;
 use League\Flysystem\Visibility;
+use Google\Cloud\Storage\StorageObject;
+use Google\Cloud\Core\Exception\NotFoundException;
 
-class PortableVisibilityHandler implements VisibilityHandler
-{
+class PortableVisibilityHandler implements VisibilityHandler {
     public const NO_PREDEFINED_VISIBILITY = 'noPredefinedVisibility';
 
     public const ACL_PUBLIC_READ = 'publicRead';
+
     public const ACL_AUTHENTICATED_READ = 'authenticatedRead';
+
     public const ACL_PRIVATE = 'private';
+
     public const ACL_PROJECT_PRIVATE = 'projectPrivate';
 
     /**
@@ -43,32 +43,29 @@ class PortableVisibilityHandler implements VisibilityHandler
         $this->predefinedPrivateAcl = $predefinedPrivateAcl;
     }
 
-    public function setVisibility(StorageObject $object, string $visibility): void
-    {
-        if ($visibility === Visibility::PRIVATE) {
+    public function setVisibility(StorageObject $object, string $visibility): void {
+        if ($visibility === Visibility::VISIBILITY_PRIVATE) {
             $object->acl()->delete($this->entity);
-        } elseif ($visibility === Visibility::PUBLIC) {
+        } elseif ($visibility === Visibility::VISIBILITY_PUBLIC) {
             $object->acl()->update($this->entity, Acl::ROLE_READER);
         }
     }
 
-    public function determineVisibility(StorageObject $object): string
-    {
+    public function determineVisibility(StorageObject $object): string {
         try {
             $acl = $object->acl()->get(['entity' => 'allUsers']);
         } catch (NotFoundException $exception) {
-            return Visibility::PRIVATE;
+            return Visibility::VISIBILITY_PRIVATE;
         }
 
         return $acl['role'] === Acl::ROLE_READER
-            ? Visibility::PUBLIC
-            : Visibility::PRIVATE;
+            ? Visibility::VISIBILITY_PUBLIC
+            : Visibility::VISIBILITY_PRIVATE;
     }
 
-    public function visibilityToPredefinedAcl(string $visibility): string
-    {
+    public function visibilityToPredefinedAcl(string $visibility): string {
         switch ($visibility) {
-            case Visibility::PUBLIC:
+            case Visibility::VISIBILITY_PUBLIC:
                 return $this->predefinedPublicAcl;
             case self::NO_PREDEFINED_VISIBILITY:
                 return self::NO_PREDEFINED_VISIBILITY;
