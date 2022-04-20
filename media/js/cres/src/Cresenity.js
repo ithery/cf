c94a27f594f864f01924965bfc928f0bbb34a115
Hery <hery@ittron.co.id>
2022-04-20 23:09:12 +0700

    fix

--- media/js/cres/src/Cresenity.js
+++ media/js/cres/src/Cresenity.js
@@ -979,9 +979,9 @@ export default class Cresenity {
         window.addEventListener('DOMContentLoaded', () => {
             const comp = document.querySelector("[defer-x-data]")
             comp.setAttribute('x-data', comp.getAttribute('defer-x-data'))
-            window.Alpine.start()
-              // document.dispatchEvent(new CustomEvent('turbolinks:load'))
-        })
+            window.Alpine.start();
+            // document.dispatchEvent(new CustomEvent('turbolinks:load'))
+        });
         this.alpine = new CresAlpine(window.Alpine);
     }
 
