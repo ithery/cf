ebbb11396128e3c1f814ea232465e18b19a8ef8f
Hery <hery@ittron.co.id>
2022-04-22 15:21:57 +0700

    add getScrollParent function to cresenity

--- media/js/cres/src/Cresenity.js
+++ media/js/cres/src/Cresenity.js
@@ -1288,4 +1288,20 @@ export default class Cresenity {
         });
 
     }
+    getScrollParent(element, includeHidden = false) {
+        var style = getComputedStyle(element);
+        var excludeStaticParent = style.position === "absolute";
+        var overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
+
+        if (style.position === "fixed") return document.body;
+        for (var parent = element; (parent = parent.parentElement);) {
+            style = getComputedStyle(parent);
+            if (excludeStaticParent && style.position === "static") {
+                continue;
+            }
+            if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX)) return parent;
+        }
+
+        return document.body;
+    }
 }
