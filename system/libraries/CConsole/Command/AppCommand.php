f4ab095b5c427884d8a04aabd8454fe46d4899e9
Haris Thohir <haris@thohir.com>
2022-05-12 10:54:09 +0700

    update appCommand

--- system/libraries/CConsole/Command/AppCommand.php
+++ system/libraries/CConsole/Command/AppCommand.php
@@ -1,20 +1,27 @@
 <?php
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 class CConsole_Command_AppCommand extends CConsole_Command {
     protected $prefix;
 
-    public function __construct() {
-        parent::__construct();
+    /**
+     * Execute the console command.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface   $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return mixed
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
         $this->prefix = CF::config('app.prefix');
 
         if (strlen($this->prefix) == 0) {
-            // echo "Application prefix is required, make sure You on app directory. You can define it on app config using key \"prefix\"\n";
-            // echo "\n" . get_called_class()."\n";
-            // exit;
+            $this->error('Application prefix is required, make sure You on app directory. You can define it on app config using key "prefix"');
+            exit;
         }
-    }
 
-    // public function handler() {
-    //
-    // }
+        return CContainer::getInstance()->call([$this, 'handle']);
+    }
 }
