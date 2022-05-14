a5db489957f3752fcee7e083becab36cc1af1966
Hery <hery@ittron.co.id>
2022-05-14 23:15:49 +0700

    update ExceptionHandler for invalid json response

--- system/libraries/CException/ExceptionHandler.php
+++ system/libraries/CException/ExceptionHandler.php
@@ -297,8 +297,11 @@ class CException_ExceptionHandler implements CException_ExceptionHandlerInterfac
      */
     protected function invalidJson($request, CValidation_Exception $exception) {
         return c::response()->json([
-            'message' => $exception->getMessage(),
-            'errors' => $exception->errors(),
+            'errCode' => '422',
+            'errMessage' => $exception->getMessage(),
+            'data' => [
+                'errors' => $exception->errors(),
+            ],
         ], $exception->status);
     }
 
@@ -479,6 +482,7 @@ class CException_ExceptionHandler implements CException_ExceptionHandlerInterfac
                 $response->getStatusCode(),
                 $response->headers->all()
             );
+            $response->setRequest(c::request());
         } else {
             $response = new CHTTP_Response(
                 $response->getContent(),
