4299c692e40e0660ded1f9a0ed6d0ec6bb033e98
Hery <hery@ittron.co.id>
2022-05-19 21:18:17 +0700

    update element image

--- system/libraries/CElement/FormInput/Image.php
+++ system/libraries/CElement/FormInput/Image.php
@@ -9,12 +9,15 @@ defined('SYSPATH') or die('No direct access allowed.');
  * @since Jun 24, 2018, 6:55:42 PM
  */
 class CElement_FormInput_Image extends CElement_FormInput {
-    use CElement_Trait_Template,
+    use CElement_Trait_UseViewTrait,
         CTrait_Compat_Element_FormInput_Image;
 
     protected $imgSrc;
+
     protected $maxWidth;
+
     protected $maxHeight;
+
     protected $disabledUpload;
 
     public function __construct($id) {
@@ -25,7 +28,7 @@ class CElement_FormInput_Image extends CElement_FormInput {
         $this->maxWidth = '200';
         $this->maxHeight = '150';
         $this->disabledUpload = false;
-        $this->templateName = 'CElement/FormInput/Image';
+        $this->templateName = 'cresenity/element/form-input/image';
 
         $this->onBeforeParse(function () {
             $this->setVar('id', $this->id);
@@ -42,33 +45,37 @@ class CElement_FormInput_Image extends CElement_FormInput {
 
     public function setImgSrc($imgsrc) {
         $this->imgSrc = $imgsrc;
+
         return $this;
     }
 
     public function setMaxWidth($maxwidth) {
         $this->maxWidth = $maxwidth;
+
         return $this;
     }
 
     public function setMaxHeight($maxheight) {
         $this->maxHeight = $maxheight;
+
         return $this;
     }
 
     public function setDisabledUpload($bool) {
         $this->disabledUpload = $bool;
+
         return $this;
     }
 
     public function html($indent = 0) {
-        $templateHtml = $this->getTemplateHtml();
+        $templateHtml = $this->getViewHtml();
         $html = $templateHtml;
 
         return $html;
     }
 
     public function js($indent = 0) {
-        $templateJs = $this->getTemplateJs();
+        $templateJs = $this->getViewJs();
         $js = $templateJs;
 
         return $js;
