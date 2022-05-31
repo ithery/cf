46881a8558e0a827dc4015623cc062385bd22945
Hery <hery@ittron.co.id>
2022-05-31 19:34:32 +0700

    update kirim email driver

---
+++ system/libraries/CEmail/Driver/KirimEmailDriver.php
@@ -0,0 +1,112 @@
+<?php
+
+class CEmail_Driver_KirimEmailDriver extends CEmail_DriverAbstract {
+    public function send(array $to, $subject, $body, $options = []) {
+        $apiKey = $this->config->getPassword();
+        $username = $this->config->getUsername();
+        $time = time();
+        $generatedToken = hash_hmac('sha256', $username . '::' . $apiKey . '::' . $time, $apiKey);
+
+        $domain = carr::get($options, 'domain', carr::get($options, 'smtp_domain'));
+
+        $from = carr::get($options, 'from', $this->config->getFrom());
+        $fromName = carr::get($options, 'from_name', $this->config->getFromName());
+        $attachments = carr::get($options, 'attachments', []);
+        $replyTo = carr::get($options, 'replyTo', '');
+        $cc = carr::get($options, 'cc', []);
+        $bcc = carr::get($options, 'bcc', []);
+
+        $toEmail = '';
+        if (!is_array($to)) {
+            $to = [$to];
+        }
+        foreach ($to as $toItem) {
+            $toName = '';
+            $toEmail = $toItem;
+            $email = $toEmail;
+            if (is_array($toItem)) {
+                $toName = carr::get($toItem, 'toName');
+                $toEmail = carr::get($toItem, 'toEmail');
+                $email = $toName . '" <' . $toEmail . '>';
+            }
+            if (strlen($toEmail) > 0) {
+                $toEmail .= ';';
+            }
+            $toEmail .= $email;
+        }
+        $cc = carr::wrap($cc);
+        $bcc = carr::wrap($bcc);
+
+        foreach ($attachments as $att) {
+            $disk = '';
+            if (is_array($att)) {
+                $path = carr::get($att, 'path');
+                $filename = basename($path);
+                $attachmentFilename = carr::get($att, 'filename');
+                $type = carr::get($att, 'type');
+                $disk = carr::get($att, 'disk');
+            } else {
+                $path = $att;
+                $filename = basename($att);
+                $attachmentFilename = $filename;
+                $type = '';
+            }
+
+            if (strlen($type) == 0) {
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+                $type = 'application/text';
+                if ($ext == 'pdf') {
+                    $type = 'application/pdf';
+                }
+                if ($ext == 'jpg' || $ext == 'jpeg') {
+                    $type = 'image/jpeg';
+                }
+                if ($ext == 'png') {
+                    $type = 'image/png';
+                }
+            }
+            $content = '';
+            if (strlen($disk) > 0) {
+                $diskObject = CStorage::instance()->disk($disk);
+                $content = $diskObject->get($path);
+            } else {
+                $content = file_get_contents($path);
+            }
+            $attachment = new CVendor_SendGrid_Mail_Attachment();
+            $attachment->setContent(base64_encode($content));
+            $attachment->setType($type);
+            $attachment->setDisposition('attachment');
+            $attachment->setFilename($attachmentFilename);
+        }
+
+        //build params
+        $auth = 'api:' . $generatedToken;
+        $apiEndPoint = 'https://aplikasi.kirim.email/api/v3/transactional/messages';
+        $headers = [
+            'Domain' => $domain,
+            'Authorization' => 'Basic ' . $auth,
+        ];
+        $post = [
+            'from' => $from,
+            'to' => $toEmail,
+            'subject' => $subject,
+            'html' => $body,
+            'headers' => []
+        ];
+        if (strlen($fromName) > 0) {
+            $post['from_name'] = $fromName;
+        }
+        if (strlen($replyTo) > 0) {
+            $post['headers']['Reply-To'] = $replyTo;
+        }
+        $curl = CCurl::factory($apiEndPoint);
+        $curl->setHttpHeader(c::collect($headers)->map(function ($value, $key) {
+            return $key . ':' . $value;
+        })->toArray());
+        $curl->setPost($post);
+        $response = $curl->exec()->response();
+
+        return $response;
+    }
+}
