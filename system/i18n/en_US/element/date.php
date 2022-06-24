17a69683413df31b1b32fdc3f8fc50ee8e239a90
Hery <hery@ittron.co.id>
2022-06-24 19:53:36 +0700

    update date range translation

---
+++ system/i18n/en_US/element/date.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'daterange:lifetime' => 'Life Time',
+    'daterange:today' => 'Today',
+    'daterange:yesterday' => 'Yesterday',
+    'daterange:lastNDays' => 'Last :n Days',
+    'daterange:thisWeek' => 'This Week',
+    'daterange:lastWeek' => 'Last Week',
+    'daterange:thisMonth' => 'This Month',
+    'daterange:lastMonth' => 'Last Month',
+    'daterange:customRange' => 'Custom Range',
+    'apply' => 'Apply',
+    'cancel' => 'Batal',
+
+];
---
+++ system/i18n/id_ID/element/date.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'daterange:lifetime' => 'Semua Waktu',
+    'daterange:today' => 'Hari Ini',
+    'daterange:yesterday' => 'Kemarin',
+    'daterange:lastNDays' => ':n Hari Terakhir',
+    'daterange:thisWeek' => 'Minggu Ini',
+    'daterange:lastWeek' => 'Minggu Kemarin',
+    'daterange:thisMonth' => 'Bulan Ini',
+    'daterange:lastMonth' => 'Bulan Kemarin',
+    'daterange:customRange' => 'Rentang Kustom',
+    'apply' => 'Apply',
+    'cancel' => 'Batal',
+];
