88c0cfcead29603b64c8dd42bd8545e5283fe560
Hery <hery@ittron.co.id>
2022-07-01 04:49:26 +0700

    update js datatable to 1.10.16

--- application/cresenity/default/controllers/demo/dashboard.php
+++ application/cresenity/default/controllers/demo/dashboard.php
@@ -3,10 +3,21 @@ use Cresenity\Demo\Model\Country as CountryModel;
 
 class Controller_Demo_Dashboard extends \Cresenity\Demo\Controller {
     public function index() {
-        $country = CountryModel::first();
+        $app = c::app();
+        $app->setTitle('Dashboard');
 
-        cdbg::dd($country);
+        $table = $app->addTable();
 
-        return c::app()->setTitle('Dashboard');
+        $table->setDataFromModel(CountryModel::class);
+        $table->addColumn('id')->setLabel('ID');
+        $table->addColumn('name')->setLabel('Name');
+        $table->addColumn('continent')->setLabel('Continent');
+        $table->addColumn('code')->setLabel('Code');
+        $table->addColumn('code3')->setLabel('Code 3');
+        $table->addColumn('num')->setLabel('Number');
+        $table->addColumn('isd')->setLabel('ISD');
+        $table->setAjax();
+
+        return $app;
     }
 }
