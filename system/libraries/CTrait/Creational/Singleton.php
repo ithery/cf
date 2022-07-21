9060030f8980c3ef10dcc50d79832872def367e9
Hery <hery@ittron.co.id>
2022-07-22 05:17:36 +0700

    update

--- system/libraries/CTrait/Creational/Singleton.php
+++ system/libraries/CTrait/Creational/Singleton.php
@@ -1,22 +1,22 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Dec 30, 2017, 8:28:30 PM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Dec 30, 2017, 8:28:30 PM
  */
 trait CTrait_Creational_Singleton {
-
     protected static $instance;
 
     final public static function instance() {
-        return isset(static::$instance) ? static::$instance : static::$instance = new static;
+        return isset(static::$instance) ? static::$instance : static::$instance = new static();
     }
 
     final public static function clean() {
-        return static::$instance = new static;
+        return static::$instance = new static();
     }
 
     final private function __construct() {
@@ -24,15 +24,11 @@ trait CTrait_Creational_Singleton {
     }
 
     protected static function init() {
-        
     }
 
     final private function __wakeup() {
-        
     }
 
     final private function __clone() {
-        
     }
-
 }
