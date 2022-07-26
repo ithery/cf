656b4e247809fb03f1949a4eeec326bbaa3109f5
Hery <hery@ittron.co.id>
2022-07-26 13:53:54 +0700

    update odbc

--- system/libraries/CDatabase/Driver/VersionAwarePlatformInterface.php
+++ system/libraries/CDatabase/Driver/VersionAwarePlatformInterface.php
@@ -1,23 +1,23 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Aug 18, 2018, 8:24:23 AM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Aug 18, 2018, 8:24:23 AM
  */
 interface CDatabase_Driver_VersionAwarePlatformInterface {
-
     /**
      * Factory method for creating the appropriate platform instance for the given version.
      *
      * @param string $version The platform/server version string to evaluate. This should be given in the notation
      *                        the underlying database vendor uses.
      *
-     * @return CDatabase_Platform
+     * @throws CDatabase_Exception if the given version string could not be evaluated
      *
-     * @throws CDatabase_Exception if the given version string could not be evaluated.
+     * @return CDatabase_Platform
      */
     public function createDatabasePlatformForVersion($version);
 }
