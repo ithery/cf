563cebf321354c2701e797c082be692feb2fceb9
Hery <hery@ittron.co.id>
2022-08-10 14:10:06 +0700

    fix php-8 syntax for php 5.7 & 7

--- system/libraries/CPeriod/Trait/OperationTrait.php
+++ system/libraries/CPeriod/Trait/OperationTrait.php
@@ -142,7 +142,7 @@ trait CPeriod_Trait_OperationTrait {
                 $this->includedStart(),
                 $other->includedStart()->sub($this->interval),
                 $this->precision(),
-                $this->boundaries(),
+                $this->boundaries()
             );
         }
 
@@ -151,7 +151,7 @@ trait CPeriod_Trait_OperationTrait {
                 $other->includedEnd()->add($this->interval),
                 $this->includedEnd(),
                 $this->precision(),
-                $this->boundaries(),
+                $this->boundaries()
             );
         }
 
