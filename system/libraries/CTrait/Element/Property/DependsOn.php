327c67a79fac252b4e6e33dad8b921ddb3866e8e
Hery <hery@ittron.co.id>
2022-08-24 20:31:24 +0700

    update dependsOn type hidden and transform on input hidden

--- system/libraries/CTrait/Element/Property/DependsOn.php
+++ system/libraries/CTrait/Element/Property/DependsOn.php
@@ -81,4 +81,57 @@ trait CTrait_Element_Property_DependsOn {
 
         return $js->text();
     }
+
+    public function getDependsOnValueJavascript() {
+        $js = new CStringBuilder();
+        foreach ($this->dependsOn as $dependOn) {
+            //we create ajax method
+
+            $dependsOnSelector = $dependOn->getSelector();
+            $targetSelector = '#' . $this->id();
+            $ajaxMethod = CAjax::createMethod();
+            $ajaxMethod->setType('DependsOn');
+            $ajaxMethod->setMethod('post');
+            $ajaxMethod->setData('dependsOn', serialize($dependOn));
+            $ajaxMethod->setData('from', static::class);
+            $ajaxUrl = $ajaxMethod->makeUrl();
+            $throttle = $dependOn->getThrottle();
+            $optionsJson = '{';
+            $optionsJson .= "url:'" . $ajaxUrl . "',";
+            $optionsJson .= "method:'" . 'post' . "',";
+            $optionsJson .= !$dependOn->getBlock() ? 'block: false,' : '';
+
+            $optionsJson .= "dataAddition: { value: $('" . $dependsOnSelector . "').val() },";
+            $optionsJson .= "onSuccess: (data) => {
+                let jQueryTarget = $('" . $targetSelector . "');
+
+                if(typeof data == 'object') {
+                    if(typeof data.value !== 'undefined') {
+                        jQueryTarget.val(data.value);
+                    } else {
+                        jQueryTarget.val(JSON.stringify(data));
+                    }
+                } else {
+                    jQueryTarget.val(data);
+                }
+
+
+            },";
+            $optionsJson .= 'handleJsonResponse: true';
+            $optionsJson .= '}';
+
+            $optionsJson = str_replace(["\r\n", "\n", "\r"], '', $optionsJson);
+
+            $dependsOnFunctionName = 'dependsOnFunction' . uniqid();
+            $js->appendln('
+                 let ' . $dependsOnFunctionName . ' = () => {
+                     cresenity.ajax(' . $optionsJson . ");
+                 };
+                 $('" . $dependsOnSelector . "').change(cresenity.debounce(" . $dependsOnFunctionName . ' ,' . $throttle . '));
+                 ' . $dependsOnFunctionName . '();
+             ');
+        }
+
+        return $js->text();
+    }
 }
