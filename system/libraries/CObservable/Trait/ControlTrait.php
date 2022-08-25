2ce7562c8808a08d45db438873e5ecf35eda80d3
Alvin <jeremyalvinchandranata@gmail.com>
2022-08-25 07:00:50 +0700

    add function addImageAjaxControl

--- system/libraries/CObservable/Trait/ControlTrait.php
+++ system/libraries/CObservable/Trait/ControlTrait.php
@@ -231,6 +231,20 @@ trait CObservable_Trait_ControlTrait {
     }
 
     /**
+     * Create Image Ajax Control.
+     *
+     * @param null|string $id
+     *
+     * @return CElement_FormInput_ImageAjax
+     */
+    public function addImageAjaxControl($id = null) {
+        $control = new CElement_FormInput_ImageAjax($id);
+        $this->wrapper->add($control);
+
+        return $control;
+    }
+
+    /**
      * Create Multiple Image Ajax Control.
      *
      * @param null|string $id
