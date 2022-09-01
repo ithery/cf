0fd706db49cff4006d1efac83f6a65cd5f78cce8
Hery <hery@ittron.co.id>
2022-09-01 10:53:46 +0700

    update CTesting

---
+++ system/libraries/CTesting/Trait/RefreshDatabaseTrait.php
@@ -0,0 +1,87 @@
+<?php
+
+trait CTesting_Trait_RefreshDatabase {
+    /**
+     * Define hooks to migrate the database before and after each test.
+     *
+     * @return void
+     */
+    public function refreshDatabase() {
+        $this->beforeRefreshingDatabase();
+
+        $this->refreshTestDatabase();
+
+        $this->afterRefreshingDatabase();
+    }
+
+    /**
+     * Refresh a conventional test database.
+     *
+     * @return void
+     */
+    protected function refreshTestDatabase() {
+        $this->beginDatabaseTransaction();
+    }
+
+    /**
+     * Begin a database transaction on the testing database.
+     *
+     * @return void
+     */
+    public function beginDatabaseTransaction() {
+        foreach ($this->connectionsToTransact() as $name) {
+            $connection = CDatabase::instance($name);
+            $dispatcher = $connection->getEventDispatcher();
+
+            $connection->unsetEventDispatcher();
+            $connection->beginTransaction();
+            $connection->setEventDispatcher($dispatcher);
+
+            if ($connection->getTransactionManager()) {
+                $connection->getTransactionManager()->callbacksShouldIgnore(
+                    $connection->getTransactionManager()->getTransactions()->first()
+                );
+            }
+        }
+
+        $this->beforeApplicationDestroyed(function () {
+            foreach ($this->connectionsToTransact() as $name) {
+                $connection = CDatabase::instance($name);
+                $dispatcher = $connection->getEventDispatcher();
+
+                $connection->unsetEventDispatcher();
+                $connection->rollBack();
+                $connection->setEventDispatcher($dispatcher);
+                //$connection->disconnect();
+            }
+        });
+    }
+
+    /**
+     * The database connections that should have transactions.
+     *
+     * @return array
+     */
+    protected function connectionsToTransact() {
+        return property_exists($this, 'connectionsToTransact')
+                            ? $this->connectionsToTransact : [null];
+    }
+
+    /**
+     * Perform any work that should take place before the database has started refreshing.
+     *
+     * @return void
+     */
+    protected function beforeRefreshingDatabase() {
+        // ...
+    }
+
+    /**
+     * Perform any work that should take place once the database has finished refreshing.
+     *
+     * @return void
+     */
+    protected function afterRefreshingDatabase() {
+        // ...
+    }
+}
