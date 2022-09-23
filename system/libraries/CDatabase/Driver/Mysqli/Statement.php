61b85aa67348dcaf4c551565f6acf01de74c52aa
Hery <hery@ittron.co.id>
2022-09-23 19:31:41 +0700

    phpdoc

--- system/libraries/CDatabase/Driver/Mysqli/Statement.php
+++ system/libraries/CDatabase/Driver/Mysqli/Statement.php
@@ -1,22 +1,25 @@
 <?php
 
-defined('SYSPATH') OR die('No direct access allowed.');
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * @author Hery Kurniawan
- * @since Jun 30, 2019, 7:52:15 PM
  * @license Ittron Global Teknologi <ittron.co.id>
+ *
+ * @since Jun 30, 2019, 7:52:15 PM
  */
 
 /**
- * MySQLi Prepared Statement (experimental)
+ * MySQLi Prepared Statement (experimental).
  */
 class CDatabase_Driver_Mysqli_Statement {
+    protected $link = null;
 
-    protected $link = NULL;
     protected $stmt;
-    protected $varNames = array();
-    protected $varValues = array();
+
+    protected $varNames = [];
+
+    protected $varValues = [];
 
     public function __construct($sql, $link) {
         $this->link = $link;
@@ -30,26 +33,37 @@ class CDatabase_Driver_Mysqli_Statement {
         $this->stmt->close();
     }
 
-    // Sets the bind parameters
+    /**
+     * Sets the bind parameters.
+     *
+     * @param mixed $paramTypes
+     * @param mixed $params
+     *
+     * @return CDatabase_Driver_Mysqli_Statement
+     */
     public function bindParams($paramTypes, $params) {
         $this->varNames = array_keys($params);
         $this->varValues = array_values($params);
-        call_user_func_array(array($this->stmt, 'bind_param'), array_merge($paramTypes, $varNames));
+        call_user_func_array([$this->stmt, 'bind_param'], array_merge($paramTypes, $this->varNames));
 
         return $this;
     }
 
     public function bindResult($params) {
-        call_user_func_array(array($this->stmt, 'bind_result'), $params);
+        call_user_func_array([$this->stmt, 'bind_result'], $params);
     }
 
-    // Runs the statement
+    /**
+     * Runs the statement.
+     *
+     * @return void
+     */
     public function execute() {
         foreach ($this->varNames as $key => $name) {
             $$name = $this->varValues[$key];
         }
         $this->stmt->execute();
+
         return $this->stmt;
     }
-
 }
