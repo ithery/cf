883e87490edcb5fcce10feb5c4f689075fe2383f
Hery <hery@ittron.co.id>
2022-10-03 14:13:54 +0700

    update add refresh token decryptor

--- system/libraries/CTrait/Controller/Application/OAuth/RefreshToken.php
+++ system/libraries/CTrait/Controller/Application/OAuth/RefreshToken.php
@@ -1,4 +1,5 @@
 <?php
+use Defuse\Crypto\Crypto;
 
 trait CTrait_Controller_Application_OAuth_RefreshToken {
     protected function getTitle() {
@@ -13,7 +14,29 @@ trait CTrait_Controller_Application_OAuth_RefreshToken {
         $app = c::app();
         $app->title($this->getTitle());
         $oauth = CApi::oauth($this->getApiGroup());
+
+        $widget = $app->addWidget();
+        $form = $widget->addForm();
+        $form->addField()->setLabel('Refresh Token')->addTextControl('refresh_token')->setValue(c::request()->refresh_token);
+        if (c::request()->method() == 'POST') {
+            $refreshToken = c::request()->refresh_token;
+            $appKey = c::env('APP_KEY');
+            $encKey = base64_decode(substr($appKey, 7));
+            $crypto = null;
+
+            try {
+                $crypto = Crypto::decryptWithPassword($refreshToken, $encKey);
+            } catch (Exception $exception) {
+                return $exception;
+            }
+
+            $jsonDecrypted = json_decode($crypto, true);
+            $form->addPre()->addClass('my-3')->add($jsonDecrypted);
+        }
+
+        $form->addActionList()->addAction()->setLabel('Decrypt')->setSubmit();
         $table = $app->addTable();
+
         $table->setDataFromModel($oauth->refreshTokenModel(), function (CModel_Query $q) {
             $q->with(['oauthAccessToken', 'oauthAccessToken.oauthClient']);
             $q->whereHas('oauthAccessToken');
