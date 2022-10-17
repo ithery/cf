83ec1478a7e486edf97a042bed3fec8f04000ef6
Hery <hery@ittron.co.id>
2022-10-17 15:18:46 +0700

    update

--- system/libraries/CComponent/Exception/ComponentNotFoundException.php
+++ system/libraries/CComponent/Exception/ComponentNotFoundException.php
@@ -8,6 +8,6 @@ defined('SYSPATH') or die('No direct access allowed.');
  *
  * @since Nov 29, 2020
  */
-class CComponent_Exception_ComponentNotFoundException extends \Exception implements CDebug_Contract_ShouldNotCollectException {
+class CComponent_Exception_ComponentNotFoundException extends \Exception implements \CDebug_Contract_ShouldNotCollectException {
     use CComponent_Exception_BypassViewHandlerTrait;
 }
