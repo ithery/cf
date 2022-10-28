8906e91e150d89c9196085c2f4beb33add471218
Ecko <ecko.santoso@gmail.com>
2022-10-28 18:11:09 +0700

    add demo

--- application/cresenity/default/navs/demo/module.php
+++ application/cresenity/default/navs/demo/module.php
@@ -5,4 +5,9 @@ return [
         'label' => c::__('Chat Bot'),
         'uri' => 'demo/module/bot/index',
     ],
+    [
+        'name' => 'module.formatter',
+        'label' => c::__('Formatter'),
+        'uri' => 'demo/module/formatter/index',
+    ],
 ];
