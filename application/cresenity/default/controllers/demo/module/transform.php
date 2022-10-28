6681a9c71803704cc6f5e39b33547a760459bea5
Hery <hery@ittron.co.id>
2022-10-28 21:48:13 +0700

    update transform

--- application/cresenity/default/controllers/demo/module/transform.php
+++ application/cresenity/default/controllers/demo/module/transform.php
@@ -19,9 +19,13 @@ class Controller_Demo_Module_Transform extends \Cresenity\Demo\Controller {
         $widgetFormatCurrency = $app->addWidget()->setTitle('Transform Format Currency')->addClass('mb-3');
         $widgetFormatCurrency->add($globalValue . ' => ' . c::manager()->transform()->call('formatCurrency', $globalValue));
 
-        // format currency
+        // format distance
         $widgetFormatDistance = $app->addWidget()->setTitle('Transform Format Distance')->addClass('mb-3');
         $widgetFormatDistance->add($globalValue . ' => ' . c::manager()->transform()->call('formatDistance', $globalValue));
+
+        // format bytes
+        $widgetFormatByte = $app->addWidget()->setTitle('Transform Format Byte')->addClass('mb-3');
+        $widgetFormatByte->add($globalValue . ' => ' . c::manager()->transform()->call('formatByte', $globalValue));
 
         return $app;
     }
