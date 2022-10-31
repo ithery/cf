5c9f0e3109d4e65e4dd029f2fcb80974a11e07b8
Ecko <ecko.santoso@gmail.com>
2022-10-31 12:02:56 +0700

    demo showmore on table

--- application/cresenity/default/libraries/Cresenity/Demo/Model/Item.php
+++ application/cresenity/default/libraries/Cresenity/Demo/Model/Item.php
@@ -7,7 +7,7 @@ class Item extends \CModel {
 
     protected $rows = [
         ['items_id' => 1, 'category_id' => 1, 'name' => 'Item 1'],
-        ['items_id' => 2, 'category_id' => 1, 'name' => 'Item 2'],
+        ['items_id' => 2, 'category_id' => 1, 'name' => 'Item 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu lectus ante. Nulla in arcu dolor. Vestibulum efficitur ac nisi eget tristique. Aliquam feugiat elit quis volutpat iaculis'],
         ['items_id' => 3, 'category_id' => 1, 'name' => 'Item 3'],
         ['items_id' => 4, 'category_id' => 2, 'name' => 'Item 4'],
         ['items_id' => 5, 'category_id' => 2, 'name' => 'Item 5'],
