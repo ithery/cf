50caf58a7b76b6f1f00506beb0f9a408eb1f89f9
Hery <hery@ittron.co.id>
2022-11-07 23:54:32 +0700

    fix phpstan return

--- system/helpers/c.php
+++ system/helpers/c.php
@@ -682,7 +682,9 @@ class c {
      * @param null|array|string $key
      * @param mixed             $default
      *
-     * @return ($key is null ? CHTTP_Request : string|array)
+     * @return CHTTP_Request|string|array
+     *
+     * @phpstan-return ($key is null ? CHTTP_Request : string|array)
      */
     public static function request($key = null, $default = null) {
         if (is_null($key)) {
