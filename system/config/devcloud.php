ab005993cb0b2f5804f8b16bdb658e0c39406610
Hery <hery@ittron.co.id>
2022-12-01 23:05:07 +0700

    update devcloud inspector

--- system/config/devcloud.php
+++ system/config/devcloud.php
@@ -3,4 +3,268 @@
 return [
     'api_key' => c::env('DEVCLOUD_KEY'),
     'secret_key' => c::env('DEVCLOUD_SECRET'),
+    'inspector' => [
+        /*
+        |--------------------------------------------------------------------------
+        | Enabling
+        |--------------------------------------------------------------------------
+        |
+        | Setting "false" the package stop sending data to Inspector.
+        |
+        */
+
+        'enable' => c::env('DEVCLOUD_INSPECTOR_ENABLE', true),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Remote URL
+        |--------------------------------------------------------------------------
+        |
+        | You can set the url of the remote endpoint to send data to.
+        |
+        */
+
+        'url' => c::env('DEVCLOUD_INSPECTOR_URL', 'https://cpanel.ittron.co.id/inspector'),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Transport method
+        |--------------------------------------------------------------------------
+        |
+        | This is where you can set the data transport method.
+        | Supported options: "sync", "async"
+        |
+        */
+
+        'transport' => 'async',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Max number of items.
+        |--------------------------------------------------------------------------
+        |
+        | Max number of items to record in a single execution cycle.
+        |
+        */
+
+        'max_items' => 100,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Proxy
+        |--------------------------------------------------------------------------
+        |
+        | This is where you can set the transport option settings you'd like us to use when
+        | communicating with Inspector.
+        |
+        */
+
+        'options' => [
+            // 'proxy' => 'https://55.88.22.11:3128',
+            // 'curlPath' => '/usr/bin/curl',
+        ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Query
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to automatically add all queries executed in the timeline.
+        |
+        */
+
+        'query' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Bindings
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to include the query bindings.
+        |
+        */
+
+        'bindings' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | User
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to set the current user logged in via
+        | Laravel's authentication system.
+        |
+        */
+
+        'user' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Email
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to monitor email sending.
+        |
+        */
+
+        'email' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Notifications
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to monitor notifications.
+        |
+        */
+
+        'notifications' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | View
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to monitor background job processing.
+        |
+        */
+
+        'views' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Job
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to monitor background job processing.
+        |
+        */
+
+        'job' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Job
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to monitor background job processing.
+        |
+        */
+
+        'redis' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Exceptions
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to report unhandled exceptions.
+        |
+        */
+
+        'unhandled_exceptions' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Http Client monitoring
+        |--------------------------------------------------------------------------
+        |
+        | Enable this if you'd like us to report the http requests done using the CF HTTP Client.
+        |
+        */
+
+        'http_client' => true,
+        'http_client_body' => true,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Hide sensible data from http requests
+        |--------------------------------------------------------------------------
+        |
+        | List request fields that you want mask from the http payload.
+        | You can specify nested fields using the dot notation: "user.password"
+        */
+
+        'hidden_parameters' => [
+            'password',
+            'password_confirmation'
+        ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Artisan command to ignore
+        |--------------------------------------------------------------------------
+        |
+        | Add at this list all command signature that you don't want monitoring
+        | in your Inspector dashboard.
+        |
+        */
+
+        'ignore_commands' => [
+            'storage:link',
+            'optimize',
+            'optimize:clear',
+            'schedule:run',
+            'schedule:finish',
+            'package:discover',
+            'vendor:publish',
+            'list',
+            'test',
+            'migrate',
+            'migrate:rollback',
+            'migrate:refresh',
+            'migrate:fresh',
+            'migrate:reset',
+            'migrate:install',
+            'cache:clear',
+            'config:cache',
+            'config:clear',
+            'route:cache',
+            'route:clear',
+            'view:cache',
+            'view:clear',
+            'queue:listen',
+            'queue:work',
+            'queue:restart',
+            'vapor:work',
+            'horizon',
+            'horizon:work',
+            'horizon:supervisor',
+            'horizon:terminate',
+            'horizon:snapshot',
+            'nova:publish',
+        ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Web request url to ignore
+        |--------------------------------------------------------------------------
+        |
+        | Add at this list the url schemes that you don't want monitoring
+        | in your Inspector dashboard. You can also use wildcard expression (*).
+        |
+        */
+
+        'ignore_url' => [
+            'telescope*',
+            'vendor/telescope*',
+            'horizon*',
+            'vendor/horizon*',
+            'nova*'
+        ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Job classes to ignore
+        |--------------------------------------------------------------------------
+        |
+        | Add at this list the job classes that you don't want monitoring
+        | in your Inspector dashboard.
+        |
+        */
+
+        'ignore_jobs' => [
+            //\App\Jobs\MyJob::class
+        ],
+    ]
 ];
---
+++ system/libraries/CDevCloud/Inspector/Middleware/WebRequestMonitoring.php
@@ -0,0 +1,110 @@
+<?php
+
+use Symfony\Component\HttpKernel\TerminableInterface;
+
+/**
+ * @see CDevCloud
+ */
+class CDevCloud_Inspector_Middleware_WebRequestMonitoring implements TerminableInterface {
+    /**
+     * Handle an incoming request.
+     *
+     * @param CHTTP_Request $request
+     * @param \Closure      $next
+     *
+     * @throws \Exception
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        if (CDevCloud::inspector()->needTransaction()
+            && CDevCloud_Inspector_Filters::isApprovedRequest(CF::config('devcloud.inspector.ignore_url'), $request)
+            && $this->shouldRecorded($request)
+        ) {
+            $this->startTransaction($request);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Determine if Inspector should monitor current request.
+     *
+     * @param \CHTTP_Request $request
+     *
+     * @return bool
+     */
+    protected function shouldRecorded($request): bool {
+        return true;
+    }
+
+    /**
+     * Start a transaction for the incoming request.
+     *
+     * @param \CHTTP_Request $request
+     */
+    protected function startTransaction($request) {
+        $transaction = CDevCloud::inspector()->startTransaction(
+            $this->buildTransactionName($request)
+        );
+        if (c::auth()->check() && CF::config('devcloud.inspector.user')) {
+            $transaction->withUser(c::auth()->user()->getAuthIdentifier());
+        }
+    }
+
+    /**
+     * Terminates a request/response cycle.
+     *
+     * @param \CHTTP_Request  $request
+     * @param \CHTTP_Response $response
+     */
+    public function terminate($request, $response) {
+
+        if (CDevCloud::inspector()->isRecording() && CDevCloud::inspector()->hasTransaction()) {
+
+            CDevCloud::inspector()->currentTransaction()
+                ->addContext('Request Body', CDevCloud_Inspector_Filters::hideParameters(
+                    $request->request->all(),
+                    CF::config('devcloud.inspector.hidden_parameters')
+                ))
+                ->addContext('Response', [
+                    'status_code' => $response->getStatusCode(),
+                    'version' => $response->getProtocolVersion(),
+                    'charset' => $response->getCharset(),
+                    'headers' => $response->headers->all(),
+                ])
+                ->setResult($response->getStatusCode());
+        }
+    }
+
+    /**
+     * Generate readable name.
+     *
+     * @param \CHTTP_Request $request
+     *
+     * @return string
+     */
+    protected function buildTransactionName($request) {
+        $route = $request->route();
+
+        if ($route instanceof CRouting_Route) {
+            $uri = $request->route()->uri();
+        } else {
+            $array = explode('?', $_SERVER['REQUEST_URI']);
+            $uri = array_shift($array);
+        }
+
+        return $request->method() . ' ' . $this->normalizeUri($uri);
+    }
+
+    /**
+     * Normalize URI string.
+     *
+     * @param $uri
+     *
+     * @return string
+     */
+    protected function normalizeUri($uri) {
+        return '/' . trim($uri, '/');
+    }
+}
