128097102a2e000db122efb86f0891ddb1a250ba
hery <hery@ittron.co.id>
2022-12-19 16:21:56 +0700

    update charts image

--- application/cresenity/default/controllers/demo/image/chart/line.php
+++ application/cresenity/default/controllers/demo/image/chart/line.php
@@ -8,9 +8,10 @@ class Controller_Demo_Image_Chart_Line extends \Cresenity\Demo\Controller {
         $lineChart = CChart::lineChart();
         $lineChart->addSeries([100, 200, 400, 500, 300, 600], 'Item 1');
         $lineChart->addSeries([400, 500, 800, 300, 500, 100], 'Item 2');
+        $lineChart->addSeries([600, 400, 1000, 100, 200, 400], 'Item 3');
 
         $lineChart->setDataLabels(['A', 'B', 'C', 'D', 'E', 'F']);
-        $lineChart->setColors(['#ff0000', '#00ff00']);
+        $lineChart->setColors(['#ff0000', '#00ff00', '#0000ff']);
 
         $imageChart = CImage::chart(500, 200);
         $imageChart->setChart($lineChart);
