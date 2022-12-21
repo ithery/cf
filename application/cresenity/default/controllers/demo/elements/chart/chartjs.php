87fa4cd9b3e312a4c83d732d4eef4b70d46bcc28
Hery <hery@ittron.co.id>
2022-12-22 04:08:57 +0700

    update chart element

--- application/cresenity/default/controllers/demo/elements/chart/chartjs.php
+++ application/cresenity/default/controllers/demo/elements/chart/chartjs.php
@@ -7,7 +7,33 @@ class Controller_Demo_Elements_Chart_Chartjs extends \Cresenity\Demo\Controller 
         $app->setTitle('Chart JS');
 
         $app->addH5()->add('Line Chart');
-        $app->addDiv()->addChart();
+
+        $lineChart = CChart::lineChart();
+        $lineChart->addSeries([100, 200, 400, 500, 300, 600], 'Item 1');
+        $lineChart->addSeries([400, 500, 800, 300, 500, 100], 'Item 2');
+        $lineChart->addSeries([600, 400, 1000, 100, 200, 400], 'Item 3');
+
+        $lineChart->setDataLabels(['A', 'B', 'C', 'D', 'E', 'F']);
+        $lineChart->setColors(['#ff0000', '#00ff00', '#0000ff']);
+        $lineChart->setLegendPosition(CChart::POSITION_LEFT);
+        $app->addDiv()->addChart()->setChart($lineChart)->addClass('mb-3');
+        $app->addH5()->add('Bar Chart');
+        $barChart = CChart::barChart();
+        $barChart->addSeries([100, 200, 400, 500, 300, 600], 'Item 1');
+        $barChart->addSeries([400, 500, 800, 300, 500, 100], 'Item 2');
+        $barChart->setColors(['#ff0000', '#00ff00']);
+        $barChart->setDataLabels(['Bar1', 'Bar2', 'Bar3', 'Bar4', 'Bar5', 'Bar6']);
+        $barChart->setTitle('Vertical Bar');
+        $app->addDiv()->addChart()->setChart($barChart)->addClass('mb-3');
+
+        $app->addH5()->add('Pie Chart');
+
+        $pieChart = CChart::pieChart();
+        $pieChart->addSeries([500, 200, 300, 100]);
+        $pieChart->setDataLabels(['A', 'B', 'C', 'D']);
+        $pieChart->setColors(['#ff0000', '#00ff00', '#0000ff', '#ff00ff']);
+        $pieChart->setTitle('Regular Pie');
+        $app->addDiv()->addChart()->setChart($pieChart)->addClass('mb-3');
 
         return $app;
     }
--- system/libraries/CElement/Component/Chart/Engine/ChartJsEngine.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class CElement_Component_Chart_Engine_ChartJsEngine extends CElement_Component_Chart_EngineAbstract {
-    public function buildJs() {
-    }
-}
