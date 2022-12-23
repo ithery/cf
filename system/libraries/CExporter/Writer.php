eef5bfaf228b8fcda40ce8828c81514719bbbb53
hery <hery@ittron.co.id>
2022-12-23 23:45:55 +0700

    add Enum vendor libraries

--- system/libraries/CExporter/Writer.php
+++ system/libraries/CExporter/Writer.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 use PhpOffice\PhpSpreadsheet\Cell\Cell;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
