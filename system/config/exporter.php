5582c139f551d265c855457fc399a4244f9d7337
hery <hery@ittron.co.id>
2022-12-26 15:22:31 +0700

    fix default wkhtmltopdf path

--- system/config/exporter.php
+++ system/config/exporter.php
@@ -5,7 +5,7 @@ return [
 
         'pdf' => [
             'enabled' => true,
-            'binary' => c::env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf.sh'),
+            'binary' => c::env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
             'timeout' => false,
             'options' => [],
             'env' => [],
@@ -13,7 +13,7 @@ return [
 
         'image' => [
             'enabled' => true,
-            'binary' => c::env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage.sh'),
+            'binary' => c::env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage'),
             'timeout' => false,
             'options' => [],
             'env' => [],
