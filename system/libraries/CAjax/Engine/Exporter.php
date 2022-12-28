c51fdc3a6c6d03b94aba3a455516f9bc711242f9
hery <hery@ittron.co.id>
2022-12-28 21:34:53 +0700

    create new CAjax Exporter

---
+++ system/libraries/CAjax/Engine/Exporter.php
@@ -0,0 +1,14 @@
+<?php
+class CAjax_Engine_Exporter extends CAjax_Engine {
+    public function execute() {
+        $data = $this->ajaxMethod->getData();
+
+        $exporter = carr::get($data, 'exporter');
+        $filename = carr::get($data, 'filename');
+        $writerType = carr::get($data, 'writerType');
+        $headers = carr::get($data, 'headers', []);
+        $exporter = unserialize($exporter);
+
+        return CExporter::toDownloadResponse($exporter, $filename, $writerType, $headers);
+    }
+}
