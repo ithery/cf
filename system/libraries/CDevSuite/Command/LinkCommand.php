784f6a9ab6a3c80b7f616e6b762f0b46c6f5d8c8
Hery <hery@ittron.co.id>
2023-02-01 21:53:59 +0700

    update devsuite

--- system/libraries/CDevSuite/Command/LinkCommand.php
+++ system/libraries/CDevSuite/Command/LinkCommand.php
@@ -7,18 +7,18 @@
  */
 class CDevSuite_Command_LinkCommand extends CDevSuite_CommandAbstract {
     public function getSignatureArguments() {
-        return '{name} {--secure}';
+        return '{name?} {--secure}';
     }
 
     public function run(CConsole_Command $cfCommand) {
-        $name = $cfCommand->argument('name');
+        $name = $cfCommand->argument('name') ?: CF::appCode();
         $secure = $cfCommand->option('secure');
         $linkPath = CDevSuite::site()->link(DOCROOT, $name = $name ?: basename(DOCROOT));
 
         CDevSuite::info('A [' . $name . '] symbolic link has been created in [' . $linkPath . '].');
 
         if ($secure) {
-            $cfCommand->call('devsuite::secure ' . $name);
+            $cfCommand->call('devsuite:secure ' . $name);
         }
     }
 }
