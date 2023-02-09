24e3fcc6e6867a5fd4c42a92178517e3b303c814
Hery <hery@ittron.co.id>
2023-02-09 16:57:12 +0700

    add bootstrap 5 for paginator

--- system/libraries/CPagination.php
+++ system/libraries/CPagination.php
@@ -4,4 +4,48 @@
  * @see CPagination_Paginator
  */
 class CPagination {
+    /**
+     * Indicate that Tailwind styling should be used for generated links.
+     *
+     * @return void
+     */
+    public static function useTailwind() {
+        CPagination_Paginator::useTailwind();
+    }
+
+    /**
+     * Indicate that Bootstrap 4 styling should be used for generated links.
+     *
+     * @return void
+     */
+    public static function useBootstrap() {
+        CPagination_Paginator::useBootstrap();
+    }
+
+    /**
+     * Indicate that Bootstrap 3 styling should be used for generated links.
+     *
+     * @return void
+     */
+    public static function useBootstrapThree() {
+        CPagination_Paginator::useBootstrapThree();
+    }
+
+    /**
+     * Indicate that Bootstrap 4 styling should be used for generated links.
+     *
+     * @return void
+     */
+    public static function useBootstrapFour() {
+        CPagination_Paginator::useBootstrapFour();
+    }
+
+    /**
+     * Indicate that Bootstrap 5 styling should be used for generated links.
+     *
+     * @return void
+     */
+    public static function useBootstrapFive() {
+        CPagination_Paginator::useBootstrapFive();
+    }
 }
