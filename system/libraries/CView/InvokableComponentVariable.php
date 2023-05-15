2f5cfba75f3d5037709d39e33ff9612e588ad33a
Hery <hery@ittron.co.id>
2023-05-15 16:34:50 +0700

    surpress ide error

--- system/libraries/CView/InvokableComponentVariable.php
+++ system/libraries/CView/InvokableComponentVariable.php
@@ -33,7 +33,7 @@ class CView_InvokableComponentVariable implements CBase_DeferringDisplayableValu
      *
      * @return \ArrayIterator
      */
-    #[ReturnTypeWillChange]
+    #[\ReturnTypeWillChange]
     public function getIterator() {
         $result = $this->__invoke();
 
