ec5deab9d75f2d6d30fbbe19802e9d2d3c548e8e
Hery <hery@ittron.co.id>
2023-05-26 11:47:21 +0700

    fix c

--- system/helpers/c.php
+++ system/helpers/c.php
@@ -436,7 +436,7 @@ class c {
             return $value->toHtml();
         }
 
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', $doubleEncode);
+        return htmlspecialchars($value ?: '', ENT_QUOTES, 'UTF-8', $doubleEncode);
     }
 
     /**
