f90d277803475a9c56481bf0da23ee713d48f666
Hery <hery@ittron.co.id>
2023-06-09 13:08:41 +0700

    Move Analytics to systems

--- system/libraries/CDevSuite/Db.php
+++ system/libraries/CDevSuite/Db.php
@@ -137,19 +137,22 @@ class CDevSuite_Db {
 
     /**
      * @param mixed $key
+     *
+     * @return CDatabase_Connection
      */
     public function getDatabase($key) {
         $config = $this->toDbConfig($key);
         $host = carr::get($config, 'connection.host');
         $database = carr::get($config, 'connection.database');
+        CDatabase::manager()->addConnection($config, $host . '-' . $database);
 
-        return CDatabase::instance($host . '-' . $database, $config, CF::domain());
+        return c::db($host . '-' . $database);
     }
 
     public function isCanConnect($key) {
         try {
             $db = $this->getDatabase($key);
-            $db->connect();
+            $db->getPdo();
         } catch (Exception $ex) {
             $errMessage = $ex->getMessage();
             CDevSuite::info($ex->getMessage());
@@ -163,9 +166,6 @@ class CDevSuite_Db {
     public function compare($from, $to) {
         $fromDB = $this->getDatabase($from);
         $toDB = $this->getDatabase($to);
-
-        $fromDB->connect();
-        $toDB->connect();
 
         $fromSchemaManager = $fromDB->getSchemaManager();
         $toSchemaManager = $toDB->getSchemaManager();
@@ -193,9 +193,6 @@ class CDevSuite_Db {
     public function sync($from, $to) {
         $fromDB = $this->getDatabase($from);
         $toDB = $this->getDatabase($to);
-
-        $fromDB->connect();
-        $toDB->connect();
 
         $fromSchemaManager = $fromDB->getSchemaManager();
         $toSchemaManager = $toDB->getSchemaManager();
