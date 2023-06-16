53bc21f3742a3cb6f158509c12b8fe5156065c75
Hery <hery@ittron.co.id>
2023-06-16 23:07:49 +0700

    add vendor Cresenity

--- system/vendor/Cresenity/Base/Contract/Jsonable.php
+++ system/vendor/Cresenity/Base/Contract/Jsonable.php
@@ -1,19 +1,13 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Cresenity\Base\Contract;
 
 interface Jsonable {
-
     /**
      * Convert the object to its JSON representation.
      *
-     * @param  int  $options
+     * @param int $options
+     *
      * @return string
      */
     public function toJson($options = 0);
