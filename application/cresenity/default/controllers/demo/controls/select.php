052df09d801bdbb2396004c02fcba9083bf418fc
Hery <hery@ittron.co.id>
2023-07-27 20:37:52 +0700

    update demo

--- application/cresenity/default/controllers/demo/controls/select.php
+++ application/cresenity/default/controllers/demo/controls/select.php
@@ -15,6 +15,39 @@ class Controller_Demo_Controls_Select extends \Cresenity\Demo\Controller {
         $div->addH5()->add('Default Select');
         $div->addSelectControl()->setList($selectOptions);
 
+        $div->addH5()->add('Nested Select');
+        $selectParentOptions = [
+            '1' => 'One',
+            '2' => 'Two',
+            '3' => 'Three'
+        ];
+        $selectChildOptions = [
+            '1' => [
+                'a' => 'A',
+                'b' => 'B',
+                'c' => 'C',
+            ],
+            '2' => [
+                'd' => 'D',
+                'e' => 'E',
+            ],
+            '3' => [
+                'f' => 'F',
+                'g' => 'G',
+                'h' => 'H',
+                'i' => 'I',
+            ],
+        ];
+        $parentSelect = $div->addSelectControl()->setName('parent_select')->setList($selectOptions);
+        $childDiv = $div->addDiv();
+        $childDiv->setDependsOn($parentSelect, function ($value) use ($selectChildOptions) {
+            $options = carr::get($selectChildOptions, $value, []);
+            $div = c::div();
+            $div->addSelectControl()->setName('child_select')->setList($options);
+
+            return $div;
+        }, ['block' => false]);
+
         return $app;
     }
 }
