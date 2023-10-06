4b93a5af78f9f64efbd9592abf2c13763f700ccb
hery <hery@ittron.co.id>
2023-10-06 20:48:30 +0700

    update ml config

--- system/config/ml.php
+++ system/config/ml.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'csv_path_output' => DOCROOT. 'temp/ml/'.CF::appCode().'/csv/output/',
-    'csv_path_input' => DOCROOT. 'temp/ml/'.CF::appCode().'/csv/input/',
-    'ai_model_path_output' => __DIR__ . '/../data/ai_model/',
+    'csv_path_output' => DOCROOT. 'temp/ml/' . CF::appCode() . '/csv/output/',
+    'csv_path_input' => DOCROOT. 'temp/ml/' . CF::appCode() . '/csv/input/',
+    'ai_model_path_output' => DOCROOT. 'temp/ml/' . CF::appCode() . '/model/',
     'RubixMainClass' => RubixService::class,
 ];
