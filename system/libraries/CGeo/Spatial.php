bda22bc9ae420b09059424b0eceb13c1d091fb55
hery <hery@ittron.co.id>
2023-10-08 19:12:33 +0700

    update geo spatial

--- system/libraries/CGeo/Spatial.php
+++ system/libraries/CGeo/Spatial.php
@@ -3,6 +3,22 @@
  * @see https://github.com/grimzy/laravel-mysql-spatial/
  */
 class CGeo_Spatial {
+    public static function point($geojson) {
+        return CGeo_Spatial_Type_Point::fromJson($geojson);
+    }
+
+    public static function multiPoint($geojson) {
+        return CGeo_Spatial_Type_MultiPoint::fromJson($geojson);
+    }
+
+    public static function lineString($geojson) {
+        return CGeo_Spatial_Type_LineString::fromJson($geojson);
+    }
+
+    public static function multiLineString($geojson) {
+        return CGeo_Spatial_Type_MultiLineString::fromJson($geojson);
+    }
+
     public static function polygon($geojson) {
         return CGeo_Spatial_Type_Polygon::fromJson($geojson);
     }
