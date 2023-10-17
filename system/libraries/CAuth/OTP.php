142b446726c86788d46873d0125f25b2d0a9b534
Hery <hery@ittron.co.id>
2023-10-18 04:50:16 +0700

    add auth otp

--- system/libraries/CAuth/OTP.php
+++ system/libraries/CAuth/OTP.php
@@ -3,6 +3,13 @@
  * @see https://github.com/Spomky-Labs/otphp/
  */
 class CAuth_OTP {
+    public static function otp($secret = null, $type = 'totp') {
+        if ($type == 'totp') {
+            return self::totp($secret);
+        }
+        self::hotp($secret);
+    }
+
     public static function hotp($secret = null) {
         if ($secret == null) {
             return CAuth_OTP_HOTP::generate();
