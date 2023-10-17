27eda7fe74918e7f29f82e387a8492e3e1da22ed
Hery <hery@ittron.co.id>
2023-10-18 03:57:10 +0700

    update xendit

--- system/libraries/CVendor/Xendit/ApiOperation/RetrieveAll.php
+++ system/libraries/CVendor/Xendit/ApiOperation/RetrieveAll.php
@@ -2,12 +2,15 @@
 
 trait CVendor_Xendit_ApiOperation_RetrieveAll {
     /**
-     * Send request to get all object, e.g Invoice
+     * Send request to get all object, e.g Invoice.
+     *
+     * @param array $options
      *
      * @return array
      */
-    public function retrieveAll() {
+    public function retrieveAll($options = []) {
         $url = $this->classUrl();
-        return $this->request('GET', $url, []);
+
+        return $this->request('GET', $url, $options);
     }
 }
