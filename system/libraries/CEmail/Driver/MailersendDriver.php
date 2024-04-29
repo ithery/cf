14d71761b5740d8a7a93c6e5f8e5cadf82523c7f
albert <albert@ittron.co.id>
2024-04-29 15:32:12 +0700

    ms driver

--- system/libraries/CEmail/Driver/MailersendDriver.php
+++ system/libraries/CEmail/Driver/MailersendDriver.php
@@ -7,58 +7,25 @@ class CEmail_Driver_MailersendDriver extends CEmail_DriverAbstract {
         $from = carr::get($options, 'from', $this->config->getFrom());
         $fromName = carr::get($options, 'from_name', $this->config->getFromName());
 
-        $domain = carr::get($options, 'domain', carr::get($options, 'smtp_domain'));
+        $mailersend = new CVendor_MailerSend(['api_key'=>$apiKey]);
+        $bulkEmailParams = [];
 
-        $url = 'https://api.mailersend.com/v1/email';
-
-        $files = [];
-        $paramTo = [];
         foreach ($to as $t) {
-            $paramTo[]=[
-                'email'=>$t,
-            ];
+            $bulkEmailParams[] = (new CVendor_MailerSend_Helpers_Builder_EmailParams())
+                ->setFrom($from)
+                ->setFromName($fromName)
+                ->setRecipients([
+                    new CVendor_MailerSend_Helpers_Builder_Recipient($t, ''),
+                ])
+                ->setSubject($subject)
+                ->setHtml($body);
         }
-        $params = [
-            'to' => $paramTo,
-            'from' => [
-                'email'=>$from,
-            ],
-            'subject' => $subject . '',
-            'html' => $body,
-        ];
+        $response = $mailersend->bulkEmail->send($bulkEmailParams);
+        cdbg::dd($response);
+        // if ($response->statusCode() > 400) {
+        //     throw new Exception('Fail to send mail, API Response:(' . $response->statusCode() . ')' . $response->body());
+        // }
 
-        $cc = carr::get($options, 'cc');
-        $bcc = carr::get($options, 'bcc');
-        if (is_array($cc) && count($cc) > 0) {
-            $params['cc'] = $cc;
-        }
-        if (is_array($cc) && count($bcc) > 0) {
-            $params['bcc'] = $bcc;
-        }
-
-        // Generate curl request
-        $paramsJson = json_encode($params);
-        $session = curl_init($url);
-        curl_setopt($session, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($session, CURLOPT_POSTFIELDS, json_encode($paramsJson));
-        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($session, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json',
-            'X-Requested-With: XMLHttpRequest',
-            'Authorization: Bearer '.$apiKey
-        ]);
-        // obtain response
-        $response = curl_exec($session);
-        // cdbg::varDump($paramsJson);
-        // cdbg::varDump($response);
-
-        curl_close($session);
-
-        $responseArray = json_decode($response, true);
-        if (strlen(carr::get($responseArray, 'id')) == 0) {
-            throw new Exception('Fail to send mail, API Response:' . $response);
-        }
-
-        return $responseArray;
+        return $response;
     }
 }
