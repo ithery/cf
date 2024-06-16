0e39373825256533d063c9b0bd7224d05048099d
Hery <hery@ittron.co.id>
2024-06-16 19:41:31 +0700

    update phpseclib3

--- system/vendor/phpseclib3/Crypt/Random.php
+++ system/vendor/phpseclib3/Crypt/Random.php
@@ -14,8 +14,6 @@
  * ?>
  * </code>
  *
- * @category  Crypt
- *
  * @author    Jim Wigginton <terrafrost@php.net>
  * @copyright 2007 Jim Wigginton
  * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
@@ -23,7 +21,11 @@
  * @link      http://phpseclib.sourceforge.net
  */
 
+declare(strict_types=1);
+
 namespace phpseclib3\Crypt;
+
+use phpseclib3\Exception\RuntimeException;
 
 /**
  * Pure-PHP Random Number Generator.
@@ -38,13 +40,9 @@ abstract class Random {
      * microoptimizations because this function has the potential of being called a huge number of times.
      * eg. for RSA key generation.
      *
-     * @param int $length
-     *
-     * @throws \RuntimeException if a symmetric cipher is needed but not loaded
-     *
-     * @return string
+     * @throws RuntimeException if a symmetric cipher is needed but not loaded
      */
-    public static function string($length) {
+    public static function string(int $length): string {
         if (!$length) {
             return '';
         }
@@ -86,13 +84,13 @@ abstract class Random {
             $old_session_id = session_id();
             $old_use_cookies = ini_get('session.use_cookies');
             $old_session_cache_limiter = session_cache_limiter();
-            $_OLD_SESSION = isset($_SESSION) ? $_SESSION : false;
+            $_OLD_SESSION = $_SESSION ?? false;
             if ($old_session_id != '') {
                 session_write_close();
             }
 
-            session_id(1);
-            ini_set('session.use_cookies', 0);
+            session_id('1');
+            ini_set('session.use_cookies', '0');
             session_cache_limiter('');
             session_start();
 
@@ -100,7 +98,10 @@ abstract class Random {
                  . (isset($_POST) ? self::safe_serialize($_POST) : '')
                  . (isset($_GET) ? self::safe_serialize($_GET) : '')
                  . (isset($_COOKIE) ? self::safe_serialize($_COOKIE) : '')
-                 . self::safe_serialize($GLOBALS)
+                 // as of PHP 8.1 $GLOBALS can't be accessed by reference, which eliminates
+                 // the need for phpseclib_safe_serialize. see https://wiki.php.net/rfc/restrict_globals_usage
+                 // for more info
+                 . serialize($GLOBALS)
                  . self::safe_serialize($_SESSION)
                  . self::safe_serialize($_OLD_SESSION);
             $v = $seed = $_SESSION['seed'] = sha1($v, true);
@@ -166,7 +167,7 @@ abstract class Random {
 
                     break;
                 default:
-                    throw new \RuntimeException(__CLASS__ . ' requires at least one symmetric cipher be loaded');
+                    throw new RuntimeException(__CLASS__ . ' requires at least one symmetric cipher be loaded');
             }
 
             $crypto->setKey(substr($key, 0, $crypto->getKeyLength() >> 3));
@@ -201,10 +202,8 @@ abstract class Random {
      * If a class has a private __sleep() it'll emit a warning
      *
      * @param mixed $arr
-     *
-     * @return mixed
      */
-    private static function safe_serialize(&$arr) {
+    private static function safe_serialize(&$arr): string {
         if (is_object($arr)) {
             return '';
         }
