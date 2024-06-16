a3974eef233a09509b78090a3f7284cdf2fb4b34
Hery <hery@ittron.co.id>
2024-06-16 19:30:30 +0700

    reformat

--- system/vendor/phpseclib3/Crypt/Common/Traits/Fingerprint.php
+++ system/vendor/phpseclib3/Crypt/Common/Traits/Fingerprint.php
@@ -1,15 +1,16 @@
 <?php
 
 /**
- * Fingerprint Trait for Public Keys
+ * Fingerprint Trait for Public Keys.
  *
  * PHP version 5
  *
  * @category  Crypt
- * @package   Common
+ *
  * @author    Jim Wigginton <terrafrost@php.net>
  * @copyright 2015 Jim Wigginton
  * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ *
  * @link      http://phpseclib.sourceforge.net
  */
 
@@ -18,28 +19,24 @@ namespace phpseclib3\Crypt\Common\Traits;
 use phpseclib3\Crypt\Hash;
 
 /**
- * Fingerprint Trait for Private Keys
+ * Fingerprint Trait for Private Keys.
  *
- * @package Common
  * @author  Jim Wigginton <terrafrost@php.net>
- * @access  public
  */
-trait Fingerprint
-{
+trait Fingerprint {
     /**
-     * Returns the public key's fingerprint
+     * Returns the public key's fingerprint.
      *
      * The public key's fingerprint is returned, which is equivalent to running `ssh-keygen -lf rsa.pub`. If there is
      * no public key currently loaded, false is returned.
      * Example output (md5): "c1:b1:30:29:d7:b8:de:6c:97:77:10:d7:46:41:63:87" (as specified by RFC 4716)
      *
-     * @access public
      * @param string $algorithm The hashing algorithm to be used. Valid options are 'md5' and 'sha256'. False is returned
-     * for invalid values.
+     *                          for invalid values.
+     *
      * @return mixed
      */
-    public function getFingerprint($algorithm = 'md5')
-    {
+    public function getFingerprint($algorithm = 'md5') {
         $type = self::validatePlugin('Keys', 'OpenSSH', 'savePublicKey');
         if ($type === false) {
             return false;
@@ -52,6 +49,7 @@ trait Fingerprint
             case 'sha256':
                 $hash = new Hash('sha256');
                 $base = base64_encode($hash->hash($key));
+
                 return substr($base, 0, strlen($base) - 1);
             case 'md5':
                 return substr(chunk_split(md5($key), 2, ':'), 0, -1);
