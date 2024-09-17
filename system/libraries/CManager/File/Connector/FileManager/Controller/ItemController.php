6e6b3d8ffa0abc1eeaeb1b4de6f5fd2d453b4880
albert <albert@ittron.co.id>
2024-09-17 18:03:01 +0700

    repair file manager

--- system/libraries/CManager/File/Connector/FileManager/Controller/ItemController.php
+++ system/libraries/CManager/File/Connector/FileManager/Controller/ItemController.php
@@ -8,6 +8,7 @@ defined('SYSPATH') or die('No direct access allowed.');
  *
  * @since Aug 11, 2019, 3:16:48 AM
  */
+use League\Flysystem\UnableToRetrieveMetadata;
 use CManager_File_Connector_FileManager_FM as FM;
 
 class CManager_File_Connector_FileManager_Controller_ItemController extends CManager_File_Connector_FileManager_AbstractController {
@@ -18,14 +19,19 @@ class CManager_File_Connector_FileManager_Controller_ItemController extends CMan
      */
     public function execute() {
         $fm = $this->fm();
+        $data = [];
 
-        $data = [
-            'items' => array_map(function ($item) {
-                return $item->fill()->attributes;
-            }, array_merge($fm->path()->folders(), $fm->path()->files())),
-            'display' => $fm->getDisplayMode(),
-            'working_dir' => $fm->path()->path('working_dir'),
-        ];
+        try {
+            $data = [
+                'items' => array_map(function ($item) {
+                    return $item->fill()->attributes;
+                }, array_merge($fm->path()->folders(), $fm->path()->files())),
+                'display' => $fm->getDisplayMode(),
+                'working_dir' => $fm->path()->path('working_dir'),
+            ];
+        } catch (UnableToRetrieveMetadata $e) {
+            return c::abort(404);
+        }
 
         return c::response()->json($data);
     }
