348e457927d027a79f5920287188e31ce9127c2a
Hery Kurniawan <hery.cresenity@gmail.com>
2025-02-11 14:38:41 +0700

    update editorjs image withBackground

--- system/views/cresenity/element/editorjs/image.blade.php
+++ system/views/cresenity/element/editorjs/image.blade.php
@@ -1,6 +1,9 @@
 <div class="cres-editor-js-block">
     <div class="cres-editor-js-image {{ $classes }} {{ isset($alignment) ? 'text-'.$alignment : 'text-left' }}">
-        <img src="{{ $file['url'] }}" alt="{{ $caption }}">
+        <div class="cres-editor-js-image-tool">
+            <img src="{{ $file['url'] }}" alt="{{ $caption }}">
+
+        </div>
         @if (!empty($caption))
             <div class="cres-editor-js-image-caption">{{ $caption }}</div>
         @endif
