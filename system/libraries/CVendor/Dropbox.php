21a236c033c72593ca466c4edb1cb6a4cc6f94aa
Hery <hery@ittron.co.id>
2025-05-04 01:03:28 +0700

    update

--- system/libraries/CVendor/Dropbox.php
+++ system/libraries/CVendor/Dropbox.php
@@ -1,4 +1,11 @@
 <?php
 
 class CVendor_Dropbox {
+    public function connect($options = []) {
+        $clientId = carr::get($options, 'clientId', CF::config('vendor.dropbox.client_id'));
+        $clientSecret = carr::get($options, 'clientSecret', CF::config('vendor.dropbox.client_secret'));
+        $client = new CVendor_Dropbox_Client($clientId, $clientSecret, $options);
+
+        return $client->connect();
+    }
 }
