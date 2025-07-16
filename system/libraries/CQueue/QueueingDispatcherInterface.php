40b2f01eab53a4be072ca763da04eb3c006db3ea
Hery <hery@ittron.co.id>
2025-07-16 21:43:49 +0700

    update queue interface

--- system/libraries/CQueue/QueueingDispatcherInterface.php
+++ system/libraries/CQueue/QueueingDispatcherInterface.php
@@ -4,6 +4,24 @@ defined('SYSPATH') or die('No direct access allowed.');
 
 interface CQueue_QueueingDispatcherInterface extends CQueue_DispatcherInterface {
     /**
+     * Attempt to find the batch with the given ID.
+     *
+     * @param string $batchId
+     *
+     * @return null|\CQueue_Batch
+     */
+    public function findBatch(string $batchId);
+
+    /**
+     * Create a new batch of queueable jobs.
+     *
+     * @param \CCollection|array $jobs
+     *
+     * @return \CQueue_PendingBatch
+     */
+    public function batch($jobs);
+
+    /**
      * Dispatch a command to its appropriate handler behind a queue.
      *
      * @param mixed $command
