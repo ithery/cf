98744b1e7476953176057207b82907dd069869fb
Hery <hery@ittron.co.id>
2025-09-08 11:03:12 +0700

    fix final class on CGeo

--- system/libraries/CGeo/Exception/InvalidArgument.php
+++ system/libraries/CGeo/Exception/InvalidArgument.php
@@ -2,5 +2,5 @@
 
 defined('SYSPATH') or die('No direct access allowed.');
 
-final class CGeo_Exception_InvalidArgument extends InvalidArgumentException implements CGeo_Interface_ExceptionInterface {
+class CGeo_Exception_InvalidArgument extends InvalidArgumentException implements CGeo_Interface_ExceptionInterface {
 }
