cb7c0da78ce22bc65438ced433c0aa4a9b5d1491
Hery <hery@ittron.co.id>
2025-12-05 19:30:26 +0700

    update phpdoc

--- system/libraries/CModel/Spatial/Geometry/Point.php
+++ system/libraries/CModel/Spatial/Geometry/Point.php
@@ -8,6 +8,13 @@ class CModel_Spatial_Geometry_Point extends CModel_Spatial_GeometryAbstract {
 
     protected $lng;
 
+    /**
+     * Construct a new Point object
+     *
+     * @param float $lat latitude value
+     * @param float $lng longitude value
+     * @param int $srid srid value (optional, defaults to 0)
+     */
     public function __construct($lat, $lng, $srid = 0) {
         parent::__construct($srid);
 
@@ -15,40 +22,94 @@ class CModel_Spatial_Geometry_Point extends CModel_Spatial_GeometryAbstract {
         $this->lng = (float) $lng;
     }
 
+    /**
+     * Returns the latitude value of the point
+     *
+     * @return float The latitude value
+     */
     public function getLat() {
         return $this->lat;
     }
 
+    /**
+     * Set the latitude value of the point
+     *
+     * @param float $lat The new latitude value
+     */
     public function setLat($lat) {
         $this->lat = (float) $lat;
     }
 
+
+    /**
+     * Returns the longitude value of the point
+     *
+     * @return float The longitude value
+     */
     public function getLng() {
         return $this->lng;
     }
 
+
+    /**
+     * Set the longitude value of the point
+     *
+     * @param float $lng The new longitude value
+     */
     public function setLng($lng) {
         $this->lng = (float) $lng;
     }
 
+    /**
+     * Returns a string representation of the point as a pair of longitude and latitude
+     * values separated by a space.
+     *
+     * @return string The string representation of the point
+     */
     public function toPair() {
         return $this->getLng() . ' ' . $this->getLat();
     }
 
+    /**
+     * Create a new Point object from a pair of longitude and latitude values
+     * separated by a space.
+     *
+     * @param string $pair The pair of longitude and latitude values
+     * @param int    $srid The SRID value (default is 0)
+     * @return static A new Point object
+     */
     public static function fromPair($pair, $srid = 0) {
         list($lng, $lat) = explode(' ', trim($pair, "\t\n\r \x0B()"));
 
         return new static((float) $lat, (float) $lng, (int) $srid);
     }
 
+    /**
+     * Returns a Well-Known Text (WKT) representation of the point.
+     *
+     * @return string The WKT representation of the point
+     */
     public function toWKT() {
         return sprintf('POINT(%s)', (string) $this);
     }
 
+    /**
+     * Create a new Point object from a Well-Known Text (WKT) representation of the point
+     *
+     * @param string $wktArgument The WKT representation of the point
+     * @param int    $srid The SRID value (default is 0)
+     * @return static A new Point object
+     */
     public static function fromString($wktArgument, $srid = 0) {
         return static::fromPair($wktArgument, $srid);
     }
 
+    /**
+     * Returns a string representation of the point as a pair of longitude and latitude
+     * values separated by a space.
+     *
+     * @return string The string representation of the point
+     */
     public function __toString() {
         return $this->getLng() . ' ' . $this->getLat();
     }
