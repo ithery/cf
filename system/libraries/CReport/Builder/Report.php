54f3934150721e3e3455b527ecba8c6481af3434
Hery <hery@ittron.co.id>
2026-03-13 17:56:52 +0700

    debug

--- system/libraries/CReport/Builder/Report.php
+++ system/libraries/CReport/Builder/Report.php
@@ -63,7 +63,6 @@ class CReport_Builder_Report implements CReport_Builder_Contract_JrXmlElementInt
         }
         foreach ($xml as $tag => $xmlElement) {
             if ($tag == 'defaultFont') {
-                cdbg::d($xmlElement);
                 $report->setDefaultFont(CReport_Builder_Object_Font::fromXml($xmlElement));
                 $report->setFont(CReport_Builder_Object_Font::fromXml($xmlElement));
             }
@@ -99,6 +98,7 @@ class CReport_Builder_Report implements CReport_Builder_Contract_JrXmlElementInt
      */
     public function setDefaultFont(CReport_Builder_Object_Font $font) {
         $this->defaultFont = $font;
+        cdbg::d($this->defaultFont);
 
         return $this;
     }
