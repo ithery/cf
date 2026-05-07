da72e8420f10c1973c33247f0edbe922aea8b503
ari <ari.muntari1@gmail.com>
2026-05-07 14:40:07 +0700

    mailersend add attachment

--- system/libraries/CEmail/Driver/MailersendDriver.php
+++ system/libraries/CEmail/Driver/MailersendDriver.php
@@ -7,6 +7,7 @@ class CEmail_Driver_MailersendDriver extends CEmail_DriverAbstract {
         $errMessage = '';
         $from = carr::get($options, 'from', $this->config->getFrom());
         $fromName = carr::get($options, 'from_name', $this->config->getFromName());
+        $attachments = carr::get($options, 'attachments', []);
         $mailersend = new CVendor_MailerSend(['api_key' => $apiKey]);
         $bulkEmailParams = [];
 
@@ -20,16 +21,45 @@ class CEmail_Driver_MailersendDriver extends CEmail_DriverAbstract {
         }
         if (!$errCode) {
             foreach ($to as $t) {
-                if (filter_var($t, FILTER_VALIDATE_EMAIL)) {
-                    $bulkEmailParams[] = (new CVendor_MailerSend_Helpers_Builder_EmailParams())
-                        ->setFrom($from)
-                        ->setFromName($fromName)
-                        ->setRecipients([
-                            new CVendor_MailerSend_Helpers_Builder_Recipient($t, ''),
-                            ])
-                        ->setSubject($subject)
-                        ->setHtml($body);
+                if (!filter_var($t, FILTER_VALIDATE_EMAIL)) {
+                    continue;
                 }
+
+                $emailParams = (new CVendor_MailerSend_Helpers_Builder_EmailParams())
+                    ->setFrom($from)
+                    ->setFromName($fromName)
+                    ->setRecipients([
+                        new CVendor_MailerSend_Helpers_Builder_Recipient($t, '')
+                    ])
+                    ->setSubject($subject)
+                    ->setHtml($body);
+
+                // Attachments
+                if (!empty($attachments)) {
+                    $mailAttachments = [];
+
+                    foreach ($attachments as $attachment) {
+                        $filePath = carr::get($attachment, 'path');
+
+                        if (empty($filePath) || !file_exists($filePath)) {
+                            continue;
+                        }
+                        $fileName = carr::get($attachment, 'filename', basename($filePath));
+                        $fileType = carr::get($attachment, 'type', mime_content_type($filePath));
+
+                        $mailAttachments[] = ['content' => base64_encode(file_get_contents($filePath)),
+                            'filename' => $fileName,
+                            'disposition' => 'attachment',
+                            'type' => $fileType,
+                        ];
+                    }
+
+                    if (!empty($mailAttachments)) {
+                        $emailParams->setAttachments($mailAttachments);
+                    }
+                }
+
+                $bulkEmailParams[] = $emailParams;
             }
         }
         if (!$errCode && !$bulkEmailParams) {
@@ -46,8 +76,8 @@ class CEmail_Driver_MailersendDriver extends CEmail_DriverAbstract {
             }
         } else {
             $response = [
-                'errCode'=>$errCode,
-                'errMessage'=>$errMessage,
+                'errCode' => $errCode,
+                'errMessage' => $errMessage,
             ];
         }
 
