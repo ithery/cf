688025d9458242ffca1f01828f7da4b946f419e7
Hery <hery@ittron.co.id>
2026-06-24 18:55:02 +0700

    add changelog

--- application/cresenity/default/navs/docs/change.php
+++ application/cresenity/default/navs/docs/change.php
@@ -4,22 +4,22 @@ return [
     [
         'name' => 'change.1-1',
         'label' => c::__('CF 1.1'),
-        'uri' => 'docs/change/1-5',
+        'uri' => 'docs/change/1-1',
     ],
     [
         'name' => 'change.1-2',
-        'label' => c::__('CF 1.1'),
-        'uri' => 'docs/change/1-5',
+        'label' => c::__('CF 1.2'),
+        'uri' => 'docs/change/1-2',
     ],
     [
         'name' => 'change.1-3',
-        'label' => c::__('CF 1.1'),
-        'uri' => 'docs/change/1-5',
+        'label' => c::__('CF 1.3'),
+        'uri' => 'docs/change/1-3',
     ],
     [
         'name' => 'change.1-4',
-        'label' => c::__('CF 1.1'),
-        'uri' => 'docs/change/1-5',
+        'label' => c::__('CF 1.4'),
+        'uri' => 'docs/change/1-4',
     ],
     [
         'name' => 'change.1-5',
