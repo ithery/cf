beadefa0f6a461df9891dd68e3c418dc91376f47
Hery <hery@ittron.co.id>
2026-06-28 15:10:22 +0700

    refactor CRemote_SSH: use CRemote_SSH_Config internally
    Array input is cast to CRemote_SSH_Config in constructor.
    Removed getAuth method, delegated to Config::toAuthArray.

--- system/libraries/CRemote/SSH.php
+++ system/libraries/CRemote/SSH.php
@@ -9,8 +9,14 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 class CRemote_SSH {
+    /**
+     * @var string
+     */
     protected $name;
 
+    /**
+     * @var CRemote_SSH_Config
+     */
     protected $config;
 
     /**
@@ -22,12 +28,12 @@ class CRemote_SSH {
      * @param array|CRemote_SSH_Config $config
      */
     public function __construct($config) {
-        if ($config instanceof CRemote_SSH_Config) {
-            $config = $config->toArray();
+        if (!$config instanceof CRemote_SSH_Config) {
+            $config = new CRemote_SSH_Config($config);
         }
         $this->config = $config;
 
-        $this->name = carr::get($config, 'name', carr::get($config, 'host'));
+        $this->name = $config->getHost();
         $this->connection = $this->makeConnection($this->name, $config);
     }
 
@@ -41,28 +47,20 @@ class CRemote_SSH {
     }
 
     /**
-     * Make a new connection instance.
-     *
-     * @param string $name
-     * @param array  $config
+     * @param string              $name
+     * @param CRemote_SSH_Config $config
      *
      * @return CRemote_SSH_Connection
      */
-    protected function makeConnection($name, array $config) {
-        $timeout = isset($config['timeout']) ? $config['timeout'] : 10;
-        $host = carr::get($config, 'ip_address');
-        if ($host == null) {
-            $host = carr::get($config, 'host');
-        }
-
+    protected function makeConnection($name, CRemote_SSH_Config $config) {
         $this->setOutput($connection = new CRemote_SSH_Connection(
             $name,
-            $host,
-            carr::get($config, 'port', 22),
-            $config['username'],
-            $this->getAuth($config),
+            $config->getConnectionHost(),
+            $config->getPort(),
+            $config->getUsername(),
+            $config->toAuthArray(),
             null,
-            $timeout
+            $config->getTimeout()
         ));
 
         return $connection;
@@ -81,28 +79,6 @@ class CRemote_SSH {
         $connection->setOutput($output);
     }
 
-    /**
-     * Format the appropriate authentication array payload.
-     *
-     * @param array $config
-     *
-     * @throws InvalidArgumentException
-     *
-     * @return array
-     */
-    protected function getAuth(array $config) {
-        if (isset($config['agent']) && $config['agent'] === true) {
-            return ['agent' => true];
-        } elseif (isset($config['key']) && trim($config['key']) != '') {
-            return ['key' => $config['key'], 'keyphrase' => $config['keyphrase']];
-        } elseif (isset($config['keytext']) && trim($config['keytext']) != '') {
-            return ['keytext' => $config['keytext']];
-        } elseif (isset($config['password'])) {
-            return ['password' => $config['password']];
-        }
-
-        throw new InvalidArgumentException('Password / key is required.');
-    }
 
     public function output() {
         $output = $this->connection->getOutput();
@@ -250,11 +226,17 @@ class CRemote_SSH {
         return $this->connection->getString($remote);
     }
 
+    /**
+     * @return CRemote_SSH_Config
+     */
     public function getConfig() {
         return $this->config;
     }
 
+    /**
+     * @return string
+     */
     public function getHost() {
-        return carr::get($this->config, 'host', carr::get($this->config, 'ip_address'));
+        return $this->config->getConnectionHost();
     }
 }
