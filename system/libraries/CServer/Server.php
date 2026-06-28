7f1c263d5b19e36d60d27960e4e198298c068c2a
Hery <hery@ittron.co.id>
2026-06-28 15:16:41 +0700

    implement CServer_Server with SSH support
    Accepts CRemote_SSH or CRemote_SSH_Config, null means local server.

--- system/libraries/CServer/Server.php
+++ system/libraries/CServer/Server.php
@@ -3,4 +3,39 @@
 defined('SYSPATH') or die('No direct access allowed.');
 
 class CServer_Server {
+    /**
+     * @var null|CRemote_SSH
+     */
+    private $ssh;
+
+    /**
+     * @param null|CRemote_SSH|CRemote_SSH_Config $ssh
+     */
+    public function __construct($ssh = null) {
+        if ($ssh instanceof CRemote_SSH_Config) {
+            $ssh = new CRemote_SSH($ssh);
+        }
+        $this->ssh = $ssh;
+    }
+
+    /**
+     * @return null|CRemote_SSH
+     */
+    public function getSSH() {
+        return $this->ssh;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRemote() {
+        return $this->ssh !== null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLocal() {
+        return $this->ssh === null;
+    }
 }
