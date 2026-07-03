712e0d719a613e58f51ee40905a8bcffa226537b
Hery <hery@ittron.co.id>
2026-07-04 05:55:44 +0700

    style: remove stale author/license docblock in Form Field compat trait

--- system/libraries/CTrait/Compat/Element/Form/Field.php
+++ system/libraries/CTrait/Compat/Element/Form/Field.php
@@ -3,10 +3,6 @@
 defined('SYSPATH') or die('No direct access allowed.');
 
 /**
- * @author Hery Kurniawan
- * @license Ittron Global Teknologi <ittron.co.id>
- *
- * @since Feb 17, 2018, 2:29:43 AM
  * @see CElement_Component_Form_Field
  */
 //@codingStandardsIgnoreStart
