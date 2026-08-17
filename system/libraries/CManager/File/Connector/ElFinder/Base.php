a068dc6474185d1fb8e8d277698b8c1bcb8a011a
Hery <hery@ittron.co.id>
2026-08-18 03:38:53 +0700

    fix(CManager): ElFinder gagal diurai di PHP 8
    $url{0} - sintaks offset berkurung kurawal yang dihapus di PHP 8.0.
    Bukan deprecation melainkan parse error, jadi seluruh berkas mati saat
    dimuat, dan kelasnya terjangkau lewat CElement_Component_ElFinder. Artinya
    file manager tidak berfungsi di server PHP 8 mana pun - termasuk produksi
    yang berjalan di lsphp84.
    
    Diganti $url[0], yang sah sejak PHP 5 sampai 8 - diuji lint di lsphp74 dan
    di PHP 8.2, dua-duanya bersih. Satu-satunya galat sintaks di ~6.000 berkas
    yang dilint, dan ditemukan gerbang lint yang baru, bukan dari laporan.

--- system/libraries/CManager/File/Connector/ElFinder/Base.php
+++ system/libraries/CManager/File/Connector/ElFinder/Base.php
@@ -1841,7 +1841,7 @@ class CManager_File_Connector_ElFinder_Base {
                     $_url = $url;
                     $url = trim($matches[1]);
                     if (!preg_match('/^https?:\//', $url)) { // no scheme
-                        if ($url{0} != '/') { // Relative path
+                        if ($url[0] != '/') { // Relative path
                             // to Absolute path
                             $url = substr($url_path, 0, strrpos($url_path, '/')) . '/' . $url;
                         }
